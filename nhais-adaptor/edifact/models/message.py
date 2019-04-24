e8ebbf2c3461a03d4a0389cb396fe36da1f630bf
Fawad Ali <fawad.ali@bjss.com>
2019-04-24 12:46:32 +0100

    added MessageTrailer to models

--- nhais-adaptor/edifact/models/message.py
+++ nhais-adaptor/edifact/models/message.py
@@ -3,7 +3,7 @@ from edifact.models.segment import Segment
 
 class MessageHeader(Segment):
     """
-     A specialisation of a segment for the specific use case of a message header
+    A specialisation of a segment for the specific use case of a message header
     takes in specific values required to generate an message header
     example: UNH+00000003+FHSREG:0:1:FH:FHS001'
     """
@@ -15,5 +15,23 @@ class MessageHeader(Segment):
         :param sequence_number: a unique reference of the message
         """
         segment_value = f"{sequence_number}+FHSREG:0:1:FH:FHS001"
+        super().__init__(key=self.SEGMENT_KEY, value=segment_value)
+
+
+class MessageTrailer(Segment):
+    """
+    A specialisation of a segment for the specific use case of a message trailer
+    takes in specific values required to generate a message trailer
+    example: UNT+18+00000003'
+    """
+
+    SEGMENT_KEY = "UNT"
+
+    def __init__(self, number_of_segments, sequence_number):
+        """
+        :param number_of_segments: the total number of segments in the message including the header and trailer
+        :param sequence_number: a unique reference of the message
+        """
+        segment_value = f"{number_of_segments}+{sequence_number}"
         super().__init__(key=self.SEGMENT_KEY, value=segment_value)
 
--- nhais-adaptor/edifact/models/tests/message_test.py
+++ nhais-adaptor/edifact/models/tests/message_test.py
@@ -1,5 +1,5 @@
 import unittest
-from edifact.models.message import MessageHeader
+from edifact.models.message import MessageHeader, MessageTrailer
 
 
 class MessageHeaderTest(unittest.TestCase):
@@ -10,6 +10,16 @@ class MessageHeaderTest(unittest.TestCase):
     def test_message_header_to_edifact(self):
         msg_hdr = MessageHeader(sequence_number="00001").to_edifact()
         self.assertEqual(msg_hdr, "UNH+00001+FHSREG:0:1:FH:FHS001'")
+
+
+class MessageTrailerTest(unittest.TestCase):
+    """
+    Test the generating of a message trailer
+    """
+
+    def test_message_trailer_to_edifact(self):
+        msg_hdr = MessageTrailer(number_of_segments=5, sequence_number="00001").to_edifact()
+        self.assertEqual(msg_hdr, "UNT+5+00001'")
 
 
 if __name__ == '__main__':
