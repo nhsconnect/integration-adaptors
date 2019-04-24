17d6bdb757350102a5c5c87630a63f904f2ed8be
Philip Woods <philip.woods@bjss.com>
2019-04-24 15:44:39 +0100

    Make tests only read string from file rather than objectify twice

--- SCR/scr/tests/test_gpsummaryupdate.py
+++ SCR/scr/tests/test_gpsummaryupdate.py
@@ -6,54 +6,66 @@ from pathlib import Path
 from scr.gpsummaryupdate import SummaryCareRecord
 
 
+def get_file_string(file_path):
+    """
+    Gets the contents of a string from a file
+    :param file_path:
+    :return: string containing the file data
+    """
+    with open(file_path) as file:
+        return file.read()
+
+
 class FullTest(unittest.TestCase):
 
     summaryCareRecord = SummaryCareRecord()
+
+    xmlFileDir = Path(ROOT_DIR + '/scr/tests/test_xmls/')
+    hashFileDir = Path(ROOT_DIR + '/scr/tests/hashes/')
 
     def test_basic(self):
         """
         A basic test using the clean summary update from the spine tests
         """
-        root = etree.parse(str(Path(ROOT_DIR) / 'scr/tests/test_xmls/cleanSummaryUpdate.xml'))
-        expected = etree.tostring(root)
-        render = self.summaryCareRecord.populate_template_with_file(
-            str(Path(ROOT_DIR) / 'scr/tests/hashes/hash16UK05.json'))
-        Utilities.assert_xml_equal(expected, render)
+        expected_xml_file_path = str(self.xmlFileDir / 'cleanSummaryUpdate.xml')
+        hash_file_path = str(self.hashFileDir / 'hash16UK05.json')
+
+        expected_string = get_file_string(expected_xml_file_path)
+        render = self.summaryCareRecord.populate_template_with_file(hash_file_path)
+        Utilities.assert_xml_equal(expected_string, render)
 
     def test_extended_html(self):
         """
         Uses a larger set of Html for the human readable contents
         """
-        root = etree.parse(str(Path(ROOT_DIR) / 'scr/tests/test_xmls/SummaryUpdateExtendedContents.xml'))
-        expected = etree.tostring(root)
+        expected_xml_file_path = str(self.xmlFileDir / 'SummaryUpdateExtendedContents.xml')
+        hash_file_path = str(self.hashFileDir / 'extendedHTMLhash.json')
 
-        file_path = str(Path(ROOT_DIR) / 'scr/tests/hashes/extendedHTMLhash.json')
-        render = self.summaryCareRecord.populate_template_with_file(file_path)
-
-        Utilities.assert_xml_equal(expected, render)
+        expected_string = get_file_string(expected_xml_file_path)
+        render = self.summaryCareRecord.populate_template_with_file(hash_file_path)
+        Utilities.assert_xml_equal(expected_string, render)
 
     def test_empty_html(self):
         """
         A test for an empty human readable content value
         """
-        root = etree.parse(str(Path(ROOT_DIR) / 'scr/tests/test_xmls/EmptyHtmlGpSummaryUpdate.xml'))
-        expected = etree.tostring(root)
+        expected_xml_file_path = str(self.xmlFileDir / 'EmptyHtmlGpSummaryUpdate.xml')
+        hash_file_path = str(self.hashFileDir / 'emptyHtmlHash.json')
 
-        file_path = str(Path(ROOT_DIR) / 'scr/tests/hashes/emptyHtmlHash.json')
-        render = self.summaryCareRecord.populate_template_with_file(file_path)
-
-        Utilities.assert_xml_equal(expected, render)
+        expected_string = get_file_string(expected_xml_file_path)
+        render = self.summaryCareRecord.populate_template_with_file(hash_file_path)
+        Utilities.assert_xml_equal(expected_string, render)
 
     def test_empty_hash(self):
         """
         Tests the contents are empty when a completely blank hash is provided
         """
-        root = etree.parse(str(Path(ROOT_DIR) / 'scr/tests/test_xmls/EmptyHash.xml'))
-        expected = etree.tostring(root)
+        expected_xml_file_path = str(self.xmlFileDir / 'EmptyHash.xml')
+        hash_file_path = str(self.hashFileDir / 'EmptyHash.json')
 
-        file_path = str(Path(ROOT_DIR) / 'scr/tests/hashes/EmptyHash.json')
-        render = self.summaryCareRecord.populate_template_with_file(file_path)
-        Utilities.assert_xml_equal(expected, render)
+        expected_string = get_file_string(expected_xml_file_path)
+        render = self.summaryCareRecord.populate_template_with_file(hash_file_path)
+        Utilities.assert_xml_equal(expected_string, render)
 
     def test_replacementOf(self):
         """
@@ -62,12 +74,13 @@ class FullTest(unittest.TestCase):
         this partial doesnt appear in the previous tests but here a list with a single
         element is used to show how conditionals are used in mustache
         """
-        root = etree.parse(str(Path(ROOT_DIR) / 'scr/tests/test_xmls/replacementOf.xml'))
-        expected = etree.tostring(root)
+        expected_xml_file_path = str(self.xmlFileDir / 'replacementOf.xml')
+        hash_file_path = str(self.hashFileDir / 'replacementOfhash.json')
 
-        file_path = str(Path(ROOT_DIR) / 'scr/tests/hashes/replacementOfhash.json')
-        render = self.summaryCareRecord.populate_template_with_file(file_path)
-        Utilities.assert_xml_equal(expected, render)
+        expected_string = get_file_string(expected_xml_file_path)
+        render = self.summaryCareRecord.populate_template_with_file(hash_file_path)
+        Utilities.assert_xml_equal(expected_string, render)
+
 
     def test_multipleReplacementOf(self):
         """
@@ -75,32 +88,32 @@ class FullTest(unittest.TestCase):
         This test is build purely for demonstrating having a variable number of occurrences of
         a partial in mustache, it does not conform to the schema and will not be accepted as a valid message
         """
-        root = etree.parse(str(Path(ROOT_DIR) / 'scr/tests/test_xmls/multipleReplacementOf.xml'))
-        expected = etree.tostring(root)
+        expected_xml_file_path = str(self.xmlFileDir / 'multipleReplacementOf.xml')
+        hash_file_path = str(self.hashFileDir / 'multiReplacementOfhash.json')
 
-        file_path = str(Path(ROOT_DIR) / 'scr/tests/hashes/multiReplacementOfhash.json')
-        render = self.summaryCareRecord.populate_template_with_file(file_path)
-        Utilities.assert_xml_equal(expected, render)
+        expected_string = get_file_string(expected_xml_file_path)
+        render = self.summaryCareRecord.populate_template_with_file(hash_file_path)
+        Utilities.assert_xml_equal(expected_string, render)
+
 
     def test_python_dictionary_example(self):
         """
         Basic test to demonstrate passing a python dict to the interface instead of a json file
         """
-        root = etree.parse(str(Path(ROOT_DIR) / 'scr/tests/test_xmls/cleanSummaryUpdate.xml'))
-        expected = etree.tostring(root)
+        expected_string = get_file_string(str(Path(ROOT_DIR) / 'scr/tests/test_xmls/cleanSummaryUpdate.xml'))
         from scr.tests.hashes.basic_dict import input_hash
 
         render = self.summaryCareRecord.populate_template(input_hash)
-        Utilities.assert_xml_equal(expected, render)
+        Utilities.assert_xml_equal(expected_string, render)
+
 
     def test_json_string_example(self):
         """
         Basic example showing how a json string can be passed to the interface
         """
-        root = etree.parse(str(Path(ROOT_DIR) / 'scr/tests/test_xmls/cleanSummaryUpdate.xml'))
-        expected = etree.tostring(root)
-        json_file = str(Path(ROOT_DIR) / 'scr/tests/hashes/hash16UK05.json')
+        expected_string = get_file_string(str(self.xmlFileDir / 'cleanSummaryUpdate.xml'))
+        json_file = str(self.hashFileDir / 'hash16UK05.json')
         with open(json_file) as file:
             data = file.read()  # Reads file contents into a string
             render = self.summaryCareRecord.populate_template_with_json_string(data)
-            Utilities.assert_xml_equal(expected, render)
+            Utilities.assert_xml_equal(expected_string, render)
