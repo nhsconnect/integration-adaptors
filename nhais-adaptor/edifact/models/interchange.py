1508c52688dc79ae815fbf8301c4ff0121cfdd7a
Fawad Ali <fawad.ali@bjss.com>
2019-04-24 12:07:27 +0100

    added InterchangeTrailer

--- nhais-adaptor/edifact/models/interchange.py
+++ nhais-adaptor/edifact/models/interchange.py
@@ -20,7 +20,25 @@ class InterchangeHeader(Segment):
         """
         formatted_date_time = datetime.strptime(date_time, '%Y-%m-%d %H:%M:%S.%f').strftime('%y%m%d:%H%M')
         segment_value = f"UNOA:2+{sender}+{recipient}+{formatted_date_time}+{sequence_number}"
-        super().__init__(key=InterchangeHeader.SEGMENT_KEY, value=segment_value)
+        super().__init__(key=self.SEGMENT_KEY, value=segment_value)
+
+
+class InterchangeTrailer(Segment):
+    """
+    A specialisation of a segment for the specific use case of an interchange trailer
+    takes in specific values required to generate an interchange trailer
+    example: UNZ+1+00000002'
+    """
+
+    SEGMENT_KEY = "UNZ"
+
+    def __init__(self, number_of_messages, sequence_number):
+        """
+        :param number_of_messages: the number of messages within this interchange
+        :param sequence_number: a unique reference of the interchange
+        """
+        segment_value = f"{number_of_messages}+{sequence_number}"
+        super().__init__(key=self.SEGMENT_KEY, value=segment_value)
 
 
 class Interchange(object):
--- nhais-adaptor/edifact/models/tests/interchange_test.py
+++ nhais-adaptor/edifact/models/tests/interchange_test.py
@@ -1,5 +1,5 @@
 import unittest
-from edifact.models.interchange import Interchange, InterchangeHeader
+from edifact.models.interchange import Interchange, InterchangeHeader, InterchangeTrailer
 
 
 class InterchangeHeaderTest(unittest.TestCase):
@@ -10,6 +10,16 @@ class InterchangeHeaderTest(unittest.TestCase):
     def test_interchange_header_to_edifact(self):
         int_hdr = InterchangeHeader(sender="SNDR", recipient="RECP", date_time="2019-04-23 09:00:04.159338", sequence_number="00001").to_edifact()
         self.assertEqual(int_hdr, "UNB+UNOA:2+SNDR+RECP+190423:0900+00001'")
+
+
+class InterchangeTrailerTest(unittest.TestCase):
+    """
+    Test the generating of an interchange trailer
+    """
+
+    def test_interchange_trailer_to_edifact(self):
+        int_hdr = InterchangeTrailer(number_of_messages=1, sequence_number="00001").to_edifact()
+        self.assertEqual(int_hdr, "UNZ+1+00001'")
 
 
 class InterchangeTest(unittest.TestCase):
