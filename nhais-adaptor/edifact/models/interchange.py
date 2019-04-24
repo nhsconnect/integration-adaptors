a5e59a4b90abdcd786b15068f68d677f63fc9bd2
Fawad Ali <fawad.ali@bjss.com>
2019-04-24 11:09:17 +0100

    added InterchangeHeader model

--- nhais-adaptor/edifact/models/interchange.py
+++ nhais-adaptor/edifact/models/interchange.py
@@ -1,3 +1,26 @@
+from edifact.models.segment import Segment
+
+
+class InterchangeHeader(Segment):
+    """
+    A specialisation of a segment for the specific use case of an interchange header
+    takes in specific values required to generate an interchange header
+    example: UNB+UNOA:2+TES5+XX11+920113:1317+00000002'
+    """
+
+    SEGMENT_KEY = "UNB"
+
+    def __init__(self, sender, recipient, date_time, sequence_number):
+        """
+        :param sender: the sender of the interchange
+        :param recipient: the intended recipient of the interchange
+        :param date_time: the date time stamp of the interchange header
+        :param sequence_number: a unique reference of the interchange
+        """
+        segment_value = f"UNOA:2+{sender}+{recipient}+{date_time}+{sequence_number}"
+        super().__init__(key=InterchangeHeader.SEGMENT_KEY, value=segment_value)
+
+
 class Interchange(object):
     """
     The edifact interchange that is used to interface with NHAIS
--- nhais-adaptor/edifact/models/tests/interchange_test.py
+++ nhais-adaptor/edifact/models/tests/interchange_test.py
@@ -1,5 +1,15 @@
 import unittest
-from edifact.models.interchange import Interchange
+from edifact.models.interchange import Interchange, InterchangeHeader
+
+
+class InterchangeHeaderTest(unittest.TestCase):
+    """
+    Test the generating of an interchange header
+    """
+
+    def test_interchange_header_to_edifact(self):
+        int_hdr = InterchangeHeader(sender="SNDR", recipient="RECP", date_time="920113:1317", sequence_number="00001").to_edifact()
+        self.assertEqual(int_hdr, "UNB+UNOA:2+SNDR+RECP+920113:1317+00001'")
 
 
 class InterchangeTest(unittest.TestCase):
