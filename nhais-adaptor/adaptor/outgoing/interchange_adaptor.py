f36b7cc17e9830f5b26b5fad2d6186094b9b9e2b
Fawad Ali <fawad.ali@bjss.com>
2019-04-30 16:21:06 +0100

    extracted function to generate recipient cypher

--- nhais-adaptor/adaptor/outgoing/interchange_adaptor.py
+++ nhais-adaptor/adaptor/outgoing/interchange_adaptor.py
@@ -11,6 +11,21 @@ class InterchangeAdaptor:
     """
 
     @staticmethod
+    def generate_recipient_from(nhais_cypher):
+        """
+        Generate the recipient cypher.
+        Will append '1' when the nhais cypher is 3 characters and '01' if 2 characters
+        :param nhais_cypher: The nhais cypher provided. Should be 2-3 characters in length
+        :return: The recipient cypher
+        """
+        recipient = ''
+        if len(nhais_cypher) == 3:
+            recipient = nhais_cypher + '1'
+        elif len(nhais_cypher) == 2:
+            recipient = nhais_cypher + "01"
+        return recipient
+
+    @staticmethod
     def create_interchange(fhir_operation):
         """
         Create the edifact interchange from the fhir operation definition
@@ -21,11 +36,7 @@ class InterchangeAdaptor:
                                                               parameter_name=ParameterName.INTERCHANGE_SEQ_NO)
         sender_cypher = odh.get_parameter_value(fhir_operation, parameter_name=ParameterName.SENDER_CYPHER)
         nhais_cypher = odh.get_parameter_value(fhir_operation, parameter_name=ParameterName.NHAIS_CYPHER)
-        recipient = ''
-        if len(nhais_cypher) == 3:
-            recipient = nhais_cypher + '1'
-        elif len(nhais_cypher) == 2:
-            recipient = nhais_cypher + "01"
+        recipient = InterchangeAdaptor.generate_recipient_from(nhais_cypher)
 
         messages = Messages(messages=[MessageAdaptor.create_message(fhir_operation)])
 
--- nhais-adaptor/adaptor/outgoing/tests/test_interchange_adaptor.py
+++ nhais-adaptor/adaptor/outgoing/tests/test_interchange_adaptor.py
@@ -11,6 +11,20 @@ class InterchangeAdaptorTest(unittest.TestCase):
     Test the conversation of fhir to an edifact interchange
     """
 
+    def test_generate_recipient_from(self):
+        """
+        Test the generation of a recipient cypher from the nhais cypher
+        """
+        with self.subTest("When the nhais cypher is 3 characters"):
+            recipient_cypher = InterchangeAdaptor.generate_recipient_from('XX1')
+
+            self.assertEqual(recipient_cypher, "XX11")
+
+        with self.subTest("When the nhais cypher is 2 characters"):
+            recipient_cypher = InterchangeAdaptor.generate_recipient_from('XX')
+
+            self.assertEqual(recipient_cypher, "XX01")
+
     def test_create_interchange(self):
         """
         Test the function to create an edifact interchange
