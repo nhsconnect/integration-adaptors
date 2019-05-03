1a1bd2f5bb7ce6eb20eaf5c801295bcd7e696f38
Philip Woods <philip.woods@bjss.com>
2019-05-03 17:14:57 +0100

    Add tests for message handler

--- OneOneOne/OneOneOne/tests/test_message_handler.py
+++ OneOneOne/OneOneOne/tests/test_message_handler.py
@@ -1,6 +1,7 @@
 import unittest
 from pathlib import Path
 
+from builder.pystache_message_builder import PystacheMessageBuilder
 from utilities.file_utilities import FileUtilities
 from utilities.xml_utilities import XmlUtilities
 from OneOneOne.OneOneOne.message_handler import MessageHandler
@@ -9,36 +10,53 @@ from definitions import ROOT_DIR
 
 
 class MessageHandlerTest(unittest.TestCase):
-
-    xmlFileDir = Path(ROOT_DIR) / 'OneOneOne' / 'tests' / 'expected_output_xmls'
-    hashFileDir = Path(ROOT_DIR + '/scr/tests/hashes/')
-    action_does_not_match_service = """
-    <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:wsa="http://www.w3.org/2005/08/addressing"
-                   xmlns:itk="urn:nhs-itk:ns:201005">
-        <soap:Header>
-            <wsa:Action>urn:nhs-itk:services:201005:SendNHS111Report-v2-0-ThisDoesNotMatchBelow</wsa:Action>
-        </soap:Header>
-        <soap:Body>
-            <itk:DistributionEnvelope
-                    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-                <itk:header service="urn:nhs-itk:services:201005:SendNHS111Report-v2-0_Bad_Service-ThisDoesNotMatchAbove"
-                            trackingid="7D6F23E0-AE1A-11DB-9808-B18E1E0994CD">        
-                </itk:header>
-            </itk:DistributionEnvelope>
-        </soap:Body>
-    </soap:Envelope>
-    """
+    expectedXmlFileDir = Path(ROOT_DIR) / 'OneOneOne' / 'tests' / 'expected_output_xmls'
+    inputXmlFileDir = Path(ROOT_DIR) / 'OneOneOne' / 'tests' / 'input_xmls'
 
     def test_action_not_matching_service(self):
-        mh = MessageHandler(self.action_does_not_match_service)
+        builder = PystacheMessageBuilder(str(self.inputXmlFileDir), 'action_does_not_match_service')
+
+        input = {'action': "urn:nhs-itk:services:201005:SendNHS111Report-v2-0-ThisDoesNotMatchBelow",
+                 'service': "urn:nhs-itk:services:201005:SendNHS111Report-v2-0_Bad_Service-ThisDoesNotMatchAbove"}
+
+        msg = builder.build_message(input)
+        mh = MessageHandler(msg)
         status_code, response = mh.evaluate_message()
 
-        assert(status_code == 500)
+        assert (status_code == 500)
 
-        expected = FileUtilities.get_file_string(str(self.xmlFileDir / 'invalid_action_service_values_response.xml'))
-
+        expected = FileUtilities.get_file_string(
+            str(self.expectedXmlFileDir / 'invalid_action_service_values_response.xml'))
         XmlUtilities.assert_xml_equal_utf_8(expected, response)
 
-    def test_action_does_match_service(self):
-        pass
+    def test_manifest_payload_count(self):
+        builder = PystacheMessageBuilder(str(self.inputXmlFileDir), 'manifest_payload_count')
 
+        input = {'manifestCount': "2",
+                 'service': "5"}
+
+        msg = builder.build_message(input)
+        mh = MessageHandler(msg)
+        status_code, response = mh.evaluate_message()
+
+        assert (status_code == 500)
+
+        expected = FileUtilities.get_file_string(
+            str(self.expectedXmlFileDir / 'manifest_not_equal_to_payload_count.xml'))
+        XmlUtilities.assert_xml_equal_utf_8(expected, response)
+
+    def test_manifest_count_matches_manifest_instances(self):
+        builder = PystacheMessageBuilder(str(self.inputXmlFileDir), 'manifest_payload_count')
+
+        input = {'manifestCount': "2",
+                 'manifest': ['one', 'two']}
+
+        msg = builder.build_message(input)
+        mh = MessageHandler(msg)
+        status_code, response = mh.evaluate_message()
+
+        assert (status_code == 500)
+
+        expected = FileUtilities.get_file_string(
+            str(self.expectedXmlFileDir / 'manifest_not_equal_to_payload_count.xml'))
+        XmlUtilities.assert_xml_equal_utf_8(expected, response)
--- OneOneOne/OneOneOne/web_service.py
+++ OneOneOne/OneOneOne/web_service.py
@@ -1,19 +1,33 @@
+import logging
+from time import sleep
+
 from tornado.httpserver import HTTPServer
 import tornado.ioloop
 import tornado.web
 from lxml import etree
 import xml.etree.ElementTree as ET
+import requests
+
+from tornado.iostream import StreamClosedError
+
 from utilities.file_utilities import FileUtilities
 from definitions import XML_PATH
 from OneOneOne.OneOneOne.message_handler import MessageHandler
 
 response = FileUtilities.get_file_string(XML_PATH / 'basic_success_response.xml')
 
+
 class MessageReceiver(tornado.web.RequestHandler):
+
     def initialize(self, servicesDict):
+        logging.basicConfig(level=logging.DEBUG)
+        self.connection_closed = False
         pass
 
+    @tornado.gen.coroutine
     def post(self):
+        print(self.connection_closed)
+        logging.basicConfig(level=logging.DEBUG)
         mh = MessageHandler(self.request.body)
         # parser = etree.XMLParser(remove_blank_text=True)
         # root = etree.fromstring(self.request.body, parser)
@@ -22,13 +36,17 @@ class MessageReceiver(tornado.web.RequestHandler):
         status_code, message_response = mh.evaluate_message()
         self.set_status(status_code)
         self.write(message_response)
-        # self.finish()
-        # self.flush(True)
-        # self.finish(message_response)
-        print("Responded")
+        #yield self.flush()
+        #self.finish()
+        #yield self.flush()
+
 
     def get(self):
         print("Test get method")
+
+    @tornado.gen.coroutine
+    def on_connection_close(self) -> None:
+        yield self.flush()
 
 
 if __name__ == "__main__":
