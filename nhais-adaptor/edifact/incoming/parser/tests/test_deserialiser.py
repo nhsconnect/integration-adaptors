eb4cdfb89938a04b89bee41586b36aa528a3cc94
Fawad Ali <fawad.ali@bjss.com>
2019-05-03 16:48:44 +0100

    test parser can handle an interchange with multiple messages

--- nhais-adaptor/edifact/incoming/parser/tests/test_deserialiser.py
+++ nhais-adaptor/edifact/incoming/parser/tests/test_deserialiser.py
@@ -133,3 +133,38 @@ class TestDeserialiser(unittest.TestCase):
 
             result = deserialiser.convert(input_lines)
             compare(result, expected)
+
+        with self.subTest("When the edifact incoming interchange has 2 messages"):
+            expected = Interchange(InterchangeHeader("SO01", "ROO5", "190429:1756"),
+                                   Messages([
+                                       MessageSegment(MessageSegmentBeginningDetails("F4"),
+                                                      MessageSegmentRegistrationDetails("211102")),
+                                       MessageSegment(MessageSegmentBeginningDetails("F4"),
+                                                      MessageSegmentRegistrationDetails("211103"))
+                                   ]))
+
+            input_lines = [
+                "UNB+UNOA:2+SO01+ROO5+190429:1756+00016288++FHSREG+++FHSA EDI TRANSFERS",
+                "UNH+00024986+FHSREG:0:1:FH:FHS001",
+                "BGM+++507",
+                "NAD+FHS+SO:954",
+                "DTM+137:201904291755:203",
+                "RFF+950:F4",
+                "S01+1",
+                "RFF+TN:211102",
+                "NAD+GP+1231231,PLP348:900",
+                "UNT+9+00024986",
+                "UNH+00024987+FHSREG:0:1:FH:FHS001",
+                "BGM+++507",
+                "NAD+FHS+SO:954",
+                "DTM+137:201904291755:203",
+                "RFF+950:F4",
+                "S01+1",
+                "RFF+TN:211103",
+                "NAD+GP+1231231,PLP348:900",
+                "UNT+9+00024986",
+                "UNZ+1+00016288",
+            ]
+
+            result = deserialiser.convert(input_lines)
+            compare(result, expected)
