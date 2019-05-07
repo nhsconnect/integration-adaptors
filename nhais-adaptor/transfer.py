7aa164d61cac69923fd85614ba6c1e4d2ac1760e
Fawad Ali <fawad.ali@bjss.com>
2019-05-07 09:17:53 +0100

    updated transfer script

--- nhais-adaptor/transfer.py
+++ nhais-adaptor/transfer.py
@@ -1,8 +1,39 @@
+import os
 import json
 from fhirclient.models.operationdefinition import OperationDefinition
 import adaptor.outgoing.interchange_adaptor as adaptor
+from PyInquirer import prompt
 
-with open("./mailbox/GP/TES5/outbox/patient-register-birth.json", "r") as patient_register:
+dirs = os.listdir("./mailbox/GP")
+
+gp_outbox_choices = [
+    {
+        'type': 'list',
+        'name': 'gp',
+        'message': 'Select a GP outbox to read from: ',
+        'choices': dirs
+    }
+]
+
+answers = prompt(gp_outbox_choices)
+gp_cypher = answers["gp"]
+
+files = os.listdir(f"./mailbox/GP/{gp_cypher}/outbox")
+
+file_choices = [
+    {
+        'type': 'list',
+        'name': 'file',
+        'message': 'Select a file: ',
+        'choices': files
+    }
+]
+
+file_answer = prompt(file_choices)
+file_name = file_answer["file"]
+print(file_name)
+
+with open(f"./mailbox/GP/{gp_cypher}/outbox/{file_name}", "r") as patient_register:
     patient_register_json = json.load(patient_register)
 
 op_def = OperationDefinition(patient_register_json)
