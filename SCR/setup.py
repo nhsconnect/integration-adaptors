d14fd98d3659f6c48b327752ac371df3a485a77a
Gareth Allan <gareth.allan@bjss.com>
2019-05-08 16:42:59 +0100

    Added a setup.py for the SCR project

---
+++ SCR/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+import setuptools
+
+setup(
+    name='scr',
+    version='',
+    packages=setuptools.find_packages(),
+    url='',
+    license='',
+    author='Phillip Woods',
+    author_email='',
+    description='Artifacts for producing SCR requests.',
+)
