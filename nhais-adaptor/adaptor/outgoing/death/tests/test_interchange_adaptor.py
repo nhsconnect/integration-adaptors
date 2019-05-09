3eb2366dc34d31f2ce76ea337751dc3266e6e45f
Fawad Ali <fawad.ali@bjss.com>
2019-05-09 13:05:47 +0100

    outgoing interchange tests for death registration with free texts

--- nhais-adaptor/adaptor/outgoing/death/tests/test_interchange_adaptor.py
+++ nhais-adaptor/adaptor/outgoing/death/tests/test_interchange_adaptor.py
@@ -14,7 +14,7 @@ class TestInterchangeAdaptor(unittest.TestCase):
         """
         Test the function to create an edifact interchange
         """
-        with self.subTest("When the operation is for a Death Registration"):
+        with self.subTest("When the operation is for a Death Registration and without free texts"):
             expected_edifact_interchange = ("UNB+UNOA:2+TES5+XX11+190423:0900+000001++FHSREG'"
                                             "UNH+000001+FHSREG:0:1:FH:FHS001'"
                                             "BGM+++507'"
@@ -32,6 +32,30 @@ class TestInterchangeAdaptor(unittest.TestCase):
                                             "UNZ+1+000001'")
 
             op_def = fixtures.create_operation_definition_for_death_registration()
+
+            (sender, recipient, interchange_seq_no, interchange) = adaptor.create_interchange(fhir_operation=op_def)
+
+            compare(interchange, expected_edifact_interchange)
+
+        with self.subTest("When the operation is for a Death Registration and with free texts"):
+            expected_edifact_interchange = ("UNB+UNOA:2+TES5+XX11+190423:0900+000001++FHSREG'"
+                                            "UNH+000001+FHSREG:0:1:FH:FHS001'"
+                                            "BGM+++507'"
+                                            "NAD+FHS+XX1:954'"
+                                            "DTM+137:201904230900:203'"
+                                            "RFF+950:G5'"
+                                            "S01+1'"
+                                            "RFF+TN:17'"
+                                            "NAD+GP+4826940,281:900'"
+                                            "GIS+1:ZZZ'"
+                                            "DTM+961:20190420:102'"
+                                            "FTX+RGI+++DIED IN INFINITY WARS'"
+                                            "S02+2'"
+                                            "PNA+PAT+NHSNO22222:OPI'"
+                                            "UNT+14+000001'"
+                                            "UNZ+1+000001'")
+
+            op_def = fixtures.create_operation_definition_for_death_registration(free_text="Died in Infinity Wars")
 
             (sender, recipient, interchange_seq_no, interchange) = adaptor.create_interchange(fhir_operation=op_def)
 
