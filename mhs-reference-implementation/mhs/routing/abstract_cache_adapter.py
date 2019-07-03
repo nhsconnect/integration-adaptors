a3f7cbf5f086eab6a45f47ed5d5078c2f5a22a4d
Philip Woods <philip.woods@bjss.com>
2019-07-03 16:58:27 +0100

    Add type hints

--- mhs-reference-implementation/mhs/routing/abstract_cache_adapter.py
+++ mhs-reference-implementation/mhs/routing/abstract_cache_adapter.py
@@ -1,33 +1,30 @@
 import abc
+from typing import Optional, Dict
+
+FIFTEEN_MINUTES_IN_SECONDS = 900
 
 
 class AbstractMHSCacheAdaptor(abc.ABC):
 
-    def __init__(self, expiry_time=15):
+    def __init__(self, expiry_time=FIFTEEN_MINUTES_IN_SECONDS):
         """
-        :param expiry_time: Time for a value to expire in minutes
+        :param expiry_time: Time for a value to expire in seconds
         """
+        if expiry_time < 0:
+            raise ValueError('Expiry time must not be non-negative')
         self.expiry_time = expiry_time
-        pass
 
     @abc.abstractmethod
-    async def retrieve_mhs_attributes_value(self, ods_code: str, interaction_id: str):
+    async def retrieve_mhs_attributes_value(self, ods_code: str, interaction_id: str) -> Optional[Dict]:
         """
         Given a key the cache should return either the cached value or None if the value is not found or if the value is
         out of date
-        :param ods_code:
-        :param interaction_id:
-        :return:
         """
         raise NotImplementedError()
 
     @abc.abstractmethod
-    def add_cache_value(self, ods_code: str, interaction_id: str, value):
+    def add_cache_value(self, ods_code: str, interaction_id: str, value) -> None:
         """
-
-        :param ods_code:
-        :param interaction_id:
-        :param value:
-        :return:
+        Adds a value to the cache, recording the input time used to determine when values have expired
         """
         raise NotImplementedError()
--- mhs-reference-implementation/mhs/routing/dictionary_cache.py
+++ mhs-reference-implementation/mhs/routing/dictionary_cache.py
@@ -1,5 +1,6 @@
 import mhs.routing.abstract_cache_adapter as ACA
 import time
+from typing import Dict, Optional
 
 FIFTEEN_MINUTES_IN_SECONDS = 900
 
@@ -10,32 +11,33 @@ def _generate_key(ods_code: str, interaction_id: str):
 
 class DictionaryCache(ACA.AbstractMHSCacheAdaptor):
 
-    def __init__(self, expiry_time=FIFTEEN_MINUTES_IN_SECONDS):
+    def __init__(self, expiry_time: float = FIFTEEN_MINUTES_IN_SECONDS):
         if expiry_time < 0:
             raise ValueError('Invalid expiry time, must be non-negative')
         self.cache = {}
         super().__init__(expiry_time)
 
-    async def retrieve_mhs_attributes_value(self, ods_code: str, interaction_id: str):
+    async def retrieve_mhs_attributes_value(self, ods_code: str, interaction_id: str) -> Optional[Dict]:
         key = _generate_key(ods_code, interaction_id)
 
         if key in self.cache and not self._is_value_expired(key):
             if self._is_value_expired(key):
                 self.cache[key] = None
+                del self.cache[key]
                 return None
             else:
                 return self.cache[key]['value']
 
         return None
 
-    def _is_value_expired(self, key: str):
+    def _is_value_expired(self, key: str) -> bool:
         current_time = time.time()
         insert_time = self.cache[key]['time']
         if current_time - insert_time > self.expiry_time:
             return True
         return False
 
-    async def add_cache_value(self, ods_code: str, interaction_id: str, value):
+    async def add_cache_value(self, ods_code: str, interaction_id: str, value) -> None:
         key = _generate_key(ods_code, interaction_id)
         insert_time = time.time()
         self.cache[key] = {
