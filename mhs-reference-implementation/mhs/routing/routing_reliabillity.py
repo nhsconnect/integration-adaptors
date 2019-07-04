36b865c040e0692ff4026fa1858a51ca2dbde5af
Philip Woods <philip.woods@bjss.com>
2019-07-04 11:49:50 +0100

    Base routing

---
+++ mhs-reference-implementation/mhs/routing/routing_reliabillity.py
@@ -0,0 +1,22 @@
+from mhs.routing import sds_handler
+
+class RoutingAndReliability:
+
+    def __init__(self):
+        self.lookup = sds_handler.MHSAttributeLookupHandler()
+
+    async def get_end_point(self, org_code, service_id):
+        endpoint_details = await self.lookup.retrieve_mhs_attributes(org_code, service_id)
+
+        first_endpoint = endpoint_details[0]['attributes']['nhsMHSEndPoint']
+
+        return first_endpoint[0]
+
+    async def get_reliability(self, org_code, service_id):
+        endpoint_details = await self.lookup.retrieve_mhs_attributes(org_code, service_id)
+        first_endpoint = endpoint_details[0]['attributes']
+
+        reliability_strings = ['nhsMHSAckRequested', 'nhsMHSDuplicateElimination', 'nhsMHSPersistDuration',
+                               'nhsMHSRetries', 'nhsMHSRetryInterval']
+        reliability = {item: first_endpoint[item] for item in reliability_strings}
+        return reliability
