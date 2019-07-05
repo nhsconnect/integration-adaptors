c378595a7f7e14cdffd2e6702b286930e9b90432
Philip-Woods <49308422+Philip-Woods@users.noreply.github.com>
2019-07-05 13:31:48 +0100

    Feature/RT60: Routing & RT57: Reliability (#27)
    * Added routing and reliability components

--- mhs-reference-implementation/mhs/routing/sds_exception.py
+++ mhs-reference-implementation/mhs/routing/sds_exception.py
@@ -1,6 +1,6 @@
 
 
-class RoutingException(Exception):
+class SDSException(Exception):
     """
     A RoutingException is thrown when an issue arises with the LDAP response,
     particularly when no response is returned from the LDAP query.
