8f36073cbb864d2c8c2596f34150202ccdff8b1d
Philip Woods <philip.woods@bjss.com>
2019-07-11 17:36:01 +0100

    Add request handler timer

--- common/utilities/timing.py
+++ common/utilities/timing.py
@@ -36,3 +36,33 @@ def time_method(method):
             logging.info(f"Method '{method.__name__}' called, executed in: {duration}s")
 
     return invoke_method_with_timer
+
+
+def log_request(method):
+    """
+    A method to be used with tornado end points to extract their calling details and time their execution, this
+    mainly holds as a placeholder if any extra data is required from the call
+    """
+    def method_wrapper(*args, **kwargs):
+
+        # Gets the 'self' of the caller
+        handler = args[0]
+
+        log_details = {
+            'handler': handler.__class__.__name__,
+            'method': handler.request.method.lower(),
+            'requestBody': handler.request.body
+        }
+
+        stopwatch = StopWatch()
+        stopwatch.start_timer()
+
+        try:
+            return method(*args, **kwargs)
+        finally:
+            duration = stopwatch.stop_timer()
+            duration = round(duration, 3)
+            log_details['duration'] = duration
+            logging.info(f"{log_details}")
+
+    return method_wrapper
