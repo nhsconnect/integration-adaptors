903330c3f7e3c80fca48f959a3b38e262d87cc90
Gareth Allan <gareth.allan@bjss.com>
2019-07-19 14:12:51 +0100

    RT-28: Refactored routing component into new structure

--- mhs/mhs/message_configuration/lookup/mhs_attribute_lookup.py
+++ mhs/mhs/message_configuration/lookup/mhs_attribute_lookup.py
@@ -1,8 +1,13 @@
 """This module defines the component used to orchestrate the retrieval and caching of routing and reliability
 information for a remote MHS."""
 
-import mhs.message_configuration.lookup.sds_client as sds_client
+import logging
+from typing import Dict
+
 import mhs.message_configuration.lookup.cache_adapter as cache_adapter
+import mhs.message_configuration.lookup.sds_client as sds_client
+
+logger = logging.getLogger(__name__)
 
 
 class MHSAttributeLookup:
@@ -14,7 +19,12 @@ class MHSAttributeLookup:
         :param client The SDS client to use when retrieving remote MHS details.
         :param cache: The cache adapter to use to cache remote MHS details.
         """
-        pass
+        if not client:
+            raise ValueError('sds client required')
+        if not cache:
+            raise ValueError('No cache supplied')
+        self.cache = cache
+        self.sds_client = client
 
     async def retrieve_mhs_attributes(self, ods_code, interaction_id) -> Dict:
         """Obtains the attributes of the MHS registered for the given ODS code and interaction ID. These details will
@@ -25,4 +35,13 @@ class MHSAttributeLookup:
         :param interaction_id:
         :return:
         """
-        pass
+        cache_value = await self.cache.retrieve_mhs_attributes_value(ods_code, interaction_id)
+        if cache_value:
+            logger.info(f'MHS details found in cache for ods code: {ods_code} & interaction id: {interaction_id}')
+            return cache_value
+
+        endpoint_details = await self.sds_client.get_mhs_details(ods_code, interaction_id)
+        logger.info(f'MHS details obtained from sds, adding to cache for ods code: {ods_code} '
+                    f'interaction id: {interaction_id}')
+        await self.cache.add_cache_value(ods_code, interaction_id, endpoint_details)
+        return endpoint_details
--- mhs/mhs/message_configuration/lookup/routing_reliability.py
+++ mhs/mhs/message_configuration/lookup/routing_reliability.py
@@ -2,12 +2,25 @@
 
 import mhs.message_configuration.lookup.mhs_attribute_lookup as mhs_attribute_lookup
 
+RELIABILITY_KEYS = [
+    'nhsMHSSyncReplyMode',
+    'nhsMHSRetryInterval',
+    'nhsMHSRetries',
+    'nhsMHSPersistDuration',
+    'nhsMHSDuplicateElimination',
+    'nhsMHSAckRequested'
+]
+
+ROUTING_KEYS = ['nhsMhsFQDN', 'nhsMHSEndPoint']
+
 
 class RoutingAndReliability:
     """A tool that allows the routing and reliability information for a remote MHS to be retrieved."""
 
     def __init__(self, lookup_handler: mhs_attribute_lookup.MHSAttributeLookup):
-        pass
+        if not lookup_handler:
+            raise ValueError("MHS Attribute Lookup Handler not found")
+        self.lookup = lookup_handler
 
     async def get_end_point(self, org_code, service_id):
         """Get the endpoint of the MHS registered for the specified org code and service ID.
@@ -16,7 +29,9 @@ class RoutingAndReliability:
         :param service_id:
         :return:
         """
-        pass
+        endpoint_details = await self.lookup.retrieve_mhs_attributes(org_code, service_id)
+        routing = {item: endpoint_details[item] for item in ROUTING_KEYS}
+        return routing
 
     async def get_reliability(self, org_code, service_id):
         """Get the reliability information for the MHS registered for the specified org code and service ID.
@@ -25,4 +40,7 @@ class RoutingAndReliability:
         :param service_id:
         :return:
         """
-        pass
+        endpoint_details = await self.lookup.retrieve_mhs_attributes(org_code, service_id)
+
+        reliability = {item: endpoint_details[item] for item in RELIABILITY_KEYS}
+        return reliability
