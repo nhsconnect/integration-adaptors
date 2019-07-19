1a05ca67ce458db657542e4dc6053843a8088b77
Philip-Woods <49308422+Philip-Woods@users.noreply.github.com>
2019-07-19 17:12:34 +0100

    Feature/RT75-Logger (#35)
    * Added logging module

---
+++ common/utilities/integration_adaptors_logger.py
@@ -0,0 +1,101 @@
+import logging
+import sys
+import time
+
+AUDIT = 25
+
+
+# Set the logging info globally, make each module get a new logger based on that log ref we provide
+def configure_logging():
+    """
+    A general method to load the overall config of the system, specifically it modifies the root handler to output
+    to stdout and sets the default log levels and format. This is expected to be called once at the start of a
+    application.
+    """
+    logging.addLevelName(AUDIT, "AUDIT")
+    logger = logging.getLogger()
+    logger.setLevel(logging.NOTSET)
+    handler = logging.StreamHandler(sys.stdout)
+    logging.Formatter.converter = time.gmtime
+    formatter = logging.Formatter('[%(asctime)s.%(msecs)03dZ] '
+                                  '%(message)s pid=%(process)d LogLevel=%(levelname)s ',
+                                  '%Y-%m-%dT%H:%M:%S')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+
+class IntegrationAdaptorsLogger:
+
+    def __init__(self, log_ref: str):
+        """
+        :param log_ref: Used as part of the process key, this is a base log code to identify which module
+        each log originates from
+        """
+        if not log_ref:
+            raise ValueError('Undefined log reference')
+        self.process_key_tag = log_ref
+        self.logger = logging.getLogger(log_ref)
+
+    def _format_and_write(self, message, values, process_key_num, request_id, correlation_id, level):
+        """
+        Formats the string and appends the appropriate values if they are included before writing the log
+        """
+        if not process_key_num:
+            raise ValueError('process_key_num not defined')
+
+        message = self._formatted_string(message, values)
+
+        if request_id:
+            message += f' RequestId={request_id}'
+        if correlation_id:
+            message += f' CorrelationId={correlation_id}'
+
+        message += f' ProcessKey={self.process_key_tag + process_key_num}'
+
+        self.logger.log(level, message)
+
+    def info(self, process_key_num: str, message: str, values: dict = None,
+             request_id: str = None, correlation_id=None):
+        values = values if values is not None else {}
+        self._format_and_write(message, values, process_key_num, request_id, correlation_id, logging.INFO)
+
+    def audit(self, process_key_num: str, message: str, values: dict = None,
+              request_id: str = None, correlation_id=None):
+        values = values if values is not None else {}
+        self._format_and_write(message, values, process_key_num, request_id, correlation_id, AUDIT)
+
+    def warning(self, process_key_num: str, message: str, values: dict = None,
+                request_id: str = None, correlation_id=None):
+        values = values if values is not None else {}
+        self._format_and_write(message, values, process_key_num, request_id, correlation_id, logging.WARNING)
+
+    def error(self, process_key_num: str, message: str, values: dict = None,
+              request_id: str = None, correlation_id=None):
+        values = values if values is not None else {}
+        self._format_and_write(message, values, process_key_num, request_id, correlation_id, logging.ERROR)
+
+    def critical(self, process_key_num: str, message: str, values: dict = None,
+                 request_id: str = None, correlation_id=None):
+        values = values if values is not None else {}
+        self._format_and_write(message, values, process_key_num, request_id, correlation_id, logging.CRITICAL)
+
+    def _format_values_in_map(self, dict_values: dict) -> dict:
+        """
+        Replaces the values in the map with key=value so that the key in a string can be replaced with the correct
+        log format, also surrounds the value with quotes if it contains spaces and removes spaces from the key
+        """
+        new_map = {}
+        for key, value in dict_values.items():
+            if ' ' in value:
+                value = f'"{value}"'
+
+            new_map[key] = f"{key.replace(' ', '')}={value}"
+        return new_map
+
+    def _formatted_string(self, message: str, dict_values: dict) -> str:
+        """
+        Populates the string with the correctly formatted dictionary values
+        """
+        formatted_values = self._format_values_in_map(dict_values)
+        return message.format(**formatted_values)
+
---
+++ common/utilities/tests/test_logger.py
@@ -0,0 +1,142 @@
+import io
+import os
+import logging
+from unittest import TestCase
+from unittest.mock import patch, MagicMock
+from utilities import integration_adaptors_logger as log
+
+
+class TestLogger(TestCase):
+
+    def tearDown(self) -> None:
+        logging.getLogger().handlers = []
+
+    def test_dictionary_formatting(self):
+        # Tests both removing the spaces and surrounding values with quotes if needed
+        input_dict = {
+            'Key With Space': 'value with space',  # Needs quotes
+            'EasyKey': 'EasyValue',  # Doesn't need quotes
+        }
+
+        expected_output = {
+            'Key With Space': 'KeyWithSpace="value with space"',
+            'EasyKey': 'EasyKey=EasyValue'
+        }
+        output = log.IntegrationAdaptorsLogger('SYS')._format_values_in_map(input_dict)
+        self.assertEqual(output, expected_output)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_custom_audit_level(self, mock_stdout):
+        log.configure_logging()
+        log.IntegrationAdaptorsLogger('TES') \
+            .audit('100', '{There Will Be No Spaces Today}',
+                   {'There Will Be No Spaces Today': 'wow qwe'}, correlation_id=2)
+
+        output = mock_stdout.getvalue()
+        self.assertIn('CorrelationId=2', output)
+        self.assertIn('ThereWillBeNoSpacesToday="wow qwe"', output)
+        self.assertIn('LogLevel=AUDIT', output)
+        self.assertIn('ProcessKey=TES100', output)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_format_and_write(self, mock_std):
+        log.configure_logging()
+        log.IntegrationAdaptorsLogger('SYS')._format_and_write(
+            message='{yes} {no} {maybe}',
+            values={'yes': 'one', 'no': 'two', 'maybe': 'three'},
+            request_id=10,
+            correlation_id=5,
+            level=logging.INFO,
+            process_key_num='100'
+        )
+
+        output = mock_std.getvalue()
+
+        # Check that each value has spaces
+        self.assertIn(' CorrelationId=5 ', output)
+        self.assertIn(' LogLevel=INFO ', output)
+        self.assertIn(' ProcessKey=SYS100', output)
+        self.assertIn(' RequestId=10 ', output)
+        self.assertIn(' maybe=three ', output)
+        self.assertIn(' yes=one ', output)
+        self.assertIn(f' pid={os.getpid()}', output)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_format_and_write_empty_vals(self, mock_std):
+        log.configure_logging()
+        log.IntegrationAdaptorsLogger('SYS')._format_and_write(
+            message='{yes} {no} {maybe}',
+            values={'yes': 'one', 'no': 'two', 'maybe': 'three'},
+            request_id=None,
+            correlation_id=None,
+            level=logging.INFO,
+            process_key_num='100'
+        )
+
+        output = mock_std.getvalue()
+        self.assertIn(' LogLevel=INFO ', output)
+        self.assertIn(' ProcessKey=SYS100', output)
+        self.assertIn(' maybe=three ', output)
+        self.assertIn(f' pid={os.getpid()}', output)
+        self.assertIn(' yes=one ', output)
+        self.assertNotIn('CorrelationId=', output)
+        self.assertNotIn('RequestId=', output)
+
+    def test_log_levels(self):
+        logger = log.IntegrationAdaptorsLogger('SYS')
+        logger._format_and_write = MagicMock()
+        with self.subTest('INFO'):
+            logger.info('100', '{yes}', {'yes': 'no'}, 'REQ', 313)
+            logger._format_and_write.assert_called_with('{yes}', {'yes': 'no'}, '100', 'REQ', 313, logging.INFO)
+
+        with self.subTest('AUDIT'):
+            logger.audit('100', '{yes}', {'yes': 'no'}, 'REQ', 313)
+            logger._format_and_write.assert_called_with('{yes}', {'yes': 'no'}, '100', 'REQ', 313, log.AUDIT)
+
+        with self.subTest('WARNING'):
+            logger.warning('100', '{yes}', {'yes': 'no'}, 'REQ', 313)
+            logger._format_and_write.assert_called_with('{yes}', {'yes': 'no'}, '100', 'REQ', 313, logging.WARNING)
+
+        with self.subTest('ERROR'):
+            logger.error('100', '{yes}', {'yes': 'no'}, 'REQ', 313)
+            logger._format_and_write.assert_called_with('{yes}', {'yes': 'no'}, '100', 'REQ', 313, logging.ERROR)
+
+        with self.subTest('CRITICAL'):
+            logger.critical('100', '{yes}', {'yes': 'no'}, 'REQ', 313)
+            logger._format_and_write.assert_called_with('{yes}', {'yes': 'no'}, '100', 'REQ', 313, logging.CRITICAL)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_empty_values(self, mock_stdout):
+        log.configure_logging()
+        logger = log.IntegrationAdaptorsLogger('SYS')
+        with self.subTest('Empty info log'):
+            logger.info('100', 'I can still log info strings without values!')
+            output = mock_stdout.getvalue()
+            self.assertIn('I can still log info strings without values!', output)
+        with self.subTest('Empty audit log'):
+            logger.audit('100', 'I can still log audit strings without values!')
+            output = mock_stdout.getvalue()
+            self.assertIn('I can still log audit strings without values!', output)
+        with self.subTest('Empty warning log'):
+            logger.warning('100', 'I can still log warning strings without values!')
+            output = mock_stdout.getvalue()
+            self.assertIn('I can still log warning strings without values!', output)
+        with self.subTest('Empty error log'):
+            logger.error('100', 'I can still log error strings without values!')
+            output = mock_stdout.getvalue()
+            self.assertIn('I can still log error strings without values!', output)
+        with self.subTest('Empty Critical log'):
+            logger.critical('100', 'I can still log critical strings without values!')
+            output = mock_stdout.getvalue()
+            self.assertIn('I can still log critical strings without values!', output)
+
+    def test_write_throws_error_on_bad_params(self):
+        with self.assertRaises(ValueError):
+            log.IntegrationAdaptorsLogger('SYS')._format_and_write("message", {}, "", None, None, logging.INFO)
+
+        with self.assertRaises(ValueError):
+            log.IntegrationAdaptorsLogger('SYS')._format_and_write("message", {}, None, None, None, logging.INFO)
+
+    def test_undefined_log_ref_throws_error(self):
+        with self.assertRaises(ValueError):
+            log.IntegrationAdaptorsLogger('')
