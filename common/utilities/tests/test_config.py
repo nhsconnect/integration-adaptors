76dee0fc1902788f1ac63661f18d1f9bfa917cdc
Ismail S <ismail-s@users.noreply.github.com>
2019-07-22 14:43:31 +0100

    RT-79 use patch.dict

--- common/utilities/tests/test_config.py
+++ common/utilities/tests/test_config.py
@@ -4,11 +4,11 @@ from unittest.mock import patch
 import utilities.config as config
 
 
-@patch("utilities.config.config", new_callable=dict)
+@patch.dict(config.config)
 @patch("os.environ", new_callable=dict)
 class TestConfig(unittest.TestCase):
 
-    def test_setup_config_populates_config(self, mock_environ, _unused):
+    def test_setup_config_populates_config(self, mock_environ):
         mock_environ["PREFIX_TEST"] = "123"
         mock_environ["PREFIX_LOG_LEVEL"] = "INFO"
 
@@ -18,7 +18,7 @@ class TestConfig(unittest.TestCase):
 
         self.assertEqual({"TEST": "123", "LOG_LEVEL": "INFO"}, config.config)
 
-    def test_setup_config_filters_by_prefix(self, mock_environ, _unused):
+    def test_setup_config_filters_by_prefix(self, mock_environ):
         mock_environ["SOME_OTHER_CONFIG"] = "BLAH"
 
         config.setup_config("PREFIX")
