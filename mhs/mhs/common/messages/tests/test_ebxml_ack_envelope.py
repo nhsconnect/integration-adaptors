43bedd7b3f4851e5a65926c00d4d96540ef9e85d
Ismail S <ismail-s@users.noreply.github.com>
2019-07-25 15:42:31 +0100

    RT-93 add more tests for message generation

--- mhs/mhs/common/messages/tests/test_ebxml_ack_envelope.py
+++ mhs/mhs/common/messages/tests/test_ebxml_ack_envelope.py
@@ -1,33 +1,37 @@
 from unittest.mock import patch
 
+import mhs.common.messages.ebxml_ack_envelope as ebxml_ack_envelope
+import mhs.common.messages.ebxml_envelope as ebxml_envelope
+import mhs.common.messages.tests.test_ebxml_envelope as test_ebxml_envelope
+from builder.pystache_message_builder import MessageGenerationSOAPFaultError
 from utilities.file_utilities import FileUtilities
 from utilities.message_utilities import MessageUtilities
 from utilities.xml_utilities import XmlUtilities
 
-import mhs.common.messages.ebxml_ack_envelope as ebxml_ack_envelope
-import mhs.common.messages.ebxml_envelope as ebxml_envelope
-import mhs.common.messages.tests.test_ebxml_envelope as test_ebxml_envelope
-
 EXPECTED_EBXML = "ebxml_ack.xml"
+
+
+def get_test_message_dictionary():
+    return {
+        ebxml_envelope.FROM_PARTY_ID: "TESTGEN-201324",
+        ebxml_envelope.TO_PARTY_ID: "YEA-0000806",
+        ebxml_envelope.CPA_ID: "S1001A1630",
+        ebxml_envelope.CONVERSATION_ID: "79F49A34-9798-404C-AEC4-FD38DD81C138",
+        ebxml_ack_envelope.RECEIVED_MESSAGE_TIMESTAMP: "2013-04-16T07:52:09Z",
+        ebxml_ack_envelope.RECEIVED_MESSAGE_ID: "0CDBA95F-74DA-47E9-8383-7B8E9167D146",
+    }
 
 
 class TestEbXmlAckEnvelope(test_ebxml_envelope.TestEbxmlEnvelope):
 
     @patch.object(MessageUtilities, "get_timestamp")
     @patch.object(MessageUtilities, "get_uuid")
-    def test_build_message(self, mock_get_uuid, mock_get_timestamp):
+    def test_serialize(self, mock_get_uuid, mock_get_timestamp):
         mock_get_uuid.return_value = test_ebxml_envelope.MOCK_UUID
-        mock_get_timestamp.return_value = "2012-03-15T06:51:08Z"
+        mock_get_timestamp.return_value = test_ebxml_envelope.MOCK_TIMESTAMP
         expected_message = FileUtilities.get_file_string(str(self.expected_message_dir / EXPECTED_EBXML))
 
-        envelope = ebxml_ack_envelope.EbxmlAckEnvelope({
-            ebxml_envelope.FROM_PARTY_ID: "TESTGEN-201324",
-            ebxml_envelope.TO_PARTY_ID: "YEA-0000806",
-            ebxml_envelope.CPA_ID: "S1001A1630",
-            ebxml_envelope.CONVERSATION_ID: "79F49A34-9798-404C-AEC4-FD38DD81C138",
-            ebxml_ack_envelope.RECEIVED_MESSAGE_TIMESTAMP: "2013-04-16T07:52:09Z",
-            ebxml_ack_envelope.RECEIVED_MESSAGE_ID: "0CDBA95F-74DA-47E9-8383-7B8E9167D146",
-        })
+        envelope = ebxml_ack_envelope.EbxmlAckEnvelope(get_test_message_dictionary())
 
         message_id, message = envelope.serialize()
 
@@ -36,6 +40,21 @@ class TestEbXmlAckEnvelope(test_ebxml_envelope.TestEbxmlEnvelope):
 
         self.assertEqual(test_ebxml_envelope.MOCK_UUID, message_id)
         XmlUtilities.assert_xml_equal(expected_message_bytes, message_bytes)
+
+    @patch.object(MessageUtilities, "get_timestamp")
+    @patch.object(MessageUtilities, "get_uuid")
+    def test_serialize_required_tags(self, mock_get_uuid, mock_get_timestamp):
+        mock_get_uuid.return_value = test_ebxml_envelope.MOCK_UUID
+        mock_get_timestamp.return_value = test_ebxml_envelope.MOCK_TIMESTAMP
+
+        for required_tag in get_test_message_dictionary().keys():
+            with self.subTest(required_tag=required_tag):
+                test_message_dict = get_test_message_dictionary()
+                del test_message_dict[required_tag]
+                envelope = ebxml_ack_envelope.EbxmlAckEnvelope(test_message_dict)
+
+                with self.assertRaises(MessageGenerationSOAPFaultError):
+                    envelope.serialize()
 
     def test_from_string(self):
         message = FileUtilities.get_file_string(str(self.message_dir / "ebxml_header.xml"))
