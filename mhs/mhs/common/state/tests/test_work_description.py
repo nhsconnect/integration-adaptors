762395f92dda15f3932cdbf84dc70801b7d8b3a8
Philip Woods <philip.woods@bjss.com>
2019-08-02 00:05:19 +0100

    Added work description tests

--- mhs/mhs/common/state/tests/test_work_description.py
+++ mhs/mhs/common/state/tests/test_work_description.py
@@ -1,10 +1,110 @@
-from unittest import TestCase
+import json
+import unittest
+from unittest.mock import MagicMock, patch
 
 import mhs.common.state.work_description as wd
 
 
-class TestWorkDescription(TestCase):
+class TestWorkDescription(unittest.TestCase):
 
-    def test_update(self):
-        work = wd.WorkDescription()
-        work.update()
+    def test_constructor(self):
+        input = {
+            wd.DATA_KEY: 'aaa-aaa-aaa',
+            wd.DATA: {
+                wd.VERSION_KEY: 1,
+                wd.TIMESTAMP: '12:00',
+                wd.STATUS: wd.MessageStatus.IN_OUTBOUND_WORKFLOW
+            }
+        }
+
+        persistence = MagicMock()
+        work_description = wd.WorkDescription(persistence, input)
+
+        self.assertEqual(work_description.status, wd.MessageStatus.IN_OUTBOUND_WORKFLOW)
+        self.assertEqual(work_description.version, 1)
+        self.assertEqual(work_description.timestamp, '12:00')
+
+    def test_publish_updates(self):
+        input = {
+            wd.DATA_KEY: 'aaa-aaa-aaa',
+            wd.DATA: {
+                wd.TIMESTAMP: '12:00',
+                wd.VERSION_KEY: 1,
+                wd.STATUS: wd.MessageStatus.IN_OUTBOUND_WORKFLOW.value
+            }
+        }
+
+        old_value = {
+            wd.DATA_KEY: 'aaa-aaa-aaa',
+            wd.DATA: {
+                wd.VERSION_KEY: 1,
+                wd.TIMESTAMP: '11:00',
+                wd.STATUS: wd.MessageStatus.IN_OUTBOUND_WORKFLOW.value
+            }
+        }
+
+        persistence = MagicMock()
+        persistence.get.return_value = old_value
+        work_description = wd.WorkDescription(persistence, input)
+
+        work_description.publish()
+        persistence.add.assert_called_with('default_table', json.dumps(input))
+
+    def test_out_of_date_version(self):
+        input = {
+            wd.DATA_KEY: 'aaa-aaa-aaa',
+            wd.DATA: {
+                wd.TIMESTAMP: '12:00',
+                wd.VERSION_KEY: 1,
+                wd.STATUS: wd.MessageStatus.IN_OUTBOUND_WORKFLOW.value
+            }
+        }
+
+        old_value = {
+            wd.DATA_KEY: 'aaa-aaa-aaa',
+            wd.DATA: {
+                wd.VERSION_KEY: 3,
+                wd.TIMESTAMP: '11:00',
+                wd.STATUS: wd.MessageStatus.IN_OUTBOUND_WORKFLOW.value
+            }
+        }
+
+        persistence = MagicMock()
+        persistence.get.return_value = old_value
+        work_description = wd.WorkDescription(persistence, input)
+
+        with self.assertRaises(wd.OutOfDateVersionError):
+            work_description.publish()
+
+    def test_null_persistence(self):
+        with self.assertRaises(ValueError):
+            wd.WorkDescription(None, {'None': 'None'})
+
+
+class TestWorkDescriptionFactory(unittest.TestCase):
+
+    @patch('mhs.common.state.work_description.WorkDescription')
+    def test_get_from_store(self, work_mock):
+        old_value = {
+            wd.DATA_KEY: 'aaa-aaa-aaa',
+            wd.DATA: {
+                wd.VERSION_KEY: 3,
+                wd.TIMESTAMP: '11:00',
+                wd.STATUS: wd.MessageStatus.IN_OUTBOUND_WORKFLOW.value
+            }
+        }
+
+        persistence = MagicMock()
+        persistence.get.return_value = old_value
+        wd.WorkDescriptionFactory.get_work_description_from_store(persistence, 'aaa-aaa-aaa')
+
+        persistence.get.assert_called_with('default_table', 'aaa-aaa-aaa')
+        work_mock.assert_called_with(persistence, old_value)
+
+    def test_get_from_store_empty_store(self):
+        with self.assertRaises(ValueError):
+            wd.WorkDescriptionFactory.get_work_description_from_store(None, 'aaa')
+
+    def test_get_from_store_empty_message_id(self):
+        with self.assertRaises(ValueError):
+            wd.WorkDescriptionFactory.get_work_description_from_store(MagicMock(), None)
--- mhs/mhs/common/state/work_description.py
+++ mhs/mhs/common/state/work_description.py
@@ -1,30 +1,92 @@
-# from typing import S
 import enum
 import json
+import  utilities.integration_adaptors_logger as log
 
+
+logger = log.IntegrationAdaptorsLogger('STATE_MANAGER')
 
 class MessageStatus(enum.Enum):
     RECEIVED = 1
     STARTED = 2
     IN_OUTBOUND_WORKFLOW = 3
-    
 
-class WorkDescription(dict):
 
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+DATA_KEY = 'MESSAGE_KEY'
+VERSION_KEY = 'VERSION'
+TIMESTAMP = 'TIMESTAMP'
+DATA = 'DATA'
+STATUS = 'STATUS'
+
+
+class OutOfDateVersionError(RuntimeError):
+    pass
+
+
+class WorkDescription:
+
+    def __init__(self, persistence_store, store_data: dict):
+        if persistence_store is None:
+            raise ValueError('Expected persistence store')
+        
+        self.persistence_store = persistence_store
+
+        data = store_data['DATA']
+        self.message_key = store_data[DATA_KEY]
+        self.version = data[VERSION_KEY]
+        self.timestamp = data[TIMESTAMP]
+        self.status = data['STATUS']
 
     def publish(self):
-        x = json.loads(self.items())
-        print(x)
+        latest_data = self.persistence_store.get('default_table', self.message_key)
+        if latest_data is not None:
+            latest_version = latest_data[DATA][VERSION_KEY]
+            if latest_version > self.version:
+                raise OutOfDateVersionError(f'Failed to update message {self.message_key}: local version out of date ')
+
+        serialised = self._serialise_data()
+
+        old_data = self.persistence_store.add('default_table', serialised)
+        return old_data
+
+    def _serialise_data(self):
+        data = {
+            DATA_KEY: self.message_key,
+            'DATA': {
+                TIMESTAMP: self.timestamp,
+                VERSION_KEY: self.version,
+                STATUS: self.status
+            }
+        }
+
+        return json.dumps(data)
 
 
 class WorkDescriptionFactory:
 
     @staticmethod
     def get_work_description_from_store(persistence_store, message_id):
+        if persistence_store is None:
+            raise ValueError('Expected non-null persistence store')
+        if message_id is None:
+            raise ValueError('Expected non-null message_id')
         json_store_data = persistence_store.get('default_table', message_id)
+        return WorkDescription(persistence_store, json_store_data)
 
+    @staticmethod
+    def create_new_work_description(message_id: str,
+                                    timestamp: str,
+                                    status: MessageStatus):
+        work_description_map = {
+            DATA_KEY: message_id,
+            "DATA": {
+                TIMESTAMP: timestamp,
+                'STATUS': status,
+                VERSION_KEY: 1
+            }
+        }
+        
+        return WorkDescription(work_description_map)
+        
 
 
 
