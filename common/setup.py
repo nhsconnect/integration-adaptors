399488a895d3ba080bbd276364a63e95f4ad2bb7
Guy Flavell <Guy.Flavell@bjss.com>
2019-08-05 16:20:48 +0100

    Ensured pipfile works correctly.

---
+++ common/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+import setuptools
+
+setup(
+    name='integration-adaptors-common',
+    version='',
+    packages=setuptools.find_packages(),
+    url='',
+    license='',
+    author='Gareth Allan',
+    author_email='',
+    description='Common utilities used by the NHS integration adaptors projects.',
+    install_requires=[
+        'pystache',
+        'lxml'
+    ]
+)
