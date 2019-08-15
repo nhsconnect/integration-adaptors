2554767ab970059901f97f2412010ec0964fe4d8
Philip Woods <philip.woods@bjss.com>
2019-08-15 15:48:54 +0100

    Removed unused imports

--- mhs/common/mhs_common/workflow/common_asynchronous.py
+++ mhs/common/mhs_common/workflow/common_asynchronous.py
@@ -1,8 +1,6 @@
 """This module defines the common base for all asynchronous workflows."""
 
 import mhs_common.workflow.common as common
-import abc
-from mhs_common.state import work_description as wd
 
 
 class CommonAsynchronousWorkflow(common.CommonWorkflow):
