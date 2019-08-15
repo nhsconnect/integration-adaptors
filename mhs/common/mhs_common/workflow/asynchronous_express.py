a05f473a74d9c4e29fb8231f91a714d4ac30bae6
Ismail S <ismail-s@users.noreply.github.com>
2019-08-15 16:22:07 +0100

    Implement async-express outbound workflow

--- mhs/common/mhs_common/workflow/asynchronous_express.py
+++ mhs/common/mhs_common/workflow/asynchronous_express.py
@@ -1,9 +1,12 @@
 """This module defines the asynchronous express workflow."""
 from typing import Tuple
 
+import requests
 import utilities.integration_adaptors_logger as log
 from comms import transmission_adaptor
+from utilities import timing
 
+from mhs_common.messages import ebxml_request_envelope, ebxml_envelope
 from mhs_common.state import persistence_adaptor, work_description
 from mhs_common.workflow import common_asynchronous
 
@@ -22,3 +25,48 @@ class AsynchronousExpressWorkflow(common_asynchronous.CommonAsynchronousWorkflow
     async def handle_outbound_message(self, message_id: str, correlation_id: str, interaction_details: dict,
                                       payload: str) -> Tuple[int, str]:
         logger.info('0001', 'Entered async express workflow to handle outbound message')
+        wdo = work_description.create_new_work_description(self.persistence_store, message_id,
+                                                           work_description.MessageStatus.OUTBOUND_MESSAGE_RECEIVED)
+        await wdo.publish()
+
+        try:
+            # Call message packer to serialise message
+            interaction_details[ebxml_envelope.MESSAGE_ID] = message_id
+            interaction_details[ebxml_request_envelope.MESSAGE] = payload
+            interaction_details[ebxml_envelope.FROM_PARTY_ID] = self.party_key
+            interaction_details[ebxml_envelope.CONVERSATION_ID] = correlation_id
+            _, http_headers, message = ebxml_request_envelope.EbxmlRequestEnvelope(interaction_details).serialize()
+        except Exception as e:
+            logger.warning('0002', 'Failed to serialise outbound message. {Exception}', {'Exception': e})
+            await wdo.set_status(work_description.MessageStatus.OUTBOUND_MESSAGE_PREPARATION_FAILED)
+            return 500, 'Error serialising outbound message'
+        else:
+            logger.info('0003', 'Message serialised successfully')
+            await wdo.set_status(work_description.MessageStatus.OUTBOUND_MESSAGE_PREPARED)
+
+        try:
+            logger.info('0004', 'About to make outbound request')
+            start_time = timing.get_time()
+            response = await self.transmission.make_request(interaction_details, message)
+            end_time = timing.get_time()
+        except requests.exceptions.HTTPError as e:
+            logger.warning('0005', 'Received HTTP error from Spine. {HTTPStatus} {Exception}',
+                           {'HTTPStatus': e.response.http_status, 'Exception': e})
+            await wdo.set_status(work_description.MessageStatus.OUTBOUND_MESSAGE_NACKD)
+            return 500, 'Error received from Spine'
+        except Exception as e:
+            logger.warning('0006', 'Error encountered whilst making outbound request. {Exception}', {'Exception': e})
+            await wdo.set_status(work_description.MessageStatus.OUTBOUND_MESSAGE_TRANSMISSION_FAILED)
+            return 500, 'Error making outbound request'
+
+        if response.status_code == 202:
+            logger.audit('0007', 'Async-express workflow invoked. Message sent to Spine and acknowledgment received. '
+                                 '{RequestSentTime} {AcknowledgmentReceivedTime}',
+                         {'RequestSentTime': start_time, 'AcknowledgmentReceivedTime': end_time})
+            await wdo.set_status(work_description.MessageStatus.OUTBOUND_MESSAGE_ACKD)
+            return 202, ''
+        else:
+            logger.warning('0008', "Didn't get expected HTTP status 202 from Spine, got {HTTPStatus} instead",
+                           {'HTTPStatus': response.status_code})
+            await wdo.set_status(work_description.MessageStatus.OUTBOUND_MESSAGE_NACKD)
+            return 500, "Didn't get expected success response from Spine"
--- mhs/common/mhs_common/workflow/tests/test_asynchronous_express.py
+++ mhs/common/mhs_common/workflow/tests/test_asynchronous_express.py
@@ -1,10 +1,153 @@
+import asyncio
 import unittest
 from unittest import mock
 
+import requests
+from utilities import test_utilities
+from utilities.test_utilities import async_test
+
 import mhs_common.workflow.asynchronous_express as async_express
+from mhs_common.messages import ebxml_request_envelope, ebxml_envelope
+from mhs_common.state import work_description
+from mhs_common.state.work_description import MessageStatus
+
+PARTY_KEY = 'party-key'
+MESSAGE_ID = 'message-id'
+CORRELATION_ID = 'correlation-id'
+INTERACTION_DETAILS = {'workflow': 'async-express'}
+PAYLOAD = 'payload'
+SERIALIZED_MESSAGE = 'serialized-message'
 
 
 class TestAsynchronousExpressWorkflow(unittest.TestCase):
     def setUp(self):
         self.mock_persistence_store = mock.MagicMock()
-        self.workflow = async_express.AsynchronousExpressWorkflow(self.mock_persistence_store)
+        self.mock_transmission_adaptor = mock.MagicMock()
+
+        patcher = mock.patch.object(work_description, 'create_new_work_description')
+        self.mock_create_new_work_description = patcher.start()
+        self.addCleanup(patcher.stop)
+
+        patcher = mock.patch.object(ebxml_request_envelope, 'EbxmlRequestEnvelope')
+        self.mock_ebxml_request_envelope = patcher.start()
+        self.addCleanup(patcher.stop)
+
+        self.workflow = async_express.AsynchronousExpressWorkflow(self.mock_persistence_store,
+                                                                  self.mock_transmission_adaptor, PARTY_KEY)
+
+    def tearDown(self):
+        self.mock_create_new_work_description.assert_called_once_with(self.mock_persistence_store, MESSAGE_ID,
+                                                                      MessageStatus.OUTBOUND_MESSAGE_RECEIVED)
+        self.mock_ebxml_request_envelope.assert_called_once()
+
+    @async_test
+    async def test_handle_outbound_message(self):
+        response = mock.MagicMock()
+        response.status_code = 202
+
+        self.setup_mock_work_description()
+
+        self.mock_ebxml_request_envelope.return_value.serialize.return_value = (MESSAGE_ID, {}, SERIALIZED_MESSAGE)
+        self.mock_transmission_adaptor.make_request.return_value = test_utilities.awaitable(response)
+
+        expected_interaction_details = {ebxml_envelope.MESSAGE_ID: MESSAGE_ID, ebxml_request_envelope.MESSAGE: PAYLOAD,
+                                        ebxml_envelope.FROM_PARTY_ID: PARTY_KEY,
+                                        ebxml_envelope.CONVERSATION_ID: CORRELATION_ID}
+        expected_interaction_details.update(INTERACTION_DETAILS)
+
+        status, message = await self.workflow.handle_outbound_message(MESSAGE_ID, CORRELATION_ID, INTERACTION_DETAILS,
+                                                                      PAYLOAD)
+
+        self.assertEqual(202, status)
+        self.assertEqual('', message)
+        self.mock_work_description.publish.assert_called_once()
+        self.assertEqual(
+            [mock.call(MessageStatus.OUTBOUND_MESSAGE_PREPARED), mock.call(MessageStatus.OUTBOUND_MESSAGE_ACKD)],
+            self.mock_work_description.set_status.call_args_list)
+        self.mock_ebxml_request_envelope.assert_called_once_with(expected_interaction_details)
+        self.mock_transmission_adaptor.make_request.assert_called_once_with(expected_interaction_details,
+                                                                            SERIALIZED_MESSAGE)
+
+    @async_test
+    async def test_handle_outbound_message_serialisation_fails(self):
+        self.setup_mock_work_description()
+
+        self.mock_ebxml_request_envelope.return_value.serialize.side_effect = Exception()
+
+        status, message = await self.workflow.handle_outbound_message(MESSAGE_ID, CORRELATION_ID, INTERACTION_DETAILS,
+                                                                      PAYLOAD)
+
+        self.assertEqual(500, status)
+        self.assertEqual('Error serialising outbound message', message)
+        self.mock_work_description.publish.assert_called_once()
+        self.assertEqual([mock.call(MessageStatus.OUTBOUND_MESSAGE_PREPARATION_FAILED)],
+                         self.mock_work_description.set_status.call_args_list)
+        self.mock_transmission_adaptor.make_request.assert_not_called()
+
+    @async_test
+    async def test_handle_outbound_message_http_error_when_calling_outbound_transmission(self):
+        self.setup_mock_work_description()
+
+        self.mock_ebxml_request_envelope.return_value.serialize.return_value = (MESSAGE_ID, {}, SERIALIZED_MESSAGE)
+
+        response = mock.MagicMock()
+        response.status_code = 409
+        future = asyncio.Future()
+        future.set_exception(requests.exceptions.HTTPError(response=response))
+        self.mock_transmission_adaptor.make_request.return_value = future
+
+        status, message = await self.workflow.handle_outbound_message(MESSAGE_ID, CORRELATION_ID, INTERACTION_DETAILS,
+                                                                      PAYLOAD)
+
+        self.assertEqual(500, status)
+        self.assertEqual('Error received from Spine', message)
+        self.mock_work_description.publish.assert_called_once()
+        self.assertEqual(
+            [mock.call(MessageStatus.OUTBOUND_MESSAGE_PREPARED), mock.call(MessageStatus.OUTBOUND_MESSAGE_NACKD)],
+            self.mock_work_description.set_status.call_args_list)
+
+    @async_test
+    async def test_handle_outbound_message_error_when_calling_outbound_transmission(self):
+        self.setup_mock_work_description()
+
+        self.mock_ebxml_request_envelope.return_value.serialize.return_value = (MESSAGE_ID, {}, SERIALIZED_MESSAGE)
+
+        future = asyncio.Future()
+        future.set_exception(Exception())
+        self.mock_transmission_adaptor.make_request.return_value = future
+
+        status, message = await self.workflow.handle_outbound_message(MESSAGE_ID, CORRELATION_ID, INTERACTION_DETAILS,
+                                                                      PAYLOAD)
+
+        self.assertEqual(500, status)
+        self.assertEqual('Error making outbound request', message)
+        self.mock_work_description.publish.assert_called_once()
+        self.assertEqual(
+            [mock.call(MessageStatus.OUTBOUND_MESSAGE_PREPARED),
+             mock.call(MessageStatus.OUTBOUND_MESSAGE_TRANSMISSION_FAILED)],
+            self.mock_work_description.set_status.call_args_list)
+
+    @async_test
+    async def test_handle_outbound_message_non_http_202_success_response_received(self):
+        self.setup_mock_work_description()
+
+        self.mock_ebxml_request_envelope.return_value.serialize.return_value = (MESSAGE_ID, {}, SERIALIZED_MESSAGE)
+
+        response = mock.MagicMock()
+        response.status_code = 200
+        self.mock_transmission_adaptor.make_request.return_value = test_utilities.awaitable(response)
+
+        status, message = await self.workflow.handle_outbound_message(MESSAGE_ID, CORRELATION_ID, INTERACTION_DETAILS,
+                                                                      PAYLOAD)
+
+        self.assertEqual(500, status)
+        self.assertEqual("Didn't get expected success response from Spine", message)
+        self.mock_work_description.publish.assert_called_once()
+        self.assertEqual(
+            [mock.call(MessageStatus.OUTBOUND_MESSAGE_PREPARED), mock.call(MessageStatus.OUTBOUND_MESSAGE_NACKD)],
+            self.mock_work_description.set_status.call_args_list)
+
+    def setup_mock_work_description(self):
+        self.mock_work_description = self.mock_create_new_work_description.return_value
+        self.mock_work_description.publish.return_value = test_utilities.awaitable(None)
+        self.mock_work_description.set_status.return_value = test_utilities.awaitable(None)
