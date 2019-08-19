747aacbc913b2c07c9b1c28bdac37e59a0fa14c9
Philip Smith <Philip.Smith@bjss.com>
2019-08-19 11:49:45 +0100

    minor fix

--- integration-tests/integration_tests/test_scenarios/int_mhs_tests.py
+++ integration-tests/integration_tests/test_scenarios/int_mhs_tests.py
@@ -12,14 +12,14 @@ class FunctionalTest(TestCase):
     def test_scr_happy_path(self):
         mhs_response = methods.get_interaction_from_template('gp_summary_upload',
                                                              'REPC_IN150016UK05',
-                                                             '9446245796',
+                                                             '9689177869',
                                                              'Happy path test')
         self.assertTrue(methods.check_response(mhs_response), "Happy path test failed")
 
     def test_scr_passing_in_message_id(self):
         mhs_response = methods.get_interaction_from_template('gp_summary_upload',
                                                              'REPC_IN150016UK05',
-                                                             '9446245796',
+                                                             '9689177869',
                                                              'test with message id',
                                                              pass_message_id=True)
         self.assertTrue(methods.check_response(mhs_response),
--- integration-tests/integration_tests/test_scenarios/int_scr_adaptor_tests.py
+++ integration-tests/integration_tests/test_scenarios/int_scr_adaptor_tests.py
@@ -12,7 +12,7 @@ class FunctionalTest(TestCase):
 
     # request scr record for patient 9446245796, using json template (json_hash16UK05)
     def test_json_conversion(self):
-        scr_json, uuid = methods.get_json('hash16UK05', 9446245796, 'json message test')
+        scr_json, uuid = methods.get_json('hash16UK05', 9689177869, 'json message test')
 
         # send this to the adaptor to 'convert' it to HL7
         scr_response = interactions.call_scr_adaptor(scr_json)
