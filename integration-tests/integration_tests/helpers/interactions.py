ecacf78e844943fb15347f14884c882626de05e1
Philip Smith <Philip.Smith@bjss.com>
2019-08-19 11:15:00 +0100

    minor refactoring

--- integration-tests/integration_tests/helpers/interactions.py
+++ integration-tests/integration_tests/helpers/interactions.py
@@ -1,11 +1,12 @@
+import requests
 
-from integration_tests.helpers.build_scr import build_message
-from integration_tests.helpers.callmhs import call_mhs
+from integration_tests.helpers import methods
+from integration_tests.helpers.build_message import build_message
 
 
 def process_request(interaction_name, template, asid, nhs_number, human_readable, pass_message_id):
-    """
-    Renders the template and passes it the the MHS
+    """ Renders the template and passes it the the MHS
+
     :param interaction_name: the type of message
     :param template: the template to use
     :param asid: the asid, as supplied from NHS digital
@@ -14,27 +15,49 @@ def process_request(interaction_name, template, asid, nhs_number, human_readable
     :param pass_message_id: flag to indicate if we need to pass on the message ID
     :return: response received from the MHS
     """
+    scr, message_id = build_message(template, asid, nhs_number, human_readable)
+    if not pass_message_id:
+        message_id = None
 
-    # This will be expanded to accommodate other interactions...
-    if interaction_name.lower() == 'gp_summary_upload':
-        scr, message_id = build_message(template, asid, nhs_number, human_readable)
-        if not pass_message_id:
-            message_id = None
-
-        return process_message(interaction_name, scr, message_id)
-    else:
-        return 'Unknown MHS Adaptor Command'
+    return call_mhs(interaction_name, scr, message_id)
 
 
-def process_message(interaction_name, message, message_id=None):
+def call_mhs(mhs_command, hl7payload, message_id=None):
+    """Call the MHS with the provided details.
+
+    :param mhs_command: The command/interaction name to call the MHS with.
+    :param hl7payload: The HL7 payload to send to the MHS.
+    :param message_id: The message id to optionally pass as a query param
+    :return: The response returned by the MHS.
     """
-    Sends the message to the the MHS
-    :param interaction_name: the type of message
-    :param message: the HL7 formatted mesaage to be sent to the MHS
-    :param pass_message_id: flag to indicate if we need to pass on the message ID
-    :return: response received from the MHS
+    headers = {'Interaction-Id': mhs_command}
+    if message_id is not None:
+        headers['Message-Id'] = message_id
+
+    response = requests.post(methods.get_mhs_hostname() + mhs_command, headers=headers, data=hl7payload)
+    # response = requests.post(methods.get_mhs_hostname(), headers=headers, data=hl7payload)
+    return response.text
+
+
+def call_scr_adaptor(json_string):
+    """ Call the SCR adaptor with the provided json string
+
+    :param json_string: the json to be sent
+    :return: the response from the adaptor
     """
-    if message_id is None:
-        return call_mhs(interaction_name, message)
-    else:
-        return call_mhs(interaction_name, message, message_id=message_id)
+
+    # TODO
+    # once we know the scr_adaptor end point, we need to send the json_string to the scr_adaptor
+    # scr_adaptor_response = requests.post(methods.get_scr_adaptor_hostname() + json_string)
+    # return scr_adaptor_response.text
+
+    # meanwhile, build an example response...
+    # TEMPLATE_XML = 'COPC_IN000001UK01'
+    # TEMPLATE_XML = 'QUPA_IN040000UK32'
+    # TEMPLATE_XML = 'QUPC_IN160101UK05'
+    # TEMPLATE_XML = 'REPC_IN150016UK05'
+    scr_adaptor_response, message_id = build_message('REPC_IN150016UK05',
+                                                     methods.get_asid(),
+                                                     '9446245796',
+                                                     'json message test')
+    return scr_adaptor_response
--- integration-tests/integration_tests/helpers/methods.py
+++ integration-tests/integration_tests/helpers/methods.py
@@ -4,10 +4,8 @@ from pathlib import Path
 from utilities.file_utilities import FileUtilities
 
 from integration_tests.helpers import interactions, xml_parser
-from integration_tests.helpers.build_scr import build_message
+from integration_tests.helpers.build_message import build_message
 from test_definitions import ROOT_DIR
-
-HUMAN_READABLE = 'Payload stuff'
 
 
 def get_asid():
@@ -28,18 +26,29 @@ def get_asid():
     return os.environ.get('ASID', os.environ.get('ASID', asid))
 
 
-def get_hostname():
+def get_mhs_hostname():
     """
-    Looks up the hostname from the environment settings
-    :return: the hostname
+    Looks up the mhs hostname from the environment settings
+    :return: the mhs hostname
 
-    The hostname should be set in the 'Environment variables' section of the Run/Debug Configurations
+    The mhs hostname should be set in the 'Environment variables' section of the Run/Debug Configurations
         if this is not set, it will default to 'localhost'
     """
     return "http://" + os.environ.get('MHS_ADDRESS', 'localhost') + "/"
 
 
-def get_interaction_from_template(interaction_name, template, nhs_number, pass_message_id=False):
+def get_scr_adaptor_hostname():
+    """
+    Looks up the scr adaptor hostname from the environment settings
+    :return: the scr adaptor hostname
+
+    The scr adaptor hostname should be set in the 'Environment variables' section of the Run/Debug Configurations
+        if this is not set, it will default to 'localhost'
+    """
+    return "http://" + os.environ.get('SCR_ADAPTOR_ADDRESS', 'localhost') + "/"
+
+
+def get_interaction_from_template(interaction_name, template, nhs_number, payload, pass_message_id=False):
     """
     Sends the template to bo rendered and passed on to the the MHS
     :param interaction_name: the type of message
@@ -48,48 +57,18 @@ def get_interaction_from_template(interaction_name, template, nhs_number, pass_m
     :param pass_message_id: flag to indicate if we need to pass on the message ID
     :return: the response from the MHS
     """
-    return interactions.process_request(interaction_name, template, get_asid(), nhs_number, HUMAN_READABLE,
+    return interactions.process_request(interaction_name, template, get_asid(), nhs_number, payload,
                                         pass_message_id=pass_message_id)
 
 
-def get_interaction_from_message(interaction_name, message):
-    """
-    Sends the message to bo rendered and passed on to the the MHS
-    :param interaction_name: the type of message
-    :param message: the message to be sent
-    :return: the response from the MHS
-    """
-    return interactions.process_message(interaction_name, message)
-
-
-
-def get_json(template, patient_nhs_number):
+def get_json(template, patient_nhs_number, payload):
     """
     renders the template
     :param template: the template to use
     :param patient_nhs_number: the NHS number of the test patient
     :return: populated template xml string
     """
-    return build_message(template, get_asid(), patient_nhs_number, HUMAN_READABLE)
-
-
-def call_scr_adaptor(json_string):
-    """
-    Sends a json string the the SCR adaptor
-    :param json_string: the json to be sent
-    :return: the response from the adaptor
-    """
-
-    # TODO
-    # once we know the scr_adaptor end point, we need to send the json_string to the scr_adaptor
-        # return SummaryCareRecord().populate_template_with_json_string(json_string)
-    # meanwhile, build an example response...
-    # TEMPLATE_XML = 'xml_COPC_IN000001UK01'
-    # TEMPLATE_XML = 'xml_QUPA_IN040000UK32'
-    # TEMPLATE_XML = 'xml_QUPC_IN160101UK05'
-    # TEMPLATE_XML = 'xml_REPC_IN150016UK05'
-    scr_adaptor_response, message_id = build_message('xml_REPC_IN150016UK05', get_asid(), '9446245796', HUMAN_READABLE)
-    return scr_adaptor_response
+    return build_message(template, get_asid(), patient_nhs_number, payload)
 
 
 def check_response(returned_xml):
