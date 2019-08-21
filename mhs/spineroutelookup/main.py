13405db890e0794591b2d0f558e2065b16e66821
Gareth Allan <gareth.allan@bjss.com>
2019-08-21 14:45:05 +0100

    RT-123: Made spineroutelookup application start a Tornado server

--- mhs/spineroutelookup/main.py
+++ mhs/spineroutelookup/main.py
@@ -1,13 +1,37 @@
-
+import tornado.httpserver
+import tornado.ioloop
+import tornado.web
+from utilities import config
 from utilities import integration_adaptors_logger as log
 
-logger = log.IntegrationAdaptorsLogger('SCR_MAIN')
+from request import handler
+
+logger = log.IntegrationAdaptorsLogger('SPINE_ROUTE_LOOKUP_MAIN')
+
+
+def start_tornado_server() -> None:
+    """
+    Start the Tornado server
+    """
+    application = tornado.web.Application([(".*", handler.RoutingDirectoryRequestHandler)])
+    server = tornado.httpserver.HTTPServer(application)
+    server.listen(443)
+
+    logger.info('003', 'Starting server')
+    tornado.ioloop.IOLoop.current().start()
 
 
 def main():
-    logger.info('001', 'This SDS server is yet to be implemented')
-    logger.info('002', 'Exiting application...')
-    
+    config.setup_config("MHS")
+    log.configure_logging()
+
+    start_tornado_server()
+
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except Exception as e:
+        logger.critical('001', 'Fatal exception in main application: {exception}', {'exception': e})
+    finally:
+        logger.info('002', 'Exiting application')
