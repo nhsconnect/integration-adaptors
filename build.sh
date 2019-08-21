53848b0f647228b24f6ffa88d3626813942360c8
Gareth Allan <gareth.allan@bjss.com>
2019-08-21 15:37:21 +0100

    RT-123: Added packer file for spineroutelookup container

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 packer build -except publish pipeline/packer/inbound.json
 packer build -except publish pipeline/packer/outbound.json
-
+packer build -except publish pipeline/packer/spineroutelookup.json
