6177a94b682f7bda99ccbcd25529807854dd89ab
Gareth Allan <gareth.allan@bjss.com>
2019-08-29 12:46:51 +0100

    RT-179: Added security groups to test env terraform

---
+++ pipeline/terraform/integration-test-environment/security-groups.tf
@@ -0,0 +1,120 @@
+resource "aws_security_group" "mhs_outbound_security_group" {
+  name = "MHS Outbound Security Group"
+  description = "The security group used to control traffic for the MHS Outbound component."
+  vpc_id = aws_vpc.mhs_vpc.id
+
+  tags = {
+    Name = "${var.build_id}-mhs-outbound-sg"
+    BuildId = var.build_id
+  }
+}
+
+resource "aws_security_group" "mhs_route_security_group" {
+  name = "MHS Route Security Group"
+  description = "The security group used to control traffic for the MHS Routing component."
+  vpc_id = aws_vpc.mhs_vpc.id
+
+  tags = {
+    Name = "${var.build_id}-mhs-route-sg"
+    BuildId = var.build_id
+  }
+}
+
+resource "aws_security_group" "mhs_inbound_security_group" {
+  name = "MHS Inbound Security Group"
+  description = "The security group used to control traffic for the MHS Inbound component."
+  vpc_id = aws_vpc.mhs_vpc.id
+
+  tags = {
+    Name = "${var.build_id}-mhs-inbound-sg"
+    BuildId = var.build_id
+  }
+}
+
+resource "aws_security_group" "state_db_security_group" {
+  name = "State Database Security Group"
+  description = "The security group used to control traffic for the state database endpoint."
+  vpc_id = aws_vpc.mhs_vpc.id
+
+  tags = {
+    Name = "${var.build_id}-state-db-sg"
+    BuildId = var.build_id
+  }
+}
+
+resource "aws_security_group" "sync_async_db_security_group" {
+  name = "Sync-Async Database Security Group"
+  description = "The security group used to control traffic for the sync-async database endpoint."
+  vpc_id = aws_vpc.mhs_vpc.id
+
+  tags = {
+    Name = "${var.build_id}-sync-async-db-sg"
+    BuildId = var.build_id
+  }
+}
+
+resource "aws_security_group" "sds_cache_security_group" {
+  name = "SDS Cache Security Group"
+  description = "The security group used to control traffic for the SDS cache endpoint."
+  vpc_id = aws_vpc.mhs_vpc.id
+
+  tags = {
+    Name = "${var.build_id}-sds-cache-sg"
+    BuildId = var.build_id
+  }
+}
+
+resource "aws_security_group" "ig_security_group" {
+  name = "Internet Gateway Security Group"
+  description = "The security group used to control traffic for the Internet Gateway."
+  vpc_id = aws_vpc.mhs_vpc.id
+
+  tags = {
+    Name = "${var.build_id}-ig-sg"
+    BuildId = var.build_id
+  }
+}
+
+resource "aws_security_group" "alb_outbound_security_group" {
+  name = "Outbound ALB Security Group"
+  description = "The security group used to control traffic for the outbound MHS Application Load Balancer."
+  vpc_id = aws_vpc.mhs_vpc.id
+
+  tags = {
+    Name = "${var.build_id}-alb-outbound-sg"
+    BuildId = var.build_id
+  }
+}
+
+resource "aws_security_group" "alb_route_security_group" {
+  name = "Route ALB Security Group"
+  description = "The security group used to control traffic for the MHS routing component Application Load Balancer."
+  vpc_id = aws_vpc.mhs_vpc.id
+
+  tags = {
+    Name = "${var.build_id}-alb-route-sg"
+    BuildId = var.build_id
+  }
+}
+
+resource "aws_security_group" "alb_inbound_security_group" {
+  name = "Inbound ALB Security Group"
+  description = "The security group used to control traffic for the inbound MHS Application Load Balancer."
+  vpc_id = aws_vpc.mhs_vpc.id
+
+  tags = {
+    Name = "${var.build_id}-alb-inbound-sg"
+    BuildId = var.build_id
+  }
+}
+
+resource "aws_security_group" "async_response_queue_security_group" {
+  name = "Async Response Queue Security Group"
+  description = "The security group used to control traffic for the asynchronous response queue endpoint."
+  vpc_id = aws_vpc.mhs_vpc.id
+
+  tags = {
+    Name = "${var.build_id}-async-response-queue-sg"
+    BuildId = var.build_id
+  }
+}
