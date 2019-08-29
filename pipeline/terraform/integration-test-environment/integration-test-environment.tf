255a9fad5c266e90b47a5aee7036b4ce1eb2e5a9
Gareth Allan <gareth.allan@bjss.com>
2019-08-29 14:18:52 +0100

    RT-179: Made terraform create a subnet in each AZ in the region

--- pipeline/terraform/integration-test-environment/integration-test-environment.tf
+++ pipeline/terraform/integration-test-environment/integration-test-environment.tf
@@ -3,11 +3,29 @@ provider "aws" {
   region = var.region
 }
 
+data "aws_availability_zones" "all" {}
+
+
 resource "aws_vpc" "mhs_vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
     Name = "${var.build_id}-vpc"
+    BuildId = var.build_id
+  }
+}
+
+# Create a subnet in each availability zone in the region.
+resource "aws_subnet" "mhs_subnet" {
+  count = length(data.aws_availability_zones.all.names)
+
+  vpc_id = aws_vpc.mhs_vpc.id
+  availability_zone = data.aws_availability_zones.all.names[count.index]
+  cidr_block = "10.0.${count.index}.0/24"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "${var.build_id}-mhs-subnet-${count.index}"
     BuildId = var.build_id
   }
 }
