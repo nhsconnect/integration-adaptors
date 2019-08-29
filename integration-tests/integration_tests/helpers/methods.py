585127b8ad98c7e2f0720ba7b264b763bcdd9328
Philip Woods <philip.woods@bjss.com>
2019-08-29 13:58:14 +0100

    added debug for queue addres

--- integration-tests/integration_tests/helpers/methods.py
+++ integration-tests/integration_tests/helpers/methods.py
@@ -31,9 +31,9 @@ def get_mhs_inbound_queue():
     The mhs inbound hostname should be set in the 'Environment variables' section of the Run/Debug Configurations
         if this is not set, it will default to 'localhost:5672'
     """
-    
-    return os.environ.get('INBOUND_QUEUE_HOST', 'http://localhost:5672/')
-
+    x = os.environ.get('INBOUND_QUEUE_HOST', 'http://localhost:5672/')
+    print(x)
+    return x
 
 def get_mhs_hostname():
     """ Looks up the mhs hostname from the environment settings
