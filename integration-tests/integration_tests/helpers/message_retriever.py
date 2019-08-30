8503214a49d724a3f83182fe805f9688bbbfde28
Philip Smith <Philip.Smith@bjss.com>
2019-08-30 17:24:42 +0100

    method template for statedb

--- integration-tests/integration_tests/helpers/message_retriever.py
+++ integration-tests/integration_tests/helpers/message_retriever.py
@@ -19,11 +19,14 @@ def get_inbound_response():
     return message.properties['message-id'], message.properties['correlation-id'], message.body
 
 
+# TODO RT-202
 def get_message_state(message_id):
     """ Retrieve the message state from the statedb
 
     :param message_id:
     :return: the message state
     """
+    # A reference to dynamo_persistence_adaptor needs to be added, so we can re-use existing code
+    # See Phil Woods if further help needed
     # store = dynamo_persistence_adaptor.DynamoPersistenceAdaptor(table_name=config.get_config('STATE_TABLE_NAME'))
     # store.get(message_id)
