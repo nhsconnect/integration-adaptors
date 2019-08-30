e33fa9c55bee217a4dfe7fa568133eae0e69d762
Phil Smith <52321684+PhilSmithBJSS@users.noreply.github.com>
2019-08-30 16:50:50 +0100

    Feature/rt 156 async express int tests (#71)
    * Add framework for scr adaptor tests
    
    * add mocked mha response
    
    * minor fixes
    
    * create new integration tests module
    
    * pass the template name from the test
    
    * add more templates and add comments
    
    * minor refactoring
    
    * minor fix
    
    * Add framework for scr adaptor tests
    
    * add mocked mha response
    
    * minor fixes
    
    * create new integration tests module
    
    * pass the template name from the test
    
    * add more templates and add comments
    
    * minor refactoring
    
    * minor fix
    
    * update interaction-id
    
    * add async express tests
    
    * add integration tests to Jenkins
    
    * add code for empty response
    
    * fix
    
    * remove error handling
    
    * Add framework for scr adaptor tests
    
    * Add framework for scr adaptor tests
    
    * add mocked mha response
    
    * add mocked mha response
    
    * minor fixes
    
    * create new integration tests module
    
    * pass the template name from the test
    
    * add more templates and add comments
    
    * minor refactoring
    
    * minor fix
    
    * minor fixes
    
    * create new integration tests module
    
    * pass the template name from the test
    
    * add more templates and add comments
    
    * minor refactoring
    
    * add integration tests to Jenkins
    
    * add async express tests
    
    * add docstrings plus a new scenario
    
    * post merge fix
    
    * accept inbound msg to remove from queue
    
    * Updated inbound environment variables
    
    * Updated env variable to match jenkins
    
    * Updated queue address method
    
    * Removed old tests
    
    * added debug for queue addres
    
    * Added ssl check test
    
    * Update docker file for packages
    
    * Updated jenkins to use correct env vars
    
    * Corrected test env variable
    
    * Added proton ssl packages to inbound service
    
    * Added certs to the inbound queue reading
    
    * fix asid file location
    
    * remove asid.txt
    
    * exclude asid.txt from future commits
    
    * review changes

--- integration-tests/integration_tests/helpers/xml_parser.py
+++ integration-tests/integration_tests/helpers/xml_parser.py
@@ -8,15 +8,20 @@ class XmlMessageParser:
     """A component that extracts information from XML messages."""
 
     def parse_message(self, message):
-        """Parse the provided XML message and extract a dictionary of values from it.
+        """ Parse the provided XML message and extract a dictionary of values from it.
 
         :param message: The message to be parsed.
         :return: the parsed message.
         """
-
         return et.fromstring(message)
 
     def path_to_hl7xml_element(self, name, parent=None):
+        """ Build the X-Path for the element
+
+        :param name: the name ot the element
+        :param parent: name of the elements parent
+        :return: the X-Path
+        """
         path = ".//"
 
         if parent is not None:
@@ -26,15 +31,30 @@ class XmlMessageParser:
 
         return path
 
-    def extract_hl7xml_value(self, xml_tree, element_name, parent=None):
+    def extract_hl7xml_section(self, xml_tree, element_name, parent=None):
+        """ Extract a section of the XML
+
+        :param xml_tree: the parsed XML
+        :param element_name: the name ot the element
+        :param parent: name of the elements parent
+        :return: the extracted section
+        """
         xpath = self.path_to_hl7xml_element(element_name, parent=parent)
         return xml_tree.find(xpath, namespaces=NAMESPACES)
 
-    def extract_hl7xml_text_value(self, xml_tree, element_name, parent=None):
-        value = self.extract_hl7xml_value(xml_tree, element_name, parent)
+    def extract_hl7xml_text_value(self, xml_tree, attribute, element_name, parent=None):
+        """ Extract the value of an attribute
+
+        :param xml_tree: the parsed XML
+        :param attribute: the name of the attribute
+        :param element_name: the name ot the element
+        :param parent: name of the elements parent
+        :return: the extracted value
+        """
+        value = self.extract_hl7xml_section(xml_tree, element_name, parent)
         text = None
 
         if value is not None:
-            text = value.text
+            text = value.attrib[attribute]
 
         return text
--- integration-tests/integration_tests/test_scenarios/int_scr_adaptor_tests.py
+++ integration-tests/integration_tests/test_scenarios/int_scr_adaptor_tests.py
@@ -1,4 +1,4 @@
-from unittest import TestCase
+from unittest import TestCase, skip
 
 from integration_tests.helpers import interactions, methods
 
@@ -6,6 +6,7 @@ from integration_tests.helpers import interactions, methods
 class FunctionalTest(TestCase):
 
     # request scr record for patient 9446245796, using json template (json_hash16UK05)
+    @skip('waiting for RT-12 - Implement MHS Async Reliable Message Pattern')
     def test_json_conversion(self):
         scr_json, uuid = methods.get_json('hash16UK05', 9689177869, 'json message test')
 
@@ -13,7 +14,8 @@ class FunctionalTest(TestCase):
         scr_response = interactions.call_scr_adaptor(scr_json)
 
         # now send the scr_response to the mhs
-        mhs_response = interactions.call_mhs('gp_summary_upload', scr_response)
+        mhs_response = interactions.call_mhs('gp_summary_upload', scr_response, None, False, None, False)
 
         # then validate the mhs-response
-        self.assertTrue(methods.check_response(mhs_response), "Unsuccessful response from Spine")
+        self.assertTrue(methods.check_response(mhs_response, 'requestSuccessDetail'),
+                        "Unsuccessful response from Spine")
