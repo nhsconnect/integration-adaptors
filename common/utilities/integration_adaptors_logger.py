18713c0519f082a62ead17ae8e96438ecf45ccf3
zameerparvez <parvez.zameer@gmail.com>
2019-08-30 15:07:47 +0100

    Feature/rt 157 common logging package enhancements (#70)
    * RT-157 Log tracebacks with IntegrationAdaptorsLogger
    
    * RT-157 Update so that errors are only logged when the message contains an error field
    
    * RT-157 Update some log statements and log traceback for any logs including any capitalisations of EXCEPTION as a key
    
    * RT-157 Refactor for pull request comments

--- common/utilities/integration_adaptors_logger.py
+++ common/utilities/integration_adaptors_logger.py
@@ -2,6 +2,7 @@ import contextvars
 import datetime as dt
 import logging
 import sys
+import traceback
 
 from utilities import config
 
@@ -36,7 +37,7 @@ def configure_logging():
     handler = logging.StreamHandler(sys.stdout)
 
     formatter = _CustomFormatter(
-        fmt='[%(asctime)sZ] %(message)s pid=%(process)d LogLevel=%(levelname)s ',
+        fmt='[%(asctime)sZ] %(message)s pid=%(process)d LogLevel=%(levelname)s LoggerName=%(name)s',
         datefmt='%Y-%m-%dT%H:%M:%S.%f')
 
     handler.setFormatter(formatter)
@@ -45,6 +46,9 @@ def configure_logging():
 
 
 class IntegrationAdaptorsLogger:
+    """
+    To log exceptions correctly, key them with 'exception'
+    """
 
     def __init__(self, log_ref: str):
         """
@@ -71,7 +75,6 @@ class IntegrationAdaptorsLogger:
             message += f' CorrelationId={correlation_id.get()}'
         if inbound_message_id.get():
             message += f' InboundMessageId={inbound_message_id.get()}'
-
 
         message += f' ProcessKey={self.process_key_tag + process_key_num}'
 
@@ -102,6 +105,12 @@ class IntegrationAdaptorsLogger:
         Replaces the values in the map with key=value so that the key in a string can be replaced with the correct
         log format, also surrounds the value with quotes if it contains spaces and removes spaces from the key
         """
+
+        for key in dict_values.keys():
+            if str(key).lower() == "exception":
+                dict_values[key] = self._format_exception(str(dict_values[key]))
+                break
+
         new_map = {}
         for key, value in dict_values.items():
             value = str(value)
@@ -117,3 +126,16 @@ class IntegrationAdaptorsLogger:
         """
         formatted_values = self._format_values_in_map(dict_values)
         return message.format(**formatted_values)
+
+    @staticmethod
+    def _format_exception(message: str) -> str:
+        """
+        Returns a formatted string of the last exception on the stack
+        """
+        exc_inf = sys.exc_info()
+        if all(exc_inf):
+            (etype, value, trace) = exc_inf
+            if message:
+                return f'{message}, {traceback.format_exception(etype, value, trace)}'
+            else:
+                return f'{traceback.format_exception(etype, value, trace)}'
