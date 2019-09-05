d9d31d4fe0ab650ce9ee855df67679925b32aa1c
Colin Bateson <colin.bateson@bjss.com>
2019-09-05 10:24:40 +0100

    WIP tests - ignored

--- integration-tests/integration_tests/test_scenarios/int_async_reliable_tests.py
+++ integration-tests/integration_tests/test_scenarios/int_async_reliable_tests.py
@@ -1,66 +1,69 @@
-from unittest import TestCase
+from unittest import TestCase, skip
 
-from integration_tests.helpers import methods, message_retriever
+import integration_tests.helpers
 
 
 class FunctionalTest(TestCase):
-
-    def test_async_express_outbound_status(self):
+    def test_async_reliable_outbound_status(self):
         # the response is just an acknowledgement from spine...
-        outbound_response, _, _ = methods.get_interaction_from_template('async express',
-                                                                        'QUPC_IN160101UK05',
+        outbound_response, _, _ = integration_tests.helpers.methods.get_interaction_from_template('async reliable',
+                                                                        'REPC_IN150016UK05',
                                                                         '9689177621',
-                                                                        'Asynchronous Express test')
+                                                                        'Asynchronous Reliable test')
         # we need to 'accept' the message in the queue, so it is removed and doesn't impact on subsequent tests
-        message_retriever.get_inbound_response()
-        self.assertTrue(methods.check_status_code(outbound_response, 202),
-                        "Async Express outbound test failed")
+        integration_tests.helpers.message_retriever.get_inbound_response()
+        self.assertTrue(integration_tests.helpers.methods.check_status_code(outbound_response, 202),
+                        "Async Reliable outbound test failed")
 
-    def test_async_express_inbound_message_id(self):
+    @skip('DO NOT CHECK-IN!!!!!!')
+    def test_async_reliable_inbound_message_id(self):
         # send the message
-        outbound_response, sent_message_id, _ = methods.get_interaction_from_template('async express',
-                                                                                      'QUPC_IN160101UK05',
+        outbound_response, sent_message_id, _ = integration_tests.helpers.methods.get_interaction_from_template('async reliable',
+                                                                                      'REPC_IN150016UK05',
                                                                                       '9689177699',
-                                                                                      'Asynchronous Express test',
-                                                                                      pass_message_id=True,
-                                                                                      pass_correlation_id=False)
+                                                                                      'Asynchronous Reliable test',
+                                                                                                                pass_message_id=True,
+                                                                                                                pass_correlation_id=False)
         # then retrieve the message Id for the response (from the queue)...
-        received_message_id, _, _ = message_retriever.get_inbound_response()
+        received_message_id, _, _ = integration_tests.helpers.message_retriever.get_inbound_response()
         self.assertEqual(received_message_id, sent_message_id,
-                         "Async Express inbound message Id test failed")
+                         "Async Reliable inbound message Id test failed")
 
-    def test_async_express_inbound_correlation_id(self):
+    @skip('DO NOT CHECK-IN!!!!!!')
+    def test_async_reliable_inbound_correlation_id(self):
         # send the message
-        outbound_response, _, sent_correlation_id = methods.get_interaction_from_template('async express',
-                                                                                          'QUPC_IN160101UK05',
+        outbound_response, _, sent_correlation_id = integration_tests.helpers.methods.get_interaction_from_template('async reliable',
+                                                                                          'REPC_IN150016UK05',
                                                                                           '9689177710',
-                                                                                          'Asynchronous Express test',
-                                                                                          pass_message_id=False,
-                                                                                          pass_correlation_id=True)
+                                                                                          'Asynchronous Reliable test',
+                                                                                                                    pass_message_id=False,
+                                                                                                                    pass_correlation_id=True)
         # then retrieve the correlation Id for the response (from the queue)...
-        _, received_correlation_id, _ = message_retriever.get_inbound_response()
+        _, received_correlation_id, _ = integration_tests.helpers.message_retriever.get_inbound_response()
         self.assertEqual(received_correlation_id, sent_correlation_id,
-                         "Async Express inbound correlation Id test failed")
+                         "Async Reliable inbound correlation Id test failed")
 
-    def test_async_express_inbound_response(self):
+    @skip('DO NOT CHECK-IN!!!!!!')
+    def test_async_reliable_inbound_response(self):
         # send the message
-        methods.get_interaction_from_template('async express',
-                                              'QUPC_IN160101UK05',
+        integration_tests.helpers.methods.get_interaction_from_template('async reliable',
+                                              'REPC_IN150016UK05',
                                               '9689177869',
-                                              'Asynchronous Express test')
+                                              'Asynchronous Reliable test')
         # then retrieve the response from the queue...
-        _, _, inbound_response = message_retriever.get_inbound_response()
-        self.assertTrue(methods.check_response(inbound_response, 'queryResponseCode'),
-                        "Async Express inbound response test failed")
+        _, _, inbound_response = integration_tests.helpers.message_retriever.get_inbound_response()
+        self.assertTrue(integration_tests.helpers.methods.check_response(inbound_response, 'queryResponseCode'),
+                        "Async Reliable inbound response test failed")
 
-    def test_async_express_inbound_patient(self):
+    @skip('DO NOT CHECK-IN!!!!!!')
+    def test_async_reliable_inbound_patient(self):
         # send the message
-        methods.get_interaction_from_template('async express',
-                                              'QUPC_IN160101UK05',
+        integration_tests.helpers.methods.get_interaction_from_template('async reliable',
+                                              'REPC_IN150016UK05',
                                               '9689177923',
-                                              'Asynchronous Express test')
+                                              'Asynchronous Reliable test')
         # then retrieve the response from the queue...
-        _, _, inbound_response = message_retriever.get_inbound_response()
-        patient_number = methods.get_section(inbound_response, 'extension', 'id', 'patient')
+        _, _, inbound_response = integration_tests.helpers.message_retriever.get_inbound_response()
+        patient_number = integration_tests.helpers.methods.get_section(inbound_response, 'extension', 'id', 'patient')
         self.assertEqual(patient_number, '9689177923',
-                         "Async Express inbound response test failed")
+                         "Async Reliable inbound response test failed")
