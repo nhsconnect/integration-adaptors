867519efb338e7ea10de97d3e848d89573873ee7
Ismail S <ismail-s@users.noreply.github.com>
2019-09-05 11:47:24 +0100

    RT-179 add routes between VPCs

--- pipeline/terraform/integration-test-environment/vpc-connections.tf
+++ pipeline/terraform/integration-test-environment/vpc-connections.tf
@@ -1,4 +1,6 @@
-# data "aws_caller_identity" "current" {}
+data "aws_vpc" "supplier_vpc" {
+  id = var.supplier_vpc_id
+}
 
 resource "aws_vpc_peering_connection" "supplier_peering_connection" {
   peer_vpc_id = var.supplier_vpc_id
@@ -19,6 +21,16 @@ resource "aws_vpc_peering_connection" "supplier_peering_connection" {
   }
 }
 
+resource "aws_route" "supplier_to_mhs_route" {
+  route_table_id = data.aws_vpc.supplier_vpc.main_route_table_id
+  destination_cidr_block = aws_vpc.mhs_vpc.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.supplier_peering_connection.id
+}
+
+data "aws_vpc" "opentest_vpc" {
+  id = var.opentest_vpc_id
+}
+
 resource "aws_vpc_peering_connection" "opentest_peering_connection" {
   peer_vpc_id = var.opentest_vpc_id
   vpc_id = aws_vpc.mhs_vpc.id
@@ -36,4 +48,16 @@ resource "aws_vpc_peering_connection" "opentest_peering_connection" {
     Name = "${var.environment_id}-mhs-opentest-peering-connection"
     EnvironmentId = var.environment_id
   }
+}
+
+resource "aws_route" "mhs_to_opentest_route" {
+  route_table_id = aws_vpc.mhs_vpc.main_route_table_id
+  destination_cidr_block = data.aws_vpc.opentest_vpc.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.opentest_peering_connection.id
+}
+
+resource "aws_route" "opentest_to_mhs_route" {
+  route_table_id = data.aws_vpc.opentest_vpc.main_route_table_id
+  destination_cidr_block = aws_vpc.mhs_vpc.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.opentest_peering_connection.id
 }
