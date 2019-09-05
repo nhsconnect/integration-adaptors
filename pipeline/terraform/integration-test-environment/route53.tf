d84ab6ced47f26a90368ba58c18996722e184f21
Ismail S <ismail-s@users.noreply.github.com>
2019-09-05 16:38:04 +0100

    RT-179 fix route53 hosted zone config

--- pipeline/terraform/integration-test-environment/route53.tf
+++ pipeline/terraform/integration-test-environment/route53.tf
@@ -1,5 +1,12 @@
 resource "aws_route53_zone" "mhs_hosted_zone" {
   name = "${var.environment_id}.${var.internal_root_domain}"
+  vpc {
+    # Note that if a change is made to vpc_id here, then the lifecycle block below
+    # may need to be deleted in order for this to be picked up. But care must be
+    # taken when doing this, as this can conflict with other
+    # aws_route53_zone_association blocks
+    vpc_id = aws_vpc.mhs_vpc.id
+  }
 
   tags = {
     Name = "${var.environment_id}-mhs-hosted-zone"
@@ -7,16 +14,11 @@ resource "aws_route53_zone" "mhs_hosted_zone" {
   }
 
   lifecycle {
-    # Ignore changes to vpc_id as this is managed by aws_route53_zone_association resources
+    # Ignore changes to vpc_id as this is managed by aws_route53_zone_association resources.
     ignore_changes = [
-      vpc_id
+      vpc
     ]
   }
-}
-
-resource "aws_route53_zone_association" "mhs_hosted_zone_mhs_vpc_association" {
-  zone_id = aws_route53_zone.mhs_hosted_zone.zone_id
-  vpc_id  = aws_vpc.mhs_vpc.id
 }
 
 resource "aws_route53_record" "mhs_outbound_load_balancer_record" {
