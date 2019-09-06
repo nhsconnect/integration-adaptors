e509e84f5ce417bc7d3252b3c799ac864b8ecd39
Ismail S <ismail-s@users.noreply.github.com>
2019-09-06 11:22:25 +0100

    RT-179 reformat file

--- pipeline/terraform/integration-test-environment/route53.tf
+++ pipeline/terraform/integration-test-environment/route53.tf
@@ -9,7 +9,7 @@ resource "aws_route53_zone" "mhs_hosted_zone" {
   }
 
   tags = {
-    Name = "${var.environment_id}-mhs-hosted-zone"
+    Name          = "${var.environment_id}-mhs-hosted-zone"
     EnvironmentId = var.environment_id
   }
 
@@ -23,51 +23,51 @@ resource "aws_route53_zone" "mhs_hosted_zone" {
 
 resource "aws_route53_record" "mhs_outbound_load_balancer_record" {
   zone_id = aws_route53_zone.mhs_hosted_zone.zone_id
-  name = "mhs-outbound.${aws_route53_zone.mhs_hosted_zone.name}"
-  type = "A"
+  name    = "mhs-outbound.${aws_route53_zone.mhs_hosted_zone.name}"
+  type    = "A"
 
   alias {
-    name = aws_lb.outbound_alb.dns_name
-    zone_id = aws_lb.outbound_alb.zone_id
+    name                   = aws_lb.outbound_alb.dns_name
+    zone_id                = aws_lb.outbound_alb.zone_id
     evaluate_target_health = false
   }
 }
 
 output "outbound_lb_domain_name" {
-  value = aws_route53_record.mhs_outbound_load_balancer_record.name
+  value       = aws_route53_record.mhs_outbound_load_balancer_record.name
   description = "The DNS name of the Route53 record pointing to the MHS outbound service's load balancer."
 }
 
 resource "aws_route53_record" "mhs_route_load_balancer_record" {
   zone_id = aws_route53_zone.mhs_hosted_zone.zone_id
-  name = "mhs-route.${aws_route53_zone.mhs_hosted_zone.name}"
-  type = "A"
+  name    = "mhs-route.${aws_route53_zone.mhs_hosted_zone.name}"
+  type    = "A"
 
   alias {
-    name = aws_lb.route_alb.dns_name
-    zone_id = aws_lb.route_alb.zone_id
+    name                   = aws_lb.route_alb.dns_name
+    zone_id                = aws_lb.route_alb.zone_id
     evaluate_target_health = false
   }
 }
 
 output "route_lb_domain_name" {
-  value = aws_route53_record.mhs_route_load_balancer_record.name
+  value       = aws_route53_record.mhs_route_load_balancer_record.name
   description = "The DNS name of the Route53 record pointing to the MHS Spine route lookup service's load balancer."
 }
 
 resource "aws_route53_record" "mhs_inbound_load_balancer_record" {
   zone_id = aws_route53_zone.mhs_hosted_zone.zone_id
-  name = "mhs-inbound.${aws_route53_zone.mhs_hosted_zone.name}"
-  type = "A"
+  name    = "mhs-inbound.${aws_route53_zone.mhs_hosted_zone.name}"
+  type    = "A"
 
   alias {
-    name = aws_lb.inbound_nlb.dns_name
+    name                   = aws_lb.inbound_nlb.dns_name
     evaluate_target_health = false
-    zone_id = aws_lb.inbound_nlb.zone_id
+    zone_id                = aws_lb.inbound_nlb.zone_id
   }
 }
 
 output "inbound_lb_domain_name" {
-  value = aws_route53_record.mhs_inbound_load_balancer_record.name
+  value       = aws_route53_record.mhs_inbound_load_balancer_record.name
   description = "The DNS name of the Route53 record pointing to the MHS inbound service's load balancer."
 }
