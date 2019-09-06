b75d8c995e1aae3d27ec027f4c1f2917c8c90e19
Gareth Allan <gareth.allan@bjss.com>
2019-09-06 11:27:33 +0100

    Removed unused internet GW from terraform

--- pipeline/terraform/integration-test-environment/integration-test-environment.tf
+++ pipeline/terraform/integration-test-environment/integration-test-environment.tf
@@ -35,12 +35,3 @@ resource "aws_subnet" "mhs_subnet" {
     EnvironmentId = var.environment_id
   }
 }
-
-resource "aws_internet_gateway" "mhs_igw" {
-  vpc_id = aws_vpc.mhs_vpc.id
-
-  tags = {
-    Name = "${var.environment_id}-mhs-igw"
-    EnvironmentId = var.environment_id
-  }
-}
