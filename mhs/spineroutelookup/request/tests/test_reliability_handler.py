3fdef88401a25684dc2eb71e28e4f2dadca63ca9
Gareth Allan <gareth.allan@bjss.com>
2019-09-09 13:41:44 +0100

    RT-123: Added tests for SRL request handler error handling

--- mhs/spineroutelookup/request/tests/test_reliability_handler.py
+++ mhs/spineroutelookup/request/tests/test_reliability_handler.py
@@ -1,32 +1,30 @@
 import json
-import unittest.mock
 
-import tornado.httputil
-import tornado.testing
-import tornado.web
+from tornado.testing import AsyncHTTPTestCase
 from utilities import test_utilities
 
 from request import reliability_handler
+from request.tests import test_request_handler
 
-ORG_CODE = "org"
-SERVICE_ID = "service"
 RELIABILITY_DETAILS = {"retries": 7}
 
 
-class TestReliabilityRequestHandler(tornado.testing.AsyncHTTPTestCase):
+class TestReliabilityRequestHandler(test_request_handler.TestRequestHandler, AsyncHTTPTestCase):
     def get_app(self):
-        self.routing = unittest.mock.Mock()
-
-        return tornado.web.Application([
-            (r"/", reliability_handler.ReliabilityRequestHandler, {"routing": self.routing})
-        ])
+        return self._build_app(reliability_handler.ReliabilityRequestHandler)
 
     def test_get(self):
         self.routing.get_reliability.return_value = test_utilities.awaitable(RELIABILITY_DETAILS)
 
-        url = tornado.httputil.url_concat("/", {"org-code": ORG_CODE, "service-id": SERVICE_ID})
-        response = self.fetch(url, method="GET")
+        response = self.fetch(test_request_handler.TestRequestHandler.build_url(), method="GET")
 
         self.assertEqual(response.code, 200)
         self.assertEqual(RELIABILITY_DETAILS, json.loads(response.body))
-        self.routing.get_reliability.assert_called_with(ORG_CODE, SERVICE_ID)
+        self.routing.get_reliability.assert_called_with(test_request_handler.ORG_CODE, test_request_handler.SERVICE_ID)
+
+    def test_get_returns_error(self):
+        self.routing.get_reliability.side_effect = Exception
+
+        response = self.fetch(test_request_handler.TestRequestHandler.build_url(), method="GET")
+
+        self.assertEqual(response.code, 500)
--- mhs/spineroutelookup/request/tests/test_routing_handler.py
+++ mhs/spineroutelookup/request/tests/test_routing_handler.py
@@ -1,32 +1,31 @@
 import json
-import unittest.mock
 
-import tornado.httputil
 import tornado.testing
 import tornado.web
 from utilities import test_utilities
 
 from request import routing_handler
+from request.tests import test_request_handler
 
-ORG_CODE = "org"
-SERVICE_ID = "service"
 END_POINT_DETAILS = {"end_point": "http://www.example.com"}
 
 
-class TestRoutingRequestHandler(tornado.testing.AsyncHTTPTestCase):
+class TestRoutingRequestHandler(test_request_handler.TestRequestHandler, tornado.testing.AsyncHTTPTestCase):
     def get_app(self):
-        self.routing = unittest.mock.Mock()
-
-        return tornado.web.Application([
-            (r"/", routing_handler.RoutingRequestHandler, {"routing": self.routing})
-        ])
+        return self._build_app(routing_handler.RoutingRequestHandler)
 
     def test_get(self):
         self.routing.get_end_point.return_value = test_utilities.awaitable(END_POINT_DETAILS)
 
-        url = tornado.httputil.url_concat("/", {"org-code": ORG_CODE, "service-id": SERVICE_ID})
-        response = self.fetch(url, method="GET")
+        response = self.fetch(test_request_handler.TestRequestHandler.build_url(), method="GET")
 
         self.assertEqual(response.code, 200)
         self.assertEqual(END_POINT_DETAILS, json.loads(response.body))
-        self.routing.get_end_point.assert_called_with(ORG_CODE, SERVICE_ID)
+        self.routing.get_end_point.assert_called_with(test_request_handler.ORG_CODE, test_request_handler.SERVICE_ID)
+
+    def test_get_returns_error(self):
+        self.routing.get_end_point.side_effect = Exception
+
+        response = self.fetch(test_request_handler.TestRequestHandler.build_url(), method="GET")
+
+        self.assertEqual(response.code, 500)
