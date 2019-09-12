8f7e6a54e99248323ecc685b639379469b62f303
Ismail S <ismail-s@users.noreply.github.com>
2019-09-12 10:23:49 +0100

    RT-179 Reformat file

--- pipeline/scripts/check-target-group-health/main.py
+++ pipeline/scripts/check-target-group-health/main.py
@@ -1,9 +1,11 @@
-"A script that checks that AWS load balancers have all healthy targets. For use in CI builds."
+"""A script that checks that AWS load balancers have all healthy targets. For use in CI builds."""
 
-from typing import List
 import argparse
 import sys
+from typing import List
+
 import boto3
+
 
 def main(target_group_arns: List[str]):
     client = boto3.client('elbv2')
@@ -14,12 +16,14 @@ def main(target_group_arns: List[str]):
             if target_health_description['TargetHealth']['State'] != 'healthy':
                 sys.exit(1)
 
+
 def _parse_arguments():
     parser = argparse.ArgumentParser(description="Check AWS load balancer target groups have all healthy targets")
 
     parser.add_argument("target_group_arns", nargs = '+', help="The ARNs of the target groups to check")
 
     return parser.parse_args()
+
 
 if __name__ == '__main__':
     args = _parse_arguments()
