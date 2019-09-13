082f859c8ef9d28be579da89f912cf4b2ee9d420
Ismail S <ismail-s@users.noreply.github.com>
2019-09-13 12:15:17 +0100

    RT-179 Reformat file

--- pipeline/terraform/mhs-environment/ecs.tf
+++ pipeline/terraform/mhs-environment/ecs.tf
@@ -34,7 +34,7 @@ resource "aws_cloudwatch_log_group" "mhs_inbound_log_group" {
 resource "aws_cloudwatch_log_group" "mhs_route_log_group" {
   name = "/ecs/${var.environment_id}-mhs-route"
   tags = {
-    Name          = "${var.environment_id}-mhs-route-log-group"
+    Name = "${var.environment_id}-mhs-route-log-group"
     EnvironmentId = var.environment_id
   }
 }
@@ -205,9 +205,9 @@ resource "aws_ecs_task_definition" "mhs_inbound_task" {
         },
         # Port 80 is the port for healthcheck requests from the MHS inbound load balancer
         {
-            containerPort = 80
-            hostPort      = 80
-            protocol      = "tcp"
+          containerPort = 80
+          hostPort = 80
+          protocol = "tcp"
         }
       ]
     }
@@ -231,68 +231,68 @@ resource "aws_ecs_task_definition" "mhs_inbound_task" {
 resource "aws_ecs_task_definition" "mhs_route_task" {
   family = "${var.environment_id}-mhs-route"
   container_definitions = jsonencode(
-    [
-      {
-        name  = "mhs-route"
-        image = "${var.ecr_address}/mhs/route:route-${var.build_id}"
-        environment = [
-          {
-            name  = "MHS_LOG_LEVEL"
-            value = var.mhs_log_level
-          },
-          {
-            name = "MHS_SDS_URL"
-            value = var.spineroutelookup_service_sds_url
-          },
-          {
-            name = "MHS_DISABLE_SDS_TLS"
-            value = var.spineroutelookup_service_disable_sds_tls
-          }
-        ]
-        secrets = [
-          {
-            name = "MHS_CLIENT_CERT"
-            valueFrom = var.client_cert_arn
-          },
-          {
-            name      = "MHS_CLIENT_KEY"
-            valueFrom = var.client_key_arn
-          },
-          {
-            name      = "MHS_CA_CERTS"
-            valueFrom = var.ca_certs_arn
-          }
-        ]
-        essential = true
-        logConfiguration = {
-          logDriver = "awslogs"
-          options = {
-            awslogs-group         = aws_cloudwatch_log_group.mhs_route_log_group.name
-            awslogs-region        = var.region
-            awslogs-stream-prefix = var.build_id
-          }
+  [
+    {
+      name = "mhs-route"
+      image = "${var.ecr_address}/mhs/route:route-${var.build_id}"
+      environment = [
+        {
+          name = "MHS_LOG_LEVEL"
+          value = var.mhs_log_level
+        },
+        {
+          name = "MHS_SDS_URL"
+          value = var.spineroutelookup_service_sds_url
+        },
+        {
+          name = "MHS_DISABLE_SDS_TLS"
+          value = var.spineroutelookup_service_disable_sds_tls
         }
-        portMappings = [
-          {
-            containerPort = 80
-            hostPort      = 80
-            protocol      = "tcp"
-          }
-        ]
+      ]
+      secrets = [
+        {
+          name = "MHS_CLIENT_CERT"
+          valueFrom = var.client_cert_arn
+        },
+        {
+          name = "MHS_CLIENT_KEY"
+          valueFrom = var.client_key_arn
+        },
+        {
+          name = "MHS_CA_CERTS"
+          valueFrom = var.ca_certs_arn
+        }
+      ]
+      essential = true
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group = aws_cloudwatch_log_group.mhs_route_log_group.name
+          awslogs-region = var.region
+          awslogs-stream-prefix = var.build_id
+        }
       }
-    ]
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort = 80
+          protocol = "tcp"
+        }
+      ]
+    }
+  ]
   )
-  cpu          = "512"
-  memory       = "1024"
+  cpu = "512"
+  memory = "1024"
   network_mode = "awsvpc"
   requires_compatibilities = [
     "FARGATE"
   ]
   tags = {
-    Name          = "${var.environment_id}-mhs-route-task"
+    Name = "${var.environment_id}-mhs-route-task"
     EnvironmentId = var.environment_id
   }
-  task_role_arn      = var.task_role_arn
+  task_role_arn = var.task_role_arn
   execution_role_arn = var.execution_role_arn
 }
 
@@ -331,28 +331,30 @@ resource "aws_ecs_service" "mhs_outbound_service" {
 
   # Preserve the autoscaled instance count when this service is updated
   lifecycle {
-    ignore_changes = ["desired_count"]
+    ignore_changes = [
+      "desired_count"
+    ]
   }
 }
 
 # The autoscaling target that configures autoscaling for the MHS outbound ECS service.
 resource "aws_appautoscaling_target" "mhs_outbound_autoscaling_target" {
-  max_capacity       = var.mhs_outbound_service_maximum_instance_count
-  min_capacity       = var.mhs_outbound_service_minimum_instance_count
-  resource_id        = "service/${aws_ecs_cluster.mhs_cluster.name}/${aws_ecs_service.mhs_outbound_service.name}"
-  role_arn           = var.task_scaling_role_arn
+  max_capacity = var.mhs_outbound_service_maximum_instance_count
+  min_capacity = var.mhs_outbound_service_minimum_instance_count
+  resource_id = "service/${aws_ecs_cluster.mhs_cluster.name}/${aws_ecs_service.mhs_outbound_service.name}"
+  role_arn = var.task_scaling_role_arn
   scalable_dimension = "ecs:service:DesiredCount"
-  service_namespace  = "ecs"
+  service_namespace = "ecs"
 }
 
 # An autoscaling policy for the MHS outbound ECS service that scales services so that each instance handles the desired
 # number of requests per minute.
 resource "aws_appautoscaling_policy" "mhs_outbound_autoscaling_policy" {
-  name               = "${var.environment_id}-mhs-outbound-autoscaling-policy"
-  policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.mhs_outbound_autoscaling_target.resource_id
+  name = "${var.environment_id}-mhs-outbound-autoscaling-policy"
+  policy_type = "TargetTrackingScaling"
+  resource_id = aws_appautoscaling_target.mhs_outbound_autoscaling_target.resource_id
   scalable_dimension = aws_appautoscaling_target.mhs_outbound_autoscaling_target.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.mhs_outbound_autoscaling_target.service_namespace
+  service_namespace = aws_appautoscaling_target.mhs_outbound_autoscaling_target.service_namespace
 
   target_tracking_scaling_policy_configuration {
     target_value = var.mhs_outbound_service_target_request_count
@@ -399,28 +401,30 @@ resource "aws_ecs_service" "mhs_inbound_service" {
 
   # Preserve the autoscaled instance count when this service is updated
   lifecycle {
-    ignore_changes = ["desired_count"]
+    ignore_changes = [
+      "desired_count"
+    ]
   }
 }
 
 # The autoscaling target that configures autoscaling for the MHS inbound ECS service.
 resource "aws_appautoscaling_target" "mhs_inbound_autoscaling_target" {
-  max_capacity       = var.mhs_inbound_service_maximum_instance_count
-  min_capacity       = var.mhs_inbound_service_minimum_instance_count
-  resource_id        = "service/${aws_ecs_cluster.mhs_cluster.name}/${aws_ecs_service.mhs_inbound_service.name}"
-  role_arn           = var.task_scaling_role_arn
+  max_capacity = var.mhs_inbound_service_maximum_instance_count
+  min_capacity = var.mhs_inbound_service_minimum_instance_count
+  resource_id = "service/${aws_ecs_cluster.mhs_cluster.name}/${aws_ecs_service.mhs_inbound_service.name}"
+  role_arn = var.task_scaling_role_arn
   scalable_dimension = "ecs:service:DesiredCount"
-  service_namespace  = "ecs"
+  service_namespace = "ecs"
 }
 
 # An autoscaling policy for the MHS inbound ECS service that scales services so that each instance handles the desired
 # number of requests per minute.
 resource "aws_appautoscaling_policy" "mhs_inbound_autoscaling_policy" {
-  name               = "${var.environment_id}-mhs-inbound-autoscaling-policy"
-  policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.mhs_inbound_autoscaling_target.resource_id
+  name = "${var.environment_id}-mhs-inbound-autoscaling-policy"
+  policy_type = "TargetTrackingScaling"
+  resource_id = aws_appautoscaling_target.mhs_inbound_autoscaling_target.resource_id
   scalable_dimension = aws_appautoscaling_target.mhs_inbound_autoscaling_target.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.mhs_inbound_autoscaling_target.service_namespace
+  service_namespace = aws_appautoscaling_target.mhs_inbound_autoscaling_target.service_namespace
 
   target_tracking_scaling_policy_configuration {
     target_value = var.mhs_inbound_service_target_cpu_utilization
@@ -433,14 +437,14 @@ resource "aws_appautoscaling_policy" "mhs_inbound_autoscaling_policy" {
 # Create an ECS service that runs a configurable number of instances of the route service container across all of the
 # VPC's subnets. Each container is register with the route service's LB's target group.
 resource "aws_ecs_service" "mhs_route_service" {
-  name                               = "${var.environment_id}-mhs-route"
-  cluster                            = aws_ecs_cluster.mhs_cluster.id
-  deployment_maximum_percent         = 200
+  name = "${var.environment_id}-mhs-route"
+  cluster = aws_ecs_cluster.mhs_cluster.id
+  deployment_maximum_percent = 200
   deployment_minimum_healthy_percent = 100
-  desired_count                      = var.mhs_route_service_initial_instance_count
-  launch_type                        = "FARGATE"
-  scheduling_strategy                = "REPLICA"
-  task_definition                    = aws_ecs_task_definition.mhs_route_task.arn
+  desired_count = var.mhs_route_service_initial_instance_count
+  launch_type = "FARGATE"
+  scheduling_strategy = "REPLICA"
+  task_definition = aws_ecs_task_definition.mhs_route_task.arn
 
   network_configuration {
     assign_public_ip = false
@@ -451,8 +455,8 @@ resource "aws_ecs_service" "mhs_route_service" {
   }
 
   load_balancer {
-    container_name   = jsondecode(aws_ecs_task_definition.mhs_route_task.container_definitions)[0].name
-    container_port   = jsondecode(aws_ecs_task_definition.mhs_route_task.container_definitions)[0].portMappings[0].hostPort
+    container_name = jsondecode(aws_ecs_task_definition.mhs_route_task.container_definitions)[0].name
+    container_port = jsondecode(aws_ecs_task_definition.mhs_route_task.container_definitions)[0].portMappings[0].hostPort
     target_group_arn = aws_lb_target_group.route_alb_target_group.arn
   }
 
@@ -462,28 +466,30 @@ resource "aws_ecs_service" "mhs_route_service" {
 
   # Preserve the autoscaled instance count when this service is updated
   lifecycle {
-    ignore_changes = ["desired_count"]
+    ignore_changes = [
+      "desired_count"
+    ]
   }
 }
 
 # The autoscaling target that configures autoscaling for the MHS route ECS service.
 resource "aws_appautoscaling_target" "mhs_route_autoscaling_target" {
-  max_capacity       = var.mhs_route_service_maximum_instance_count
-  min_capacity       = var.mhs_route_service_minimum_instance_count
-  resource_id        = "service/${aws_ecs_cluster.mhs_cluster.name}/${aws_ecs_service.mhs_route_service.name}"
-  role_arn           = var.task_scaling_role_arn
+  max_capacity = var.mhs_route_service_maximum_instance_count
+  min_capacity = var.mhs_route_service_minimum_instance_count
+  resource_id = "service/${aws_ecs_cluster.mhs_cluster.name}/${aws_ecs_service.mhs_route_service.name}"
+  role_arn = var.task_scaling_role_arn
   scalable_dimension = "ecs:service:DesiredCount"
-  service_namespace  = "ecs"
+  service_namespace = "ecs"
 }
 
 # An autoscaling policy for the MHS route ECS service that scales services so that each instance handles the desired
 # number of requests per minute.
 resource "aws_appautoscaling_policy" "mhs_route_autoscaling_policy" {
-  name               = "${var.environment_id}-mhs-route-autoscaling-policy"
-  policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.mhs_route_autoscaling_target.resource_id
+  name = "${var.environment_id}-mhs-route-autoscaling-policy"
+  policy_type = "TargetTrackingScaling"
+  resource_id = aws_appautoscaling_target.mhs_route_autoscaling_target.resource_id
   scalable_dimension = aws_appautoscaling_target.mhs_route_autoscaling_target.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.mhs_route_autoscaling_target.service_namespace
+  service_namespace = aws_appautoscaling_target.mhs_route_autoscaling_target.service_namespace
 
   target_tracking_scaling_policy_configuration {
     target_value = var.mhs_route_service_target_request_count
