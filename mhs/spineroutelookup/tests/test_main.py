57d5c4907034346455b503ad7708738f55c661a0
Gareth Allan <gareth.allan@bjss.com>
2019-09-18 09:59:30 +0100

    RT-180: Fix SRL component's main module tests

--- mhs/spineroutelookup/tests/test_main.py
+++ mhs/spineroutelookup/tests/test_main.py
@@ -6,7 +6,6 @@ import main
 from lookup import dictionary_cache
 
 CONFIG_PREFIX = "MHS"
-CACHE_IMPLEMENTATION_KEY = "MHS_SDS_CACHE_IMPLEMENTATION"
 CACHE_EXPIRY_TIME_KEY = "MHS_SDS_CACHE_EXPIRY_TIME"
 DEFAULT_CACHE_EXPIRY_TIME = 900
 CACHE_EXPIRY_TIME = 450
@@ -16,7 +15,6 @@ class TestMain(unittest.TestCase):
     @unittest.mock.patch("os.environ", new_callable=dict)
     @unittest.mock.patch.dict(config.config)
     def test_load_cache_implementation(self, mock_environment):
-        mock_environment[CACHE_IMPLEMENTATION_KEY] = dictionary_cache.IMPLEMENTATION_DICTIONARY_KEY
         mock_environment[CACHE_EXPIRY_TIME_KEY] = CACHE_EXPIRY_TIME
         config.setup_config(CONFIG_PREFIX)
 
