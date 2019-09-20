47fc69ba3b88e63079dcb02032eafa627eefee41
Philip-Woods <49308422+Philip-Woods@users.noreply.github.com>
2019-09-20 14:06:56 +0100

    Feature/rt 90 forward to mhs (#95)
    * Fix failing SCR tests
    
    * Update SCR templates to have appropriate values
    
    As part of the PoC a number of parameters were added to the SCR message
    template, some of these have be set to fixed values, the SCR tests have
    been updated to reflect this as well
    
    * Updated tests for gp_summary_handler
    
    * Remove interactionId information from template json
    
    * Add interactions dependancy to MessageForwarder
    
    * Updated renames to span classes and file names
    
    * Fix message forwarder not using correct template method
    
    * Stand up SCR Web Service with message population
    
    Several comments were addressed from a quick review:
        * It's noted that pystache handling the verfication of the supplier
        message contents
        * Docstrings have been updated
        * The SCR service has been stood up as much as is possible
    
    * Remove ToDos
    
    * Update test names
    
    * Updated docstrings
    
    * Wrap main.py executable in catch all exceptions for logging
    
    * Remove unused exception and variable from forwarder
    
    * Update test names, contents and typos
    
    * Fix test data values
    
    * Update test name
    
    * Add sending functionality to the adaptor
    
    * The adaptor now requires a config element of the MHS address
    * The message forwarder now has a dependancy on a message sender
    
    * Add extra component test for no interaction-id
    
    * Update component test to check make_request call
    
    * Add missing docstrings
    
    * Remove line ending issue with xml comparison test
    
    * Update test to check message/correlation id are propogated
    
    * Update type hints
    
    * Remove magic strings from tests
    
    * Add docstrings
    
    * Update log lines

--- SCRWebService/endpoints/tests/test_summary_care_record.py
+++ SCRWebService/endpoints/tests/test_summary_care_record.py
@@ -3,7 +3,7 @@ import json
 import pathlib
 
 from builder.pystache_message_builder import MessageGenerationError
-from utilities import file_utilities
+from utilities import file_utilities, test_utilities
 from definitions import ROOT_DIR
 from tornado.testing import AsyncHTTPTestCase
 from tornado.web import Application
@@ -26,13 +26,15 @@ class TestSummaryCareRecord(AsyncHTTPTestCase):
              dict(forwarder=self.forwarder))
         ])
 
-    def test_handler_returns_message_processing_response(self):
+    @mock.patch('utilities.message_utilities.MessageUtilities.get_uuid')
+    def test_handler_returns_message_processing_response(self, uuid_mock):
+        uuid_mock.return_value = "check123"
         body = file_utilities.FileUtilities.get_file_dict(complete_data_path)
-        self.forwarder.forward_message_to_mhs.return_value = "Nice response message"
-        response = self.fetch(GP_SUMMARY_UPLOAD_URL, method='POST', headers={'interaction-id': '123'},
+        self.forwarder.forward_message_to_mhs.return_value = test_utilities.awaitable("Nice response message")
+        response = self.fetch(GP_SUMMARY_UPLOAD_URL, method='POST', headers={'interaction-name': '123'},
                               body=json.dumps(body))
 
-        self.forwarder.forward_message_to_mhs.assert_called_once_with("123", body)
+        self.forwarder.forward_message_to_mhs.assert_called_once_with("123", body, None, "check123")
         self.assertEqual(response.body.decode(), "Nice response message")
 
     def test_null_body_request_fails(self):
@@ -45,7 +47,7 @@ class TestSummaryCareRecord(AsyncHTTPTestCase):
 
     def test_invalid_json_request_fails(self):
         body = "{'yes': 'wow'}"
-        response = self.fetch(GP_SUMMARY_UPLOAD_URL, method='POST', headers={'interaction-id': '123'},
+        response = self.fetch(GP_SUMMARY_UPLOAD_URL, method='POST', headers={'interaction-name': '123'},
                               body=body)
         self.assertEqual(response.code, 400)
         error = "Exception raised while parsing message body:" \
@@ -55,7 +57,7 @@ class TestSummaryCareRecord(AsyncHTTPTestCase):
     def test_message_generation_exception_in_handler_returns_error_to_caller(self):
         body = file_utilities.FileUtilities.get_file_dict(complete_data_path)
         self.forwarder.forward_message_to_mhs.side_effect = MessageGenerationError('This is a specific error')
-        response = self.fetch(GP_SUMMARY_UPLOAD_URL, method='POST', headers={'interaction-id': '123'},
+        response = self.fetch(GP_SUMMARY_UPLOAD_URL, method='POST', headers={'interaction-name': '123'},
                               body=json.dumps(body))
 
         self.assertEqual(response.code, 400)
