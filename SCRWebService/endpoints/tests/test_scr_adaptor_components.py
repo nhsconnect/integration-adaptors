c2808c56163ab62de6ebbd69baa2f3f856874fab
Philip Woods <philip.woods@bjss.com>
2019-09-23 14:57:08 +0100

    Correct component tests not running

--- SCRWebService/endpoints/tests/test_scr_adaptor_components.py
+++ SCRWebService/endpoints/tests/test_scr_adaptor_components.py
@@ -43,10 +43,121 @@ class TestSummaryCareRecord(AsyncHTTPTestCase):
         return Application([(r"/", summary_care_record.SummaryCareRecord, dict(forwarder=forwarder))])
 
     @mock.patch('comms.common_https.CommonHttps.make_request')
-    def test_scr_adaptor_calls_mhs_end_point_happy_path(self, request_mock):
+    def test_scr_adaptor_calls_mhs_end_point_with_populated_message(self, request_mock):
         # Arrange
         body = file_utilities.FileUtilities.get_file_dict(complete_data_path)
         expected_message = file_utilities.FileUtilities.get_file_string(populated_message_path)
+
+        # Act
+        self.fetch(GP_SUMMARY_UPLOAD_URL, method='POST',
+                   headers={INTERACTION_NAME: GP_SUMMARY_UPLOAD_NAME},
+                   body=json.dumps(body))
+
+        # Assert
+        body_call_value = request_mock.call_args[1]['body']
+        call_method = request_mock.call_args[1]['method']
+        url_method = request_mock.call_args[1]['url']
+
+        xml_utilities.XmlUtilities.assert_xml_equal(body_call_value, expected_message)
+        self.assertEqual(call_method, 'POST')
+        self.assertEqual(url_method, self.address)
+
+    @mock.patch('comms.common_https.CommonHttps.make_request')
+    def test_exception_raised_during_message_sending_raises_MessageSendingError(self, request_mock):
+        body = file_utilities.FileUtilities.get_file_dict(complete_data_path)
+        request_mock.side_effect = Exception('Failed to route the dam packet')
+
+        response = self.fetch(GP_SUMMARY_UPLOAD_URL, method='POST',
+                              headers={INTERACTION_NAME: GP_SUMMARY_UPLOAD_NAME},
+                              body=json.dumps(body))
+
+        self.assertEqual(response.code, 500)
+        self.assertIn('Failed to route the dam packet', response.body.decode())
+
+    def test_empty_interaction_name_raises_error(self):
+        body = file_utilities.FileUtilities.get_file_dict(complete_data_path)
+
+        response = self.fetch(GP_SUMMARY_UPLOAD_URL, method='POST', headers={},
+                              body=json.dumps(body))
+
+        self.assertEqual(response.code, 400)
+        self.assertIn('No interaction-name header provided', response.body.decode())
+
+    @mock.patch('comms.common_https.CommonHttps.make_request')
+    def test_correlation_id_is_passed_to_mhs_if_provided_to_scr_adaptor(self, request_mock):
+        body = file_utilities.FileUtilities.get_file_dict(complete_data_path)
+
+        request_mock.return_value = test_utilities.awaitable("Response message")
+
+        self.fetch(GP_SUMMARY_UPLOAD_URL, method='POST',
+                   headers={
+                       INTERACTION_NAME: GP_SUMMARY_UPLOAD_NAME,
+                       CORRELATION_ID: "123123123"
+                   },
+                   body=json.dumps(body))
+
+        correlation_id_header = request_mock.call_args[1]['headers'][CORRELATION_ID]
+        self.assertEqual(correlation_id_header, "123123123")
+
+    @mock.patch('comms.common_https.CommonHttps.make_request')
+    def test_message_id_is_passed_to_mhs_if_provided_to_scr_adaptor(self, request_mock):
+        body = file_utilities.FileUtilities.get_file_dict(complete_data_path)
+
+        request_mock.return_value = test_utilities.awaitable("Response message")
+
+        self.fetch(GP_SUMMARY_UPLOAD_URL, method='POST',
+                   headers={
+                       INTERACTION_NAME: GP_SUMMARY_UPLOAD_NAME,
+                       MESSAGE_ID: "123123123"
+                   },
+                   body=json.dumps(body))
+
+        correlation_id_header = request_mock.call_args[1]['headers'][MESSAGE_ID]
+        self.assertEqual(correlation_id_header, "123123123")
+
+    @mock.patch('comms.common_https.CommonHttps.make_request')
+    def test_both_message_id_and_correlation_are_passed_to_mhs_when_provided(self, request_mock):
+        body = file_utilities.FileUtilities.get_file_dict(complete_data_path)
+
+        request_mock.return_value = test_utilities.awaitable("Response message")
+
+        self.fetch(GP_SUMMARY_UPLOAD_URL, method='POST',
+                   headers={
+                       INTERACTION_NAME: GP_SUMMARY_UPLOAD_NAME,
+                       MESSAGE_ID: 'messageId1212',
+                       CORRELATION_ID: 'correlationId1212'
+                   },
+                   body=json.dumps(body))
+
+        correlation_id_header = request_mock.call_args[1]['headers']['correlation-id']
+        message_id_header = request_mock.call_args[1]['headers']['message-id']
+        self.assertEqual(message_id_header, "messageId1212")
+        self.assertEqual(correlation_id_header, 'correlationId1212')
+
+    @mock.patch('utilities.message_utilities.MessageUtilities.get_uuid')
+    @mock.patch('comms.common_https.CommonHttps.make_request')
+    def test_when_no_correlation_id_is_provided_the_adaptor_generates_one_and_passes_it_to_the_mhs(self,
+                                                                                                   request_mock,
+                                                                                                   uuid_mock):
+        uuid_mock.return_value = "yes-this-is-mocked"
+        body = file_utilities.FileUtilities.get_file_dict(complete_data_path)
+
+        request_mock.return_value = test_utilities.awaitable("Response message")
+
+        self.fetch(GP_SUMMARY_UPLOAD_URL, method='POST',
+                   headers={
+                       INTERACTION_NAME: GP_SUMMARY_UPLOAD_NAME,
+                       MESSAGE_ID: 'messageId1212'
+                   },
+                   body=json.dumps(body))
+
+        correlation_id_header = request_mock.call_args[1]['headers'][CORRELATION_ID]
+        self.assertEqual(correlation_id_header, "yes-this-is-mocked")
+
+    @mock.patch('comms.common_https.CommonHttps.make_request')
+    def test_success_response_parsing(self, request_mock):
+        # Arrange
+        body = file_utilities.FileUtilities.get_file_dict(complete_data_path)
         response_body = file_utilities.FileUtilities.get_file_string(response_xml)
         response_mock = TestResponse()
         response_mock.body = response_body
@@ -57,136 +168,9 @@ class TestSummaryCareRecord(AsyncHTTPTestCase):
                               headers={INTERACTION_NAME: GP_SUMMARY_UPLOAD_NAME},
                               body=json.dumps(body))
 
-        # Assert
-        body_call_value = request_mock.call_args[1]['body']
-        call_method = request_mock.call_args[1]['method']
-        url_method = request_mock.call_args[1]['url']
-
-        xml_utilities.XmlUtilities.assert_xml_equal(body_call_value, expected_message)
-        self.assertEqual(call_method, 'POST')
-        self.assertEqual(url_method, self.address)
         response_body = json.loads(response.body.decode())
 
         self.assertEqual(response_body['messageRef'], '9C534C19-C587-4463-9AED-B76F715D3EA3')
         self.assertEqual(response_body['messageId'], '2E372546-229A-483F-9B11-EF46ABF3178C')
         self.assertEqual(response_body['creationTime'], '20190923112609')
         self.assertEqual(response_body['messageDetail'], 'GP Summary upload successful')
-
-        @mock.patch('comms.common_https.CommonHttps.make_request')
-        def test_exception_raised_during_message_sending_raises_MessageSendingError(self, request_mock):
-            body = file_utilities.FileUtilities.get_file_dict(complete_data_path)
-            request_mock.side_effect = Exception('Failed to route the dam packet')
-
-            response = self.fetch(GP_SUMMARY_UPLOAD_URL, method='POST',
-                                  headers={INTERACTION_NAME: GP_SUMMARY_UPLOAD_NAME},
-                                  body=json.dumps(body))
-
-            self.assertEqual(response.code, 500)
-            self.assertIn('Failed to route the dam packet', response.body.decode())
-
-        def test_empty_interaction_name_raises_error(self):
-            body = file_utilities.FileUtilities.get_file_dict(complete_data_path)
-
-            response = self.fetch(GP_SUMMARY_UPLOAD_URL, method='POST', headers={},
-                                  body=json.dumps(body))
-
-            self.assertEqual(response.code, 400)
-            self.assertIn('No interaction-name header provided', response.body.decode())
-
-        @mock.patch('comms.common_https.CommonHttps.make_request')
-        def test_correlation_id_is_passed_to_mhs_if_provided_to_scr_adaptor(self, request_mock):
-            body = file_utilities.FileUtilities.get_file_dict(complete_data_path)
-
-            request_mock.return_value = test_utilities.awaitable("Response message")
-
-            self.fetch(GP_SUMMARY_UPLOAD_URL, method='POST',
-                       headers={
-                           INTERACTION_NAME: GP_SUMMARY_UPLOAD_NAME,
-                           CORRELATION_ID: "123123123"
-                       },
-                       body=json.dumps(body))
-
-            correlation_id_header = request_mock.call_args[1]['headers'][CORRELATION_ID]
-            self.assertEqual(correlation_id_header, "123123123")
-
-        @mock.patch('comms.common_https.CommonHttps.make_request')
-        def test_message_id_is_passed_to_mhs_if_provided_to_scr_adaptor(self, request_mock):
-            body = file_utilities.FileUtilities.get_file_dict(complete_data_path)
-
-            request_mock.return_value = test_utilities.awaitable("Response message")
-
-            self.fetch(GP_SUMMARY_UPLOAD_URL, method='POST',
-                       headers={
-                           INTERACTION_NAME: GP_SUMMARY_UPLOAD_NAME,
-                           MESSAGE_ID: "123123123"
-                       },
-                       body=json.dumps(body))
-
-            correlation_id_header = request_mock.call_args[1]['headers'][MESSAGE_ID]
-            self.assertEqual(correlation_id_header, "123123123")
-
-        @mock.patch('comms.common_https.CommonHttps.make_request')
-        def test_both_message_id_and_correlation_are_passed_to_mhs_when_provided(self, request_mock):
-            body = file_utilities.FileUtilities.get_file_dict(complete_data_path)
-
-            request_mock.return_value = test_utilities.awaitable("Response message")
-
-            self.fetch(GP_SUMMARY_UPLOAD_URL, method='POST',
-                       headers={
-                           INTERACTION_NAME: GP_SUMMARY_UPLOAD_NAME,
-                           MESSAGE_ID: 'messageId1212',
-                           CORRELATION_ID: 'correlationId1212'
-                       },
-                       body=json.dumps(body))
-
-            correlation_id_header = request_mock.call_args[1]['headers']['correlation-id']
-            message_id_header = request_mock.call_args[1]['headers']['message-id']
-            self.assertEqual(message_id_header, "messageId1212")
-            self.assertEqual(correlation_id_header, 'correlationId1212')
-
-        @mock.patch('utilities.message_utilities.MessageUtilities.get_uuid')
-        @mock.patch('comms.common_https.CommonHttps.make_request')
-        def test_when_no_correlation_id_is_provided_the_adaptor_generates_one_and_passes_it_to_the_mhs(self,
-                                                                                                       request_mock,
-                                                                                                       uuid_mock):
-            uuid_mock.return_value = "yes-this-is-mocked"
-            body = file_utilities.FileUtilities.get_file_dict(complete_data_path)
-
-            request_mock.return_value = test_utilities.awaitable("Response message")
-
-            self.fetch(GP_SUMMARY_UPLOAD_URL, method='POST',
-                       headers={
-                           INTERACTION_NAME: GP_SUMMARY_UPLOAD_NAME,
-                           MESSAGE_ID: 'messageId1212'
-                       },
-                       body=json.dumps(body))
-
-            correlation_id_header = request_mock.call_args[1]['headers'][CORRELATION_ID]
-            self.assertEqual(correlation_id_header, "yes-this-is-mocked")
-
-        @mock.patch('utilities.message_utilities.MessageUtilities.get_uuid')
-        @mock.patch('comms.common_https.CommonHttps.make_request')
-        def test_success_response_parsing(self, request_mock, uuid_mock):
-            # Arrange
-            expected_response = {
-                "messageRef": "9C534C19-C587-4463-9AED-B76F715D3EA3",
-                "messageId": "2E372546-229A-483F-9B11-EF46ABF3178C",
-                "creationTime": "20190923112609",
-                "messageDetail": "GP Summary upload successful"
-            }
-
-            uuid_mock.return_value = "yes-this-is-mocked"
-            input_xml = FileUtilities.get_file_string(str(response_xml))
-            request_mock.return_value = test_utilities.awaitable(input_xml)
-            body = file_utilities.FileUtilities.get_file_dict(complete_data_path)
-
-            # Act
-            response = self.fetch(GP_SUMMARY_UPLOAD_URL, method='POST',
-                                  headers={
-                                      INTERACTION_NAME: GP_SUMMARY_UPLOAD_NAME,
-                                      MESSAGE_ID: 'messageId1212'
-                                  },
-                                  body=json.dumps(body))
-
-            # Assert
-            self.assertEqual(expected_response, json.loads(response.body))
