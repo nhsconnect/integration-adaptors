1dddd0985287c84fd24d7431fc8cea0adcdd9e28
Philip Woods <philip.woods@bjss.com>
2019-09-24 14:52:12 +0100

    Add test cases for SCR Adaptor

--- integration-tests/integration_tests/json/json_assertor.py
+++ integration-tests/integration_tests/json/json_assertor.py
@@ -1,10 +1,41 @@
 """
 Provides methods for asserting the contents of a json message
 """
+from __future__ import annotations
+
+import json
+from typing import Any, Optional
+
+import dpath.util
+import unittest
 
 
 class JsonResponseAssertor(object):
     """An assertor that is able to valid the contents of a json response"""
 
-    def __init__(self, json: dict):
-        
+    def __init__(self, json_response: str):
+        self.json_response = json.loads(json_response)
+        self.assertor = unittest.TestCase('__init__')
+
+    def assert_element_exists(self, xpath):
+        self.__get_value(xpath)
+        return self
+
+    def assert_key_value(self, xpath: str, expected_value: Any) -> JsonResponseAssertor:
+        value_at_xpath = self.__get_value(xpath)
+        self.assertor.assertEqual(value_at_xpath, expected_value,
+                                  f'Value at xpath does not equal expected value. '
+                                  f'expected_value = {expected_value} value found={value_at_xpath}')
+
+        return self
+
+    def __get_value(self, xpath) -> Optional[Any]:
+        try:
+            value_at_xpath = dpath.util.get(self.json_response, xpath)
+        except KeyError:
+            value_at_xpath = None
+
+        self.assertor.assertIsNotNone(value_at_xpath,
+                                      f'Key with xpath: {xpath} not found in repsonse json: '
+                                      f'{self.json_response}')
+        return value_at_xpath
--- integration-tests/integration_tests/test_scenarios/int_scr_adaptor_tests.py
+++ integration-tests/integration_tests/test_scenarios/int_scr_adaptor_tests.py
@@ -1,31 +1,49 @@
-from unittest import TestCase, skip
-
-from integration_tests.helpers import interactions, methods
+from unittest import TestCase
 from integration_tests.helpers.build_message import build_message
 from integration_tests.helpers.methods import get_asid
 from integration_tests.http.scr_http_request_builder import ScrHttpRequestBuilder
+from integration_tests.json.json_assertor import JsonResponseAssertor
+from integration_tests.dynamo.dynamo import MHS_STATE_TABLE_DYNAMO_WRAPPER
+from integration_tests.dynamo.dynamo import MHS_STATE_TABLE_DYNAMO_WRAPPER, MHS_SYNC_ASYNC_TABLE_DYNAMO_WRAPPER
+from integration_tests.dynamo.dynamo_mhs_table import DynamoMhsTableStateAssertor
+from integration_tests.dynamo.dynamo_sync_async_mhs_table import DynamoSyncAsyncMhsTableStateAssertor
 
 
-class FunctionalTest(TestCase):
+class ScrAdaptorTests(TestCase):
+    """
+    These tests show a sync-async response from spine via the SCR adaptor (and transitively the MHS) for the
+    example message Gp Summary Upload.
 
-    # request scr record for patient 9446245796, using json template (json_hash16UK05)
-    @skip('waiting for RT-12 - Implement MHS Async Reliable Message Pattern')
-    def test_json_conversion(self):
-        scr_json, uuid = methods.get_json('hash16UK05', 9689177869, 'json message test')
+    - Input: A json payload of the required details used to populate the REPC_IN150016UK05 template
+        within the SCR adaptor
+    - Expected Response:  A json dictionary of the key details parsed from a MCCI_IN010000UK13 success response
 
-        # send this to the adaptor to 'convert' it to HL7
-        scr_response = interactions.call_scr_adaptor(scr_json)
+    An example of an expected success response is as follows:
+        - {
+            "messageRef": "1E2C0BB8-A0AE-4B65-A2EE-AE062F36FFB9",
+            "messageId": "0CA0BB71-067A-49A4-AF9F-B4E19106F1C6",
+            "creationTime": "20190924132325",
+            "messageDetail": "GP Summary upload successful"
+         }
 
-        # now send the scr_response to the mhs
-        mhs_response = interactions.call_mhs('gp_summary_upload', scr_response, None, False, None, False)
 
-        # then validate the mhs-response
-        self.assertTrue(methods.check_response(mhs_response, 'requestSuccessDetail'),
-                        "Unsuccessful response from Spine")
+     This is the same message pattern used by the async-reliable message, details of the flow are documented:
+    - https://data.developer.nhs.uk/dms/mim/6.3.01/Index.htm
+        -> Domains - Health and Clinical Management
+            -> GP Summary
+                -> 7.2 (Request)
+            -> Infrastructure
+                -> 4.2 (Response)
+    """
 
-    def test_should_return_success_response_json(self):
+    def setUp(self):
+        MHS_STATE_TABLE_DYNAMO_WRAPPER.clear_all_records_in_table()
+
+    def test_should_return_success_response_from_spine_as_json(self):
+        # Arrange
         scr_json, message_id = build_message('json_16UK05', get_asid(), '9689174606', 'Scr GP Summary Upload test')
 
+        # Act
         response = ScrHttpRequestBuilder() \
             .with_headers(interaction_name='SCR_GP_SUMMARY_UPLOAD',
                           message_id=message_id,
@@ -33,4 +51,26 @@ class FunctionalTest(TestCase):
             .with_body(scr_json) \
             .execute_post_expecting_success()
 
-        print(response.text)
+        # Assert
+        JsonResponseAssertor(response.text) \
+            .assert_element_exists('messageId') \
+            .assert_element_exists('creationTime') \
+            .assert_key_value('messageRef', message_id) \
+            .assert_key_value('messageDetail', 'GP Summary upload successful')
+
+    def test_should_record_the_correct_response_between_inbound_and_outbound_components(self):
+        # Arrange
+        scr_json, message_id = build_message('json_16UK05', get_asid(), '9689174606', 'Scr GP Summary Upload test')
+
+        # Act
+        ScrHttpRequestBuilder() \
+            .with_headers(interaction_name='SCR_GP_SUMMARY_UPLOAD',
+                          message_id=message_id,
+                          correlation_id='3') \
+            .with_body(scr_json) \
+            .execute_post_expecting_success()
+
+        # Assert
+        DynamoSyncAsyncMhsTableStateAssertor(MHS_SYNC_ASYNC_TABLE_DYNAMO_WRAPPER.get_all_records_in_table()) \
+            .assert_single_item_exists_with_key(message_id) \
+            .assert_element_exists_with_value('.//requestSuccessDetail//detail', 'GP Summary upload successful')
