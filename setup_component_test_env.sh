fde1714bb34e1d69d0cd6462cf1896f490c4f97c
joe honour <joe.honour@bjss.com>
2019-09-28 17:17:32 +0100

    * adding ability to run component tests on server

--- setup_component_test_env.sh
+++ setup_component_test_env.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 mkdir -p ./generated-certs/fake-spine/
 mkdir -p ./generated-certs/inbound/
 mkdir -p ./generated-certs/outbound/
@@ -8,7 +10,7 @@ mkdir -p ./generated-certs/outbound/
 (cd ./generated-certs/inbound || exit; openssl req -x509 -subj "/CN=inbound" -newkey rsa:2048 -nodes -keyout key.pem -out cert.pem -days 365)
 (cd ./generated-certs/outbound || exit; openssl req -x509 -subj "/CN=outbound" -newkey rsa:2048 -nodes -keyout key.pem -out cert.pem -days 365)
 
-
+touch component-test-source.sh
 echo -e "export FAKE_SPINE_CERTIFICATE=\"$(cat ./generated-certs/fake-spine/cert.pem)\"" >> component-test-source.sh
 echo -e "export FAKE_SPINE_PRIVATE_KEY=\"$(cat ./generated-certs/fake-spine/key.pem)\"" >> component-test-source.sh
 echo -e "export OUTBOUND_CERTIFICATE=\"$(cat ./generated-certs/outbound/cert.pem)\"" >> component-test-source.sh
@@ -19,5 +21,7 @@ echo -e "export FAKE_SPINE_CA_STORE=\"$(cat ./generated-certs/outbound/cert.pem)
 echo -e "export OUTBOUND_CA_STORE=\"$(cat ./generated-certs/fake-spine/cert.pem)\"" >> component-test-source.sh
 echo -e "export INBOUND_CA_STORE=\"$(cat ./generated-certs/fake-spine/cert.pem)\"" >> component-test-source.sh
 echo -e "export MHS_SECRET_PARTY_KEY=\"test-party-key\"" >> component-test-source.sh
+
+cat ./component-test-source.sh
 
 rm -rf ./generated-certs
