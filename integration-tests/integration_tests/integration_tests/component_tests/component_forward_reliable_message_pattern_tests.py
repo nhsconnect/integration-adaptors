9b6c37927c02e4fc99b39be9861437717c79a824
Philip Woods <philip.woods@bjss.com>
2019-10-03 15:43:39 +0100

    Add placeholders for forward-reliable tests

---
+++ integration-tests/integration_tests/integration_tests/component_tests/component_forward_reliable_message_pattern_tests.py
@@ -0,0 +1,88 @@
+"""Component tests related to the asynchronous-express message pattern"""
+
+import unittest
+
+from integration_tests.assertors.text_error_response_assertor import TextErrorResponseAssertor
+from integration_tests.dynamo.dynamo import MHS_STATE_TABLE_DYNAMO_WRAPPER, MHS_SYNC_ASYNC_TABLE_DYNAMO_WRAPPER
+from integration_tests.dynamo.dynamo_mhs_table import DynamoMhsTableStateAssertor
+from integration_tests.helpers.build_message import build_message
+from integration_tests.http.mhs_http_request_builder import MhsHttpRequestBuilder
+
+
+class ForwardReliablesMessagingPatternTests(unittest.TestCase):
+    """
+    These tests show an asynchronous express response from Spine via the MHS for the example message interaction
+    of PSIS(Personal Spine Information Service).
+
+    They make use of the fake-spine service, which has known responses for certain message ids.
+    They make use of the fake-spine-route-lookup service, which has known responses for certain interaction ids.
+    """
+
+    def setUp(self):
+        MHS_STATE_TABLE_DYNAMO_WRAPPER.clear_all_records_in_table()
+        MHS_SYNC_ASYNC_TABLE_DYNAMO_WRAPPER.clear_all_records_in_table()
+
+    def test_should_return_successful_response_to_client_when_a_business_level_retry_is_required_and_succeeds(self):
+        """
+        Message ID: '35586865-45B0-41A5-98F6-817CA6F1F5EF' configured in fakespine to return a SOAP Fault error,
+        after 2 retries fakespine will return a success response.
+        """
+
+        # Arrange
+        message, message_id = build_message('COPC_IN000001UK01', '9689177923',
+                                            message_id='35586865-45B0-41A5-98F6-817CA6F1F5EF'
+                                            )
+        # Act: Response should be 202
+        MhsHttpRequestBuilder() \
+            .with_headers(interaction_id='COPC_IN000001UK01 ', message_id=message_id, sync_async=False) \
+            .with_body(message) \
+            .execute_post_expecting_success()
+
+    def test_should_return_information_from_soap_fault_returned_by_spine_in_original_post_request_to_client(self):
+        """
+        Message ID: '35586865-45B0-41A5-98F6-817CA6F1F5EF' configured in fakespine to return a SOAP Fault error,
+        after 2 retries fakespine will return a success response.
+        """
+
+        # Arrange
+        message, message_id = build_message('COPC_IN000001UK01', '9689177923',
+                                            message_id='35586865-45B0-41A5-98F6-817CA6F1F5EF'
+                                            )
+        # Act
+        MhsHttpRequestBuilder() \
+            .with_headers(interaction_id='COPC_IN000001UK01 ', message_id=message_id, sync_async=False) \
+            .with_body(message) \
+            .execute_post_expecting_success()
+
+        # Assert
+        DynamoMhsTableStateAssertor(MHS_STATE_TABLE_DYNAMO_WRAPPER.get_all_records_in_table()) \
+            .assert_single_item_exists_with_key(message_id) \
+            .assert_item_contains_values(
+            {
+                'INBOUND_STATUS': None,
+                'OUTBOUND_STATUS': 'OUTBOUND_MESSAGE_ACKD',
+                'WORKFLOW': 'async-reliable'
+            })
+    
+    def test_should_record_message_status_when_soap_fault_returned_from_spine(self):
+        pass
+
+    def test_should_return_information_from_ebxml_fault_returned_by_spine_in_original_post_request_to_client(self):
+        pass
+
+    def test_should_record_message_status_when_ebxml_fault_returned_from_spine(self):
+        pass
+
+    ### Sync-Async Faults
+
+    def test_should_return_information_from_soap_fault_returned_by_spine_in_original_post_request_to_client_when_sync_async_requested(self):
+        pass
+
+    def test_should_record_message_status_when_soap_fault_returned_from_spine_and_sync_async_requested(self):
+        pass
+
+    def test_should_return_information_from_ebxml_fault_returned_by_spine_in_original_post_request_to_client_when_sync_async_requested(self):
+        pass
+
+    def test_should_record_message_status_when_ebxml_fault_returned_from_spine_and_sync_async_requested(self):
+        pass
