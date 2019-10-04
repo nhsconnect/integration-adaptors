eefc9e82f82f70df548c112e0537d6aaf4ed585e
Philip Woods <philip.woods@bjss.com>
2019-10-04 17:20:00 +0100

    Add sync-async soap fault test

--- integration-tests/integration_tests/integration_tests/component_tests/component_asynchronous_reliable_message_pattern_tests.py
+++ integration-tests/integration_tests/integration_tests/component_tests/component_asynchronous_reliable_message_pattern_tests.py
@@ -201,6 +201,52 @@ class AsynchronousReliableMessagingPatternTests(unittest.TestCase):
                 'WORKFLOW': 'sync-async'
             })
 
+    def test_should_return_information_from_soap_fault_from_spine_in_original_post_request_to_client_when_sync_async_requested(self):
+        """
+        Message ID: '3771F30C-A231-4D64-A46C-E7FB0D52C27C' configured in fakespine to return a soap fault
+        """
+
+        # Arrange
+        message, message_id = build_message('REPC_IN150016UK05', '9689177923',
+                                            message_id='3771F30C-A231-4D64-A46C-E7FB0D52C27C'
+                                            )
+        # Act
+        response = MhsHttpRequestBuilder() \
+            .with_headers(interaction_id='REPC_IN150016UK05 ', message_id=message_id, sync_async=True) \
+            .with_body(message) \
+            .execute_post_expecting_error_response()
+
+        # Assert
+        TextErrorResponseAssertor(response.text) \
+            .assert_error_code(200) \
+            .assert_code_context('urn:nhs:names:error:tms') \
+            .assert_severity('Error')
+
+    def test_should_record_message_status_when_soap_error_response_returned_from_spine(self):
+        """
+        Message ID: '3771F30C-A231-4D64-A46C-E7FB0D52C27C' configured in fakespine to return a soap fault
+        """
+
+        # Arrange
+        message, message_id = build_message('REPC_IN150016UK05', '9689177923',
+                                            message_id='3771F30C-A231-4D64-A46C-E7FB0D52C27C'
+                                            )
+        # Act
+        response = MhsHttpRequestBuilder() \
+            .with_headers(interaction_id='REPC_IN150016UK05 ', message_id=message_id, sync_async=True) \
+            .with_body(message) \
+            .execute_post_expecting_error_response()
+
+        # Assert
+        DynamoMhsTableStateAssertor(MHS_STATE_TABLE_DYNAMO_WRAPPER.get_all_records_in_table()) \
+            .assert_single_item_exists_with_key(message_id) \
+            .assert_item_contains_values(
+            {
+                'INBOUND_STATUS': None,
+                'OUTBOUND_STATUS': 'OUTBOUND_SYNC_ASYNC_MESSAGE_SUCCESSFULLY_RESPONDED',
+                'WORKFLOW': 'sync-async'
+            })
+
     def test_should_return_bad_request_when_client_sends_invalid_message(self):
         # Arrange
         message, message_id = build_message('QUPC_IN160101UK05', '9689174606')
