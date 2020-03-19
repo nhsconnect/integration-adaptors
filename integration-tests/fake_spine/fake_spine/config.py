33f8a199e744a94ab82d6425ea3e557cc8f2baad
Matt Dooner <mattd@kainos.com>
2020-03-19 16:41:03 +0000

    NIAD-97 Prepare Fake Spine for V&P Testing
    * Refactored fake-spine to allow request matchers to be ordered
    * Enhanced fake-spine to generate inbound requests in addition to outbound responses
    * Handles requests for supported message types (forward reliable excluded)
    * Adds capability to control outbound and inbound response times

---
+++ integration-tests/fake_spine/fake_spine/config.py
@@ -0,0 +1,24 @@
+import os
+
+
+class ConfigurationException(Exception):
+    pass
+
+
+def environ_or_error(key: str, default: str = None) -> str:
+    value = os.environ.get(key, default)
+    if value is None:
+        raise ConfigurationException(f'The environment variable {key} is required.')
+    return value
+
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+FAKE_SPINE_PRIVATE_KEY = environ_or_error('FAKE_SPINE_PRIVATE_KEY')
+FAKE_SPINE_CERTIFICATE = environ_or_error('FAKE_SPINE_CERTIFICATE')
+FAKE_SPINE_CA_STORE = environ_or_error('FAKE_SPINE_CA_STORE')
+FAKE_SPINE_PORT = int(environ_or_error('FAKE_SPINE_PORT', default='443'))
+INBOUND_PROXY_PORT = int(environ_or_error('INBOUND_PROXY_PORT', default='80'))
+INBOUND_SERVER_BASE_URL = environ_or_error('INBOUND_SERVER_BASE_URL')
+OUTBOUND_DELAY_MS = int(environ_or_error('FAKE_SPINE_OUTBOUND_DELAY_MS', default='0'))
+INBOUND_DELAY_MS = int(environ_or_error('FAKE_SPINE_INBOUND_DELAY_MS', default='0'))
+MHS_SECRET_PARTY_KEY = os.environ.get('MHS_SECRET_PARTY_KEY')
---
+++ integration-tests/fake_spine/fake_spine/vnp_request_matcher_wrappers.py
@@ -0,0 +1,16 @@
+from fake_spine.request_matching import RequestMatcher
+
+
+def async_express():
+    return RequestMatcher('async-express-vnp',
+                          lambda request: '<eb:Action>QUPC_IN160101UK05</eb:Action>' in request.body.decode())
+
+
+def async_reliable():
+    return RequestMatcher('async-reliable-vnp',
+                          lambda request: '<eb:Action>REPC_IN150016UK05</eb:Action>' in request.body.decode())
+
+
+def sync():
+    return RequestMatcher('async-reliable-vnp',
+                          lambda request: '<wsa:Action>urn:nhs:names:services:pdsquery/QUPA_IN040000UK32</wsa:Action>' in request.body.decode())
