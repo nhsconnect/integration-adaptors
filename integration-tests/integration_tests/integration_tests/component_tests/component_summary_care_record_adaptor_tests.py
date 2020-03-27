d5a1a2f2859a957f1831fc9443564f4b3991f1e0
Peter van der Merwe <p.vandermerwe@kainos.com>
2020-03-27 10:40:37 +0000

    Jenkinsfile now performs some steps in parallel

--- integration-tests/integration_tests/integration_tests/component_tests/component_summary_care_record_adaptor_tests.py
+++ integration-tests/integration_tests/integration_tests/component_tests/component_summary_care_record_adaptor_tests.py
@@ -1,18 +1,19 @@
 """Component tests related to the Summary Care Record adaptor"""
 
-import unittest
+from unittest import TestCase
+from unittest import skip
 
 from integration_tests.helpers.build_message import build_message
 from integration_tests.http.scr_http_request_builder import ScrHttpRequestBuilder
 
 
-class SummaryCareRecordAdaptorTests(unittest.TestCase):
+class SummaryCareRecordAdaptorTests(TestCase):
     """
     These tests show a response from the SCR Adaptor
     They make use of the fake-spine service, which has known responses for certain message ids.
     They make use of the fake-spine-route-lookup service, which has known responses for certain interaction ids.
     """
-
+    @skip('SCR test is not part of MHS adapter')
     def test_should_return_a_bad_request_to_the_client_when_the_client_provides_a_bad_request(self):
         # Arrange
         scr_json, message_id = build_message('bad_gp_summary_upload_json', patient_nhs_number='9689174606',
@@ -30,6 +31,7 @@ class SummaryCareRecordAdaptorTests(unittest.TestCase):
         self.assertEqual(response.status_code, 400)
         self.assertIn('Error whilst generating message', response.text)
 
+    @skip('SCR test is not part of MHS adapter')
     def test_should_return_a_500_to_client_when_mhs_returns_500_to_SCR(self):
         # Arrange
         scr_json, message_id = build_message('json_16UK05', patient_nhs_number='9689174606',
