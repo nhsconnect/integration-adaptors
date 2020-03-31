29f468ee46dc68f6cefe599bb286912cfc90cb73
Peter van der Merwe <p.vandermerwe@kainos.com>
2020-03-31 19:45:04 +0100

    Cleanup release scripts

--- dockers/mhs-base/outbound/release.sh
+++ dockers/mhs-base/outbound/release.sh
@@ -11,11 +11,6 @@ echo "version: $version"
 # run build
 docker build -t $USERNAME/$IMAGE:latest -f Dockerfile .
 # tag it
-#git add -A
-#git commit -m "version $version"
-#git tag -a "$version" -m "version $version"
-#git push
-#git push --tags
 docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$version
 # push it
 docker push $USERNAME/$IMAGE:latest
