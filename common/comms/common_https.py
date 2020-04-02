500c72215d1fcdc44ad409e599b9f18f2e57cbb7
jaroslaww <jaroslaww@kainos.com>
2020-04-02 19:44:07 +0200

    NIAD-23: Revert "NIAD-23: cert validation disabled"
    This reverts commit 5df4574fe8e5da7aab02050d224f1323eb02ffd8.

--- common/comms/common_https.py
+++ common/comms/common_https.py
@@ -11,7 +11,7 @@ class CommonHttps(object):
 
     @staticmethod
     async def make_request(url: str, method: str, headers: Dict[str, str], body: str, client_cert: str = None,
-                           client_key: str = None, ca_certs: str = None, validate_cert: bool = False,
+                           client_key: str = None, ca_certs: str = None, validate_cert: bool = True,
                            http_proxy_host: str = None, http_proxy_port: int = None,
                            raise_error_response: bool = True):
         """Send a HTTPS request and return it's response.
@@ -36,8 +36,6 @@ class CommonHttps(object):
                         "proxy_host": http_proxy_host,
                         "proxy_port": http_proxy_port
                     })
-
-        logger.warning("Cert validation: {validate_cert}", fparams={"validate_cert": validate_cert})
 
         if not validate_cert:
             logger.warning("Server certificate validation has been disabled.")
