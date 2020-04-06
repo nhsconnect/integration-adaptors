abe538d2ed381a1b3a1828fce5818638272334ec
Ali Jaffer <a.jaffer@kainos.com>
2020-04-06 12:07:03 +0100

    test

--- pipeline/terraform/mhs-environment/vpc-connections.tf
+++ pipeline/terraform/mhs-environment/vpc-connections.tf
@@ -172,9 +172,3 @@ resource "aws_route53_zone_association" "dlt_hosted_zone_mhs_vpc_association" {
   zone_id = aws_route53_zone.mhs_hosted_zone.zone_id
   vpc_id = data.aws_vpc.dlt_vpc.id
 }
-# Add a route to the MHS VPC in the DLT VPC route table
-resource "aws_route" "dlt_to_mhs_route" {
-  route_table_id = data.aws_vpc.dlt_vpc.main_route_table_id
-  destination_cidr_block = aws_vpc.mhs_vpc.cidr_block
-  vpc_peering_connection_id = aws_vpc_peering_connection.dlt_peering_connection.id
-}
