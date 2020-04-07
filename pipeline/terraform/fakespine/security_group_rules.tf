a95740b37f329440c3cbe0e1cd084dabfb3392c2
Łukasz Kwaśniewski <l.kwasniewski@kainos.com>
2020-04-07 09:21:47 +0200

    Add rules to allow access to ECR and Cloudwatch

--- pipeline/terraform/fakespine/security_group_rules.tf
+++ pipeline/terraform/fakespine/security_group_rules.tf
@@ -17,12 +17,11 @@ resource "aws_security_group_rule" "fake_spine_security_group_vpc_endpoints_egre
   from_port = 443
   to_port = 443
   protocol = "tcp"
-  //prefix_list_ids = [aws_vpc_endpoint.s3_endpoint.prefix_list_id]
   prefix_list_ids = [data.terraform_remote_state.mhs.outputs.s3_endpoint_prefix_list_ids]
   description = "S3 (for pulling ECR images)"
 }
 
-# Egress rule to allow requests to the MHS inbound service load balancer
+# Egress rule to allow requests to the MHS inbound service load balancer is it needed?
 # resource "aws_security_group_rule" "fake_spine_security_group_route_egress_rule" {
 #   security_group_id = aws_security_group.fake_spine_security_group.id
 #   type = "egress"
@@ -34,25 +33,24 @@ resource "aws_security_group_rule" "fake_spine_security_group_vpc_endpoints_egre
 # }
 
 # Egress rule to allow requests to ECR (to pull the MHS outbound image to run)
-# resource "aws_security_group_rule" "mhs_outbound_security_group_ecr_egress_rule" {
-#   security_group_id = aws_security_group.fake_spine_security_group.id
-#   type = "egress"
-#   from_port = 443
-#   to_port = 443
-#   protocol = "tcp"
-#   //source_security_group_id = aws_security_group.ecr_security_group.id
-#   source_security_group_id = data.terraform_remote_state.mhs.outputs.ecr_security_group_id
-#   description = "HTTPS connections to ECR endpoint."
-# }
+resource "aws_security_group_rule" "fake_spine_security_group_ecr_egress_rule" {
+  security_group_id = aws_security_group.fake_spine_security_group.id
+  type = "egress"
+  from_port = 443
+  to_port = 443
+  protocol = "tcp"
+  source_security_group_id = data.terraform_remote_state.mhs.outputs.ecr_vpce_security_group_id
+  description = "HTTPS connections to ECR endpoint."
+}
 
 # # Egress rule to allow writing logs to Cloudwatch
-# resource "aws_security_group_rule" "mhs_outbound_security_group_cloudwatch_egress_rule" {
-#   security_group_id = aws_security_group.mhs_outbound_security_group.id
-#   type = "egress"
-#   from_port = 443
-#   to_port = 443
-#   protocol = "tcp"
-#   //source_security_group_id = aws_security_group.cloudwatch_security_group.id
-#   source_security_group_id = data.terraform_remote_state.mhs.outputs.cloudwatch_security_group_id
-#   description = "HTTPS connections to Cloudwatch endpoint"
-# }
+resource "aws_security_group_rule" "mhs_outbound_security_group_cloudwatch_egress_rule" {
+  security_group_id = aws_security_group.fake_spine_security_group.id
+  type = "egress"
+  from_port = 443
+  to_port = 443
+  protocol = "tcp"
+  //source_security_group_id = aws_security_group.cloudwatch_security_group.id
+  source_security_group_id = data.terraform_remote_state.mhs.outputs.cloudwatch_vpce_security_group_id
+  description = "HTTPS connections to Cloudwatch endpoint"
+}
