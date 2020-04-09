1e06147d924ba7a553571a71d4942d6d95dc204c
Łukasz Kwaśniewski <l.kwasniewski@kainos.com>
2020-04-09 13:55:42 +0200

    Move variables directly to container definition

--- pipeline/terraform/fakespine/ecs_task_definition.tf
+++ pipeline/terraform/fakespine/ecs_task_definition.tf
@@ -6,8 +6,29 @@ resource "aws_ecs_task_definition" "fake_spine_task" {
     {
       name = "fake-spine"
       image = "${var.ecr_address}/fake-spine:${var.build_id}"
-      environment = local.fake_spine_base_environment_variables
-      secrets = local.fake_spine_secret_environment_variables
+      environment = [{
+        name = "INBOUND_SERVER_BASE_URL",
+        value = var.inbound_server_base_url
+      }, {
+        name = "FAKE_SPINE_OUTBOUND_DELAY_MS",
+        value = var.outbound_delay_ms
+      }, {
+        name = "FAKE_SPINE_INBOUND_DELAY_MS",
+        value = var.inbound_delay_ms
+      }]
+      secrets = [{
+        name = "FAKE_SPINE_PRIVATE_KEY",
+        value = var.fake_spine_private_key
+      }, {
+        name = "FAKE_SPINE_CERTIFICATE",
+        value = var.fake_spine_certificate
+      }, {
+        name = "FAKE_SPINE_CA_STORE",
+        value = var.fake_spine_ca_store
+      }, {
+        name = "MHS_SECRET_PARTY_KEY",
+        value = var.party_key_arn
+      }]
       essential = true
       logConfiguration = {
         logDriver = "awslogs"
