bd36827c4056eacd7e225418ddd6f1ae35234912
Łukasz Kwaśniewski <l.kwasniewski@kainos.com>
2020-04-09 14:38:36 +0200

    Add rules to allow egress from alb to fakespine

--- pipeline/terraform/fakespine/security_group_rules.tf
+++ pipeline/terraform/fakespine/security_group_rules.tf
@@ -1,5 +1,5 @@
 # Ingress rule to allow requests from the fake-spine load balancer security group
-resource "aws_security_group_rule" "fake_spine_security_group_ingress_rule" {
+resource "aws_security_group_rule" "fake_spine_security_group_ingress_80" {
   security_group_id = aws_security_group.fake_spine_security_group.id
   type = "ingress"
   from_port = 80
@@ -7,6 +7,36 @@ resource "aws_security_group_rule" "fake_spine_security_group_ingress_rule" {
   protocol = "tcp"
   source_security_group_id = aws_security_group.alb_fake_spine_security_group.id
   description = "Allow HTTP inbound requests from fake spine load balancer"
+}
+
+resource "aws_security_group_rule" "fake_spine_security_group_ingress_443" {
+  security_group_id = aws_security_group.fake_spine_security_group.id
+  type = "ingress"
+  from_port = 443
+  to_port = 443
+  protocol = "tcp"
+  source_security_group_id = aws_security_group.alb_fake_spine_security_group.id
+  description = "Allow HTTP inbound requests from fake spine load balancer"
+}
+
+resource "aws_security_group_rule" "fake_spine_security_group_egress_80" {
+  security_group_id = aws_security_group.alb_fake_spine_security_group.id
+  source_security_group_id = aws_security_group.fake_spine_security_group.id
+  type = "egress"
+  from_port = 80
+  to_port = 80
+  protocol = "tcp"
+  description = "Allow HTTP from ALB SG to fake spine service"
+}
+
+resource "aws_security_group_rule" "fake_spine_security_group_egress_443" {
+  security_group_id = aws_security_group.alb_fake_spine_security_group.id
+  source_security_group_id = aws_security_group.fake_spine_security_group.id
+  type = "egress"
+  from_port = 443
+  to_port = 443
+  protocol = "tcp"
+  description = "Allow HTTP from ALB SG to fake spine service"
 }
 
 # Egress rule to allow requests to S3 (as ECR stores images there and we need to be
