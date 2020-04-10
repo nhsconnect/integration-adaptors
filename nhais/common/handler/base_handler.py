3b9d496d5afacb92abd43c487394fb1fb84f0cc3
Broom94 <42891767+Broom94@users.noreply.github.com>
2020-04-10 11:06:34 +0100

    Update base_handler.py

--- nhais/common/handler/base_handler.py
+++ nhais/common/handler/base_handler.py
@@ -1,21 +1,14 @@
-from typing import Any, Dict
-
 import tornado.web
-import utilities.message_utilities as message_utilities
 import utilities.integration_adaptors_logger as log
-from comms.http_headers import HttpHeaders
 
 logger = log.IntegrationAdaptorsLogger(__name__)
 
 
 class BaseHandler(tornado.web.RequestHandler):
-    """A base Tornado request handler with common functionality used by handlers in MHS outbound and MHS inbound."""
+    """A base Tornado request handler with common functionality used by handler."""
 
     def initialize(self):
         """Initialise this request handler with the provided dependencies.
-
-        :param workflows: The workflows to use to send messages.
-        :param config_manager: The object that can be used to obtain configuration details.
         """
 
 
