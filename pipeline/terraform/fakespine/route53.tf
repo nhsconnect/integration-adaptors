0754a9efda5a8c100dda7095c447d02efd2d8554
Łukasz Kwaśniewski <l.kwasniewski@kainos.com>
2020-04-16 13:55:43 +0200

    Disable LB, correct naming #2

--- pipeline/terraform/fakespine/route53.tf
+++ pipeline/terraform/fakespine/route53.tf
@@ -1,12 +1,12 @@
-# Route53 DNS record that is a domain name pointing to the fake spine load balancer
-resource "aws_route53_record" "fake_spine_load_balancer_record" {
-  zone_id = data.terraform_remote_state.mhs.outputs.mhs_hosted_zone_id
-  name = "fakespine.${data.terraform_remote_state.mhs.outputs.mhs_hosted_zone_name}"
-  type = "A"
+# # Route53 DNS record that is a domain name pointing to the fake spine load balancer
+# resource "aws_route53_record" "fake_spine_load_balancer_record" {
+#   zone_id = data.terraform_remote_state.mhs.outputs.mhs_hosted_zone_id
+#   name = "fakespine.${data.terraform_remote_state.mhs.outputs.mhs_hosted_zone_name}"
+#   type = "A"
 
-  alias {
-    name = aws_lb.fake_spine_alb.dns_name
-    zone_id = aws_lb.fake_spine_alb.zone_id
-    evaluate_target_health = false
-  }
-}
+#   alias {
+#     name = aws_lb.fake_spine_alb.dns_name
+#     zone_id = aws_lb.fake_spine_alb.zone_id
+#     evaluate_target_health = false
+#   }
+# }
