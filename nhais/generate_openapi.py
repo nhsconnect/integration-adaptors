01a16a88b5e83e0592df19158c6bba5a831b2d14
Broom94 <42891767+Broom94@users.noreply.github.com>
2020-04-17 14:25:43 +0100

    phir patient schema plus handling valid and invalid request

--- nhais/generate_openapi.py
+++ nhais/generate_openapi.py
@@ -1,27 +1,26 @@
 import json
 from jsonschema import validate
+import os
 
 import utilities.integration_adaptors_logger as log
 
 logger = log.IntegrationAdaptorsLogger(__name__)
 
 def validate_schema(request):
-    requestBody = json.loads(request)
-    schema = {
-        'type': 'object',
-        'properties': {
-            'payload': {'type': 'string'},
-            'address': {
-                'id': 'string',
-                'use': 'string'
-            }
-        },
-    }
+    request_body = json.loads(request)
+
+    ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+    json_schema_patient = ROOT_DIR + "/json-schema-patient.json"
+
+
+
+    with open(json_schema_patient) as file:
+        data = json.load(file)
 
     is_valid_schema = False
 
     try:
-        validate(instance=requestBody, schema=schema)
+        validate(instance=request_body, schema=data)
         is_valid_schema = True
     except Exception:
         logger.critical('Did not match json schema')
--- nhais/outbound/request/handler.py
+++ nhais/outbound/request/handler.py
@@ -5,7 +5,7 @@ from tornado import httputil
 from common.handler import base_handler
 
 from utilities import integration_adaptors_logger as log, timing
-from utilities import config
+from utilities import config, message_utilities
 
 import generate_openapi
 
@@ -31,5 +31,10 @@ class Handler(base_handler.BaseHandler):
         is_valid_schema = generate_openapi.validate_schema(self.request.body.decode())
         if(is_valid_schema):
             edifact = self.fhir_to_edifact.convert(self.request.body.decode())
+            unique_operation_id = message_utilities.get_uuid()
             await self.mesh_wrapper.send(edifact)
             self.set_status(202)
+            self.finish(f"<html><body>UniqueOperationID: {unique_operation_id}</body></html>")
+        else:
+            self.set_status(400)
+            self.finish("<html><body>Resource could not be parsed or failed basic FHIR validation rules (or multiple matches were found for conditional criteria)</body></html>")
