b87598fb63409cece39a02e599bbcbf944369492
Łukasz Kwaśniewski <l.kwasniewski@kainos.com>
2020-04-21 17:18:08 +0200

    Add ttl to the record

--- pipeline/terraform/fakespine-ec2/route53.tf
+++ pipeline/terraform/fakespine-ec2/route53.tf
@@ -3,5 +3,6 @@ resource "aws_route53_record" "fake_spine_load_balancer_record" {
   zone_id = data.terraform_remote_state.mhs.outputs.mhs_hosted_zone_id
   name = "fakespine.${data.terraform_remote_state.mhs.outputs.mhs_hosted_zone_name}"
   type = "A"
+  ttl = 300
   records = [ aws_instance.fake_spine_instance.private_ip ]
 }
