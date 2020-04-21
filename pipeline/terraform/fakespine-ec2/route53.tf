5788b8d16cb90616b4c8309c6a0b40c2e071ff56
Łukasz Kwaśniewski <l.kwasniewski@kainos.com>
2020-04-21 17:48:33 +0200

    Formatting, dummy commit to force rebuild

--- pipeline/terraform/fakespine-ec2/route53.tf
+++ pipeline/terraform/fakespine-ec2/route53.tf
@@ -1,8 +1,8 @@
 # Route53 DNS record that is a domain name pointing to the fake spine load balancer
 resource "aws_route53_record" "fake_spine_load_balancer_record" {
   zone_id = data.terraform_remote_state.mhs.outputs.mhs_hosted_zone_id
-  name = "fakespine.${data.terraform_remote_state.mhs.outputs.mhs_hosted_zone_name}"
-  type = "A"
-  ttl = 300
+  name    = "fakespine.${data.terraform_remote_state.mhs.outputs.mhs_hosted_zone_name}"
+  type    = "A"
+  ttl     = 300
   records = [ aws_instance.fake_spine_instance.private_ip ]
 }
