f57a4f6656b238c9e4d1895334bb2fa402fd2df9
Łukasz Kwaśniewski <l.kwasniewski@kainos.com>
2020-04-24 17:36:46 +0200

    Escaped characters for cert values

--- pipeline/terraform/fakespine-ec2/files/variables.sh
+++ pipeline/terraform/fakespine-ec2/files/variables.sh
@@ -11,7 +11,7 @@ echo "export FAKE_SPINE_OUTBOUND_SSL_ENABLED=${FAKE_SPINE_OUTBOUND_SSL_ENABLED}"
 echo "export FAKE_SPINE_PORT=${FAKE_SPINE_PORT}" >> /var/variables_source.sh
 
 # secret vars
-echo "export FAKE_SPINE_PRIVATE_KEY=$(aws secretsmanager get-secret-value --region ${REGION} --secret-id ${FAKE_SPINE_PRIVATE_KEY_ARN} --query SecretString --output text)" >> /var/variables_source.sh
-echo "export FAKE_SPINE_CERTIFICATE=$(aws secretsmanager get-secret-value --region ${REGION} --secret-id ${FAKE_SPINE_CERTIFICATE_ARN} --query SecretString --output text)" >> /var/variables_source.sh
-echo "export FAKE_SPINE_CA_STORE=$(aws secretsmanager get-secret-value --region ${REGION} --secret-id ${FAKE_SPINE_CA_STORE_ARN} --query SecretString --output text)" >> /var/variables_source.sh
+echo "export FAKE_SPINE_PRIVATE_KEY=\"$(aws secretsmanager get-secret-value --region ${REGION} --secret-id ${FAKE_SPINE_PRIVATE_KEY_ARN} --query SecretString --output text)\"" >> /var/variables_source.sh
+echo "export FAKE_SPINE_CERTIFICATE=\"$(aws secretsmanager get-secret-value --region ${REGION} --secret-id ${FAKE_SPINE_CERTIFICATE_ARN} --query SecretString --output text)\"" >> /var/variables_source.sh
+echo "export FAKE_SPINE_CA_STORE=\"$(aws secretsmanager get-secret-value --region ${REGION} --secret-id ${FAKE_SPINE_CA_STORE_ARN} --query SecretString --output text)\"" >> /var/variables_source.sh
 echo "export MHS_SECRET_PARTY_KEY=$(aws secretsmanager get-secret-value --region ${REGION} --secret-id ${MHS_SECRET_PARTY_KEY_ARN} --query SecretString --output text)" >> /var/variables_source.sh
