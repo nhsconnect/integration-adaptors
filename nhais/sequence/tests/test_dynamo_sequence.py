0bcc79782738f5ef4e6d3c49e8003fe72e27b100
alicjab <alicjab@kainos.com>
2020-04-25 19:43:55 +0200

    refactored unit tests for dynamo_sequence (extracted constants)

--- nhais/sequence/tests/test_dynamo_sequence.py
+++ nhais/sequence/tests/test_dynamo_sequence.py
@@ -11,49 +11,50 @@ class TestDynamoSequence(unittest.TestCase):
 
     def setUp(self):
         config.setup_config("MHS")
-        self.table_name = 'transaction_id_counter'
+        self.table_name = 'test_dynamo_sequence_table'
         self.key = 'transaction_id'
         self.endpoint = config.get_config('DYNAMODB_ENDPOINT_URL', None)
+        self.region_name = 'eu-west-2'
 
     @test_utilities.async_test
     async def test_transaction_ids_increase_by_one(self):
-        async with aioboto3.resource('dynamodb', region_name='eu-west-2',
+        async with aioboto3.resource('dynamodb', region_name=self.region_name,
                                      endpoint_url=self.endpoint) as dynamo_resource:
             try:
                 await self.create_table(dynamo_resource)
-                db = dynamo_sequence.DynamoSequenceGenerator('test_table')
+                db = dynamo_sequence.DynamoSequenceGenerator(self.table_name)
                 transaction_id_1 = await db.next(self.key)
                 transaction_id_2 = await db.next(self.key)
                 self.assertEqual(transaction_id_1 + 1, transaction_id_2)
             except dynamo_resource.meta.client.exceptions.ResourceInUseException:
                 self.fail()
             finally:
-                table = await dynamo_resource.Table('test_table')
+                table = await dynamo_resource.Table(self.table_name)
                 await table.delete()
 
     @test_utilities.async_test
     async def test_first_transaction_id_should_be_one(self):
-        async with aioboto3.resource('dynamodb', region_name='eu-west-2',
+        async with aioboto3.resource('dynamodb', region_name=self.region_name,
                                      endpoint_url=self.endpoint) as dynamo_resource:
             try:
                 await self.create_table(dynamo_resource)
-                db = dynamo_sequence.DynamoSequenceGenerator('test_table')
+                db = dynamo_sequence.DynamoSequenceGenerator(self.table_name)
                 transaction_id = await db.next(self.key)
                 self.assertEqual(transaction_id, 1)
             except dynamo_resource.meta.client.exceptions.ResourceInUseException:
                 self.fail()
             finally:
-                table = await dynamo_resource.Table('test_table')
+                table = await dynamo_resource.Table(self.table_name)
                 await table.delete()
 
     @test_utilities.async_test
     async def test_after_9999999_should_be_one(self):
-        async with aioboto3.resource('dynamodb', region_name='eu-west-2',
+        async with aioboto3.resource('dynamodb', region_name=self.region_name,
                                      endpoint_url=self.endpoint) as dynamo_resource:
             try:
                 await self.create_table(dynamo_resource)
-                db = dynamo_sequence.DynamoSequenceGenerator('test_table')
-                table = await dynamo_resource.Table('test_table')
+                db = dynamo_sequence.DynamoSequenceGenerator(self.table_name)
+                table = await dynamo_resource.Table(self.table_name)
                 await table.put_item(
                     Item={'key': self.key, dynamo_sequence._COUNTER_ATTRIBUTE: 9999999}
                 )
@@ -73,5 +74,5 @@ class TestDynamoSequence(unittest.TestCase):
                 {'AttributeName': 'key', 'KeyType': 'HASH'}
             ],
             ProvisionedThroughput={'ReadCapacityUnits': 1, 'WriteCapacityUnits': 1},
-            TableName='test_table'
+            TableName=self.table_name
         )
