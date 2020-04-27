356759c88fdc9f08d1b00a806514a846ef25260c
alicjab <alicjab@kainos.com>
2020-04-27 15:11:51 +0200

    removed unused import from dynamo_persistence_adaptor

--- common/persistence/dynamo_persistence_adaptor.py
+++ common/persistence/dynamo_persistence_adaptor.py
@@ -1,7 +1,6 @@
 """Module containing functionality for a DynamoDB implementation of a persistence adaptor."""
 import contextlib
 import json
-from typing import Optional
 
 import aioboto3
 import utilities.integration_adaptors_logger as log
