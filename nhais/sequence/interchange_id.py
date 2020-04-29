3fdd67acffa4a16b58b6584061aa2088edaa13b6
alicjab <alicjab@kainos.com>
2020-04-29 08:32:10 +0200

    added modules for generating Send Interchange Sequence and Send Message Sequence

---
+++ nhais/sequence/interchange_id.py
@@ -0,0 +1,15 @@
+from sequence.sequence_factory import get_sequence_generator
+
+
+class InterchangeIdGenerator:
+    """A component that provides sequential interchange id.
+    Definition:
+    SIS (Send Interchange Sequence) - sequence number for the entire EDIFACT interchange """
+
+    def __init__(self):
+        self.table_name = 'transaction_id_counter'
+
+    async def generate_outbound_id(self, sender, recipient) -> int:
+        key = "SIS-" + str(sender) + "-" + str(recipient)
+        interchange_generator = get_sequence_generator(self.table_name)
+        return await interchange_generator.next(key)
---
+++ nhais/sequence/message_id.py
@@ -0,0 +1,15 @@
+from sequence.sequence_factory import get_sequence_generator
+
+
+class MessageIdGenerator:
+    """A component that provides sequential message id
+    Definition:
+    SMS (Send Message Sequence) - sequence number applied for each message within an interchange"""
+
+    def __init__(self):
+        self.table_name = 'transaction_id_counter'
+
+    async def generate_outbound_id(self, sender, recipient) -> int:
+        key = "SMS-" + str(sender) + "-" + str(recipient)
+        message_generator = get_sequence_generator(self.table_name)
+        return await message_generator.next(key)
