bb4b52665cf20987eeb47b31e28d751234298a94
Grzegorz Robak <g.robak@kainos.com>
2020-05-15 09:43:03 +0200

    NIAD-23: Set fakespine request timeout to 2 seconds for inbound proxy calls

--- integration-tests/fake_spine/fake_spine/inbound_client.py
+++ integration-tests/fake_spine/fake_spine/inbound_client.py
@@ -29,6 +29,6 @@ class InboundClient(object):
                                                 method='POST',
                                                 body=request.body,
                                                 raise_error=True,
-                                                request_timeout=5,
+                                                request_timeout=2,
                                                 headers={**self.HEADERS, **request.headers}))
 
