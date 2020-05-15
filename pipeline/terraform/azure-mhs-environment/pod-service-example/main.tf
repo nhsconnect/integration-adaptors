5374f5e8ce156460f28b33a571120bdf10da8a84
Ali Jaffer <a.jaffer@kainos.com>
2020-05-15 17:55:38 +0100

    adding pod deployment to the nodes

--- pipeline/terraform/azure-mhs-environment/pod-service-example/main.tf
+++ pipeline/terraform/azure-mhs-environment/pod-service-example/main.tf
@@ -1,48 +1,127 @@
 provider "kubernetes" {}
 
-resource "kubernetes_namespace" "example" {
+resource "kubernetes_namespace" "mhs-adaptor" {
   metadata {
     name = "${var.namespace}"
   }
 }
-
-resource "kubernetes_pod" "web" {
+### Inbound ###
+resource "kubernetes_pod" "inbound" {
   metadata {
-    name = "nginx"
+    name = "inbound"
 
-    labels {
-      name = "nginx"
+    labels = {
+      name = "inbound"
     }
 
-    namespace = "${kubernetes_namespace.example.metadata.0.name}"
+    namespace = "${kubernetes_namespace.mhs-adaptor.metadata.0.name}"
   }
 
   spec {
     container {
-      image = "nginx:1.7.9"
-      name  = "nginx"
+      image = "nhsdev/nia-mhs-inbound:latest"
+      name  = "inbound"
     }
   }
 }
 
-resource "kubernetes_service" "web" {
+resource "kubernetes_service" "inbound" {
   metadata {
-    name      = "nginx"
-    namespace = "${kubernetes_namespace.example.metadata.0.name}"
+    name      = "inbound"
+    namespace = "${kubernetes_namespace.mhs-adaptor.metadata.0.name}"
   }
 
   spec {
-    selector {
-      name = "${kubernetes_pod.web.metadata.0.labels.name}"
+    selector = {
+      name = "${kubernetes_pod.inbound.metadata.0.labels.name}"
     }
 
-    session_affinity = "ClientIP"
+    port {
+      port        = 80
+      target_port = 80
+   }
+    # port {
+    #   port        = 443
+    #   target_port = 443
+    # }
+
+  #   type = "LoadBalancer"
+  }
+}
+
+### Outbound ####
+resource "kubernetes_pod" "outbound" {
+  metadata {
+    name = "outbound"
+
+    labels = {
+      name = "outbound"
+    }
+
+    namespace = "${kubernetes_namespace.mhs-adaptor.metadata.0.name}"
+  }
+
+  spec {
+    container {
+      image = "nhsdev/nia-mhs-outbound:latest"
+      name  = "outbound"
+    }
+  }
+}
+
+resource "kubernetes_service" "outbound" {
+  metadata {
+    name      = "outbound"
+    namespace = "${kubernetes_namespace.mhs-adaptor.metadata.0.name}"
+  }
+
+  spec {
+    selector = {
+      name = "${kubernetes_pod.outbound.metadata.0.labels.name}"
+    }
+
+    port {
+      port        = 80
+      target_port = 80
+    }
+  }
+}
+
+### Route ####
+resource "kubernetes_pod" "route" {
+  metadata {
+    name = "route"
+
+    labels = {
+      name = "route"
+    }
+
+    namespace = "${kubernetes_namespace.mhs-adaptor.metadata.0.name}"
+  }
+
+  spec {
+    container {
+      image = "nhsdev/nia-mhs-route:latest"
+      name  = "route"
+    }
+  }
+}
+
+resource "kubernetes_service" "route" {
+  metadata {
+    name      = "route"
+    namespace = "${kubernetes_namespace.mhs-adaptor.metadata.0.name}"
+  }
+
+  spec {
+    selector = {
+      name = "${kubernetes_pod.route.metadata.0.labels.name}"
+    }
 
     port {
       port        = 80
       target_port = 80
     }
 
-    type = "LoadBalancer"
   }
 }
