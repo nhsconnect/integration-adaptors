b67e2d7724258b06c1f424070141105f780fe0bd
Ali Jaffer <a.jaffer@kainos.com>
2020-05-18 17:09:48 +0100

    added async table and changed the prefix

--- pipeline/terraform/azure-mhs-environment/cosmos-db.tf
+++ pipeline/terraform/azure-mhs-environment/cosmos-db.tf
@@ -1,5 +1,5 @@
-resource "azurerm_cosmosdb_account" "cosmos_db" {
-  name                = var.cluster_name
+resource "azurerm_cosmosdb_account" "mhs_state_table" {
+  name                = "${var.cluster_name}-mhs-state"
   location            = azurerm_resource_group.mhs_adaptor.location
   resource_group_name = azurerm_resource_group.mhs_adaptor.name
   offer_type          = "Standard"
@@ -12,7 +12,26 @@ resource "azurerm_cosmosdb_account" "cosmos_db" {
   }
 
   geo_location {
-    prefix            = "${var.cluster_name}-customid"
+    prefix            = "${var.cluster_name}-mhs-state"
+    location          = azurerm_resource_group.mhs_adaptor.location
+    failover_priority = 0
+  }
+}
+resource "azurerm_cosmosdb_account" "mhs_sync_async_table" {
+  name                = "${var.cluster_name}-mhs-sync-async-state"
+  location            = azurerm_resource_group.mhs_adaptor.location
+  resource_group_name = azurerm_resource_group.mhs_adaptor.name
+  offer_type          = "Standard"
+  kind                = "GlobalDocumentDB"
+
+  consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 10
+    max_staleness_prefix    = 200
+  }
+
+  geo_location {
+    prefix            = "${var.cluster_name}-mhs-sync-async-state"
     location          = azurerm_resource_group.mhs_adaptor.location
     failover_priority = 0
   }
