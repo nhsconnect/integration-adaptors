ae69e933fcf03e589784250009bd390df23b0984
Łukasz Kwaśniewski <l.kwasniewski@kainos.com>
2020-06-02 16:27:30 +0200

    Disable nhais ecr variable #2

--- terraform/aws/components/account/ecr_lifecycle_nhais.tf
+++ terraform/aws/components/account/ecr_lifecycle_nhais.tf
@@ -1,23 +1,23 @@
-resource "aws_ecr_lifecycle_policy" "nhais_policy" {
-  repository = aws_ecr_repository.nhais_ecr_repository.name 
-  policy = jsonencode(
-    {
-      rules = [
-        {
-          rulePriority = 1
-          description = "Expire images from PRs older than 14 days"
-          selection = {
-            tagStatus = "tagged"
-            tagPrefixList = ["PR"]
-            countType = "sinceImagePushed"
-            countUnit = "days"
-            countNumber = 14
-          }
-          action = {
-            type = "expire"
-          }
-        }
-      ]
-    }
-  )
-}
+# resource "aws_ecr_lifecycle_policy" "nhais_policy" {
+#   repository = aws_ecr_repository.nhais_ecr_repository.name 
+#   policy = jsonencode(
+#     {
+#       rules = [
+#         {
+#           rulePriority = 1
+#           description = "Expire images from PRs older than 14 days"
+#           selection = {
+#             tagStatus = "tagged"
+#             tagPrefixList = ["PR"]
+#             countType = "sinceImagePushed"
+#             countUnit = "days"
+#             countNumber = 14
+#           }
+#           action = {
+#             type = "expire"
+#           }
+#         }
+#       ]
+#     }
+#   )
+# }
