d2480b54a8c24451f36171df80e63fd47d93ae77
Łukasz Kwaśniewski <l.kwasniewski@kainos.com>
2020-06-04 17:25:37 +0200

    Initial resources for 111 component

--- terraform/aws/components/111/locals.tf
+++ terraform/aws/components/111/locals.tf
@@ -10,7 +10,7 @@ locals {
   availability_zones = ["${var.region}a", "${var.region}b", "${var.region}c"]
 
   # Use below when the ECR repo is created by terraform in account component.
-  image_name = "${data.terraform_remote_state.account.outputs.nhais_ecr_repo_url}:${var.build_id}"
+  image_name = "${data.terraform_remote_state.account.outputs.ecr_repo_url_111}:${var.build_id}"
 
   subnet_cidrs = [
     cidrsubnet(data.terraform_remote_state.base.outputs.vpc_cidr,3,4),
