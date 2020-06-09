548320c4cf6a5a393618d4137496d86e63b87ef8
Ali Jaffer <a.jaffer@kainos.com>
2020-06-09 09:20:09 +0100

    further secrets amendments

--- terraform/aws/components/nhais/locals_env_variables.tf
+++ terraform/aws/components/nhais/locals_env_variables.tf
@@ -43,14 +43,14 @@ locals {
     {
       name = "NHAIS_MONGO_OPTIONS"
       value = var.mongo_options
-    },
-    {
-      name = "NHAIS_MONGO_USERNAME1"
-      value = var.docdb_master_user
-    },
-    {
-      name = "NHAIS_MONGO_PASSWORD1"
-      value = var.docdb_master_password
     }
+    # {
+    #   name = "NHAIS_MONGO_USERNAME"
+    #   value = var.docdb_master_user
+    # },
+    # {
+    #   name = "NHAIS_MONGO_PASSWORD"
+    #   value = var.docdb_master_password
+    # }
   ])
 }
