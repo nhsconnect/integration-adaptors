1cc35417ac0b18f943e64df013aaf92e5788b679
Łukasz Kwaśniewski <l.kwasniewski@kainos.com>
2020-06-15 17:20:15 +0200

    Create subnets in each availabilty zones

--- terraform/aws/components/base/subnet_base.tf
+++ terraform/aws/components/base/subnet_base.tf
@@ -3,6 +3,7 @@ resource "aws_subnet" "base_subnet" {
   count = length(local.availability_zones)
   vpc_id = aws_vpc.base_vpc.id
   cidr_block = cidrsubnet(aws_vpc.base_vpc.cidr_block,3,count.index)
+  availability_zone = local.availability_zones[count.index]
 
   tags = merge(local.default_tags, {
     Name = "${local.resource_prefix}-subnet-${count.index}"
