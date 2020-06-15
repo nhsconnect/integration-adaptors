102a091797b4a5b7e73701f6d10b8335a40dcb3c
Łukasz Kwaśniewski <l.kwasniewski@kainos.com>
2020-06-15 14:10:00 +0200

    routes are always created

--- terraform/aws/components/base/route_table_public.tf
+++ terraform/aws/components/base/route_table_public.tf
@@ -1,5 +1,4 @@
 resource "aws_route_table" "public" {
-  count = var.enable_internet_access ? 1 : 0
   vpc_id = aws_vpc.base_vpc.id
   tags = merge(local.default_tags, {
     Name = "${local.resource_prefix}-public_rt"
@@ -7,7 +6,6 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route_table_association" "public_route_public_subnet" {
-  count = var.enable_internet_access ? 1 : 0
   subnet_id = aws_subnet.nat_gw_subnet.id
   route_table_id = aws_route_table.public.id 
 }
