451ed51d1da0006072b53b6b3a8003c54fe402d8
Łukasz Kwaśniewski <l.kwasniewski@kainos.com>
2020-06-15 16:37:44 +0200

    Rename nat gw subnet to public subnet

--- terraform/aws/components/base/routes_private.tf
+++ terraform/aws/components/base/routes_private.tf
@@ -2,7 +2,7 @@ resource "aws_route" "route_private_to_nat_gw" {
   count = var.enable_internet_access ? 1 : 0
   route_table_id = aws_route_table.private.id
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id = aws_nat_gateway.nat_gw.id
+  nat_gateway_id = aws_nat_gateway.nat_gw[0].id
 }
 
 resource "aws_vpc_endpoint_route_table_association" "s3_vpce_on_private_rt" {
