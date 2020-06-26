8163d6c7846a90759ecba91cea268dd81c108f60
Łukasz Kwaśniewski <l.kwasniewski@kainos.com>
2020-06-26 15:16:47 +0200

    Add commands for getting username and password for DocDB

--- terraform/aws/components/account/files/jumpbox.sh
+++ terraform/aws/components/account/files/jumpbox.sh
@@ -6,15 +6,18 @@
 echo "Update the system"
 sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 yum makecache
-yum update
+yum update -y
 
 echo "Install software"
 yum install -y docker git mongodb python3
 pip3 install awscli
 
 echo "Setup MOTD"
+echo "  " >> /etc/motd
 echo "Welcome to NIA Jumpbox" >> /etc/motd
 echo "  " >> /etc/motd
 echo "Useful commands:" >> /etc/motd
-echo "List all DocDB clusters endpoints in the account: aws docdb describe-db-clusters --region eu-west-2 --query 'DBClusters[].Endpoint'" >> /etc/motd
-echo "List LoadBalancer endpoints for applications: aws elbv2 describe-load-balancers --region eu-west-2 --query 'LoadBalancers[].DNSName' | grep nia" >> /etc/motd
+echo "List all DocDB clusters endpoints in the account: \t aws docdb describe-db-clusters --region eu-west-2 --query 'DBClusters[].Endpoint'" >> /etc/motd
+echo "Get the username for DocDB: aws secretsmanager \t get-secret-value --region eu-west-2 --secret-id docdb-master-username --query 'SecretString'" >> /etc/motd
+echo "Get the password for DocDB: aws secretsmanager \t get-secret-value --region eu-west-2 --secret-id docdb-master-password --query 'SecretString'" >> /etc/motd
+echo "List LoadBalancer endpoints for applications: \t aws elbv2 describe-load-balancers --region eu-west-2 --query 'LoadBalancers[].DNSName' | grep nia" >> /etc/motd
