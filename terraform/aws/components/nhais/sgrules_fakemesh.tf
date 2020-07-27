a6adf3635fc913b9336999a903152c57e01c2508
Łukasz Kwaśniewski <l.kwasniewski@kainos.com>
2020-07-27 15:08:50 +0200

    Add SG rules for fake-mesh/nhais and fake-mesh jumpbox

--- terraform/aws/components/nhais/sgrules_fakemesh.tf
+++ terraform/aws/components/nhais/sgrules_fakemesh.tf
@@ -17,3 +17,14 @@ resource "aws_security_group_rule" "nhais_to_fakemesh"{
     data.terraform_remote_state.base.outputs.nhais_cidr
   ]
 }
+
+resource "aws_security_group_rule" "jumpbox_to_fakemesh"{
+  type = "egress"
+  from_port = 8829
+  to_port = 8829
+  protocol = "TCP"
+  security_group_id = data.terraform_remote_state.account.outputs.jumpbox_sg_id
+  cidr_blocks = [
+    data.terraform_remote_state.base.outputs.nhais_cidr
+  ]
+}
