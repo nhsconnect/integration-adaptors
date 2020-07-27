ed2d1631a0b0b73515343b32c30bdbe64b60b015
Łukasz Kwaśniewski <l.kwasniewski@kainos.com>
2020-07-27 16:44:18 +0200

    Add SG rules for fake-mesh/nhais and fake-mesh jumpbox

--- terraform/aws/modules/module_ecs_service/sg_rules.tf
+++ terraform/aws/modules/module_ecs_service/sg_rules.tf
@@ -37,3 +37,35 @@ resource "aws_security_group_rule" "healthcheck_to_service_from_load_balancer" {
   security_group_id = aws_security_group.service_sg.id
   source_security_group_id = aws_security_group.service_lb_sg.id  
 }
+
+# Healthcheck rules in case of Network LB
+
+# resource "aws_security_group_rule" "healthcheck_from_netwrok_load_balancer_to_service" {
+#   count = var.container_port == local.healthcheck_port ? 0 : local.network_lb
+#   type = "egress"
+#   from_port = local.healthcheck_port
+#   to_port = local.healthcheck_port
+#   protocol = var.container_protocol
+#   security_group_id = aws_security_group.service_lb_sg.id
+#   source_security_group_id = aws_security_group.service_sg.id
+# }
+
+resource "aws_security_group_rule" "healthcheck_to_service_from_network_load_balancer" {
+  count = var.container_port == local.healthcheck_port ? 0 : local.network_lb
+  type = "ingress"
+  from_port = local.healthcheck_port
+  to_port = local.healthcheck_port
+  protocol = var.container_protocol
+  security_group_id = aws_security_group.service_sg.id
+  #source_security_group_id = aws_security_group.service_lb_sg.id 
+  cidr_blocks = [
+    data.aws_subnet.lb_subnets[0].cidr_block
+    data.aws_subnet.lb_subnets[1].cidr_block
+    data.aws_subnet.lb_subnets[2].cidr_block
+  ]
+}
+
+data "aws_subnet" "lb_subnets" {
+  count = length(var.lb_subnets)
+  id = var.lb_subnets[count.index]
+}
