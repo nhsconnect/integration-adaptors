a405b794b061781ee283670275d991c33d52a104
Łukasz Kwaśniewski <l.kwasniewski@kainos.com>
2020-07-27 14:34:03 +0200

    Add SG rules for fake-mesh/nhais and fake-mesh jumpbox

---
+++ terraform/aws/components/nhais/sgrules_fakemesh.tf
@@ -0,0 +1,19 @@
+resource "aws_security_group" "nhais_external_access" {
+  name = "${local.resource_prefix}-nhais_access_sg"
+  vpc_id = vpc_id = data.terraform_remote_state.base.outputs.vpc_id
+  tags = merge(local.default_tags, {
+    Name = "${local.resource_prefix}-nhais_access_sg"
+  })
+  description = "SG for additional nhais access in env: ${var.environment}"
+}
+
+resource "aws_security_group_rule" {
+  type = "egress"
+  from_port = 8829
+  to_port = 8829
+  protocol = "TCP"
+  security_group_id = aws_security_group.nhais_external_access.id
+  cidr_blocks = [
+    data.terraform_remote_state.base.outputs.nhais_cidr
+  ]
+}
