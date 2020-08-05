086b8502b4f71ffd4009d8380f2f8f93306ae98a
Łukasz Kwaśniewski <l.kwasniewski@kainos.com>
2020-08-05 16:03:37 +0200

    Add logs config for nginx

--- terraform/aws/components/OneOneOne/locals_nginx_container.tf
+++ terraform/aws/components/OneOneOne/locals_nginx_container.tf
@@ -50,7 +50,7 @@ locals {
 resource "aws_cloudwatch_log_group" "ecs_service_cw_log_group" {
   name = "/ecs/${local.resource_prefix}"
 
-  retention_in_days = var.logs_retention
+  retention_in_days = "14"
   tags = merge(local.default_tags, {
     Name = "${local.resource_prefix}-cw_log_group"
   })
