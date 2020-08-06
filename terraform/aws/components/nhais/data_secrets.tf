f7b76760912a1fa75f9335f4509a128a9a4af330
Matt Dooner <mattd@kainos.com>
2020-08-06 13:30:21 +0100

    NIAD-526 nhais mesh vars namespaced by env

--- terraform/aws/components/nhais/data_secrets.tf
+++ terraform/aws/components/nhais/data_secrets.tf
@@ -15,25 +15,25 @@ data "aws_secretsmanager_secret" "docdb_master_password" {
 }
 
 data "aws_secretsmanager_secret" "nhais_mesh_mailbox_id" {
-  name = "nhais_mesh_mailbox_id"
+  name = "nhais_${var.environment}_mesh_mailbox_id"
 }
 
 data "aws_secretsmanager_secret" "nhais_mesh_endpoint_private_key" {
-  name = "nhais_mesh_endpoint_private_key"
+  name = "nhais_${var.environment}_mesh_endpoint_private_key"
 }
 
 data "aws_secretsmanager_secret" "nhais_mesh_shared_key" {
-  name = "nhais_mesh_shared_key"
+  name = "nhais_${var.environment}_mesh_shared_key"
 }
 
 data "aws_secretsmanager_secret" "nhais_mesh_mailbox_password" {
-  name = "nhais_mesh_mailbox_password"
+  name = "nhais_${var.environment}_mesh_mailbox_password"
 }
 
 data "aws_secretsmanager_secret" "nhais_mesh_endpoint_cert" {
-  name = "nhais_mesh_endpoint_cert"
+  name = "nhais_${var.environment}_mesh_endpoint_cert"
 }
 
 data "aws_secretsmanager_secret" "nhais_mesh_recipient_codes" {
-  name = "nhais_mesh_recipient_codes"
+  name = "nhais_${var.environment}_mesh_recipient_codes"
 }
