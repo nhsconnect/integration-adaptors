5df4bf02376d017bc856d0f801b1bbd23ccac757
Ali Jaffer <a.jaffer@kainos.com>
2020-08-06 16:29:55 +0100

    Deleting route table associations as its not needed now

--- terraform/aws/components/nhais-responder/route_table_assoc.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_route_table_association" "private_route_base_subnet" {
-  count = length(local.availability_zones)
-  subnet_id = aws_subnet.service_subnet[count.index].id
-  route_table_id = data.terraform_remote_state.base.outputs.private_nat_gw_route_table
-}
