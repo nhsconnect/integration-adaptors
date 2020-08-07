fcc2c3e2174eb62728a87a2a7f211456c3fc24ac
Ali Jaffer <a.jaffer@kainos.com>
2020-08-07 14:45:08 +0100

    Name change and other cleanup

---
+++ terraform/aws/components/nhais_responder/locals_secret_variables.tf
@@ -0,0 +1,44 @@
+locals {
+    secret_variables = [
+    {
+      name = "NHAIS_AMQP_USERNAME"
+      valueFrom = data.aws_secretsmanager_secret.mq_username.arn
+    },
+    {
+      name = "NHAIS_AMQP_PASSWORD"
+      valueFrom = data.aws_secretsmanager_secret.mq_password.arn
+    },
+    {
+      name = "NHAIS_MONGO_USERNAME"
+      valueFrom = data.aws_secretsmanager_secret.docdb_master_username.arn
+    },
+    {
+      name = "NHAIS_MONGO_PASSWORD"
+      valueFrom = data.aws_secretsmanager_secret.docdb_master_password.arn
+    },
+    {
+      name = "NHAIS_MESH_MAILBOX_ID"
+      valueFrom = data.aws_secretsmanager_secret.nhais_mesh_mailbox_id.arn
+    },
+    {
+      name = "NHAIS_MESH_MAILBOX_PASSWORD"
+      valueFrom = data.aws_secretsmanager_secret.nhais_mesh_mailbox_password.arn
+    },
+    {
+      name = "NHAIS_MESH_SHARED_KEY"
+      valueFrom = data.aws_secretsmanager_secret.nhais_mesh_shared_key.arn
+    },
+    {
+      name = "NHAIS_MESH_ENDPOINT_CERT"
+      valueFrom = data.aws_secretsmanager_secret.nhais_mesh_endpoint_cert.arn
+    },
+    {
+      name = "NHAIS_MESH_CYPHER_TO_MAILBOX"
+      valueFrom = data.aws_secretsmanager_secret.nhais_mesh_recipient_codes.arn
+    },
+    {
+      name = "NHAIS_MESH_ENDPOINT_PRIVATE_KEY"
+      valueFrom = data.aws_secretsmanager_secret.nhais_mesh_endpoint_private_key.arn
+    }
+  ]
+}
