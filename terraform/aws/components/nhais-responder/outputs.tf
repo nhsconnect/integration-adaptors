d5d1ce75c9170f88e1adc337db94e54864eba7b4
Ali Jaffer <a.jaffer@kainos.com>
2020-08-07 12:16:15 +0100

    Cleaning up

--- terraform/aws/components/nhais-responder/outputs.tf
+++ terraform/aws/components/nhais-responder/outputs.tf
@@ -1,17 +1,17 @@
 output "nhais-responder_url" {
   value = aws_route53_record.nhais-responder_lb_record.name
-  description = "The DNS name of the Route53 record pointing to the NHAIS service's load balancer."
+  description = "The DNS name of the Route53 record pointing to the NHAIS-Responder service's load balancer."
 }
 
 output "nhais_lb_sg_id" {
   value = module.nhais-responder_ecs_service.loadbalancer_sg_id
-  description = "Security Group ID for NHAIS load balancer, use it create rules to allow other components to connect"
+  description = "Security Group ID for NHAIS-Respomder load balancer, use it create rules to allow other components to connect"
 }
 # TODO - In case of chicken and egg situation - cross dependency between components - create SG and rules in base components and supply the SG IDs to components and ECS modules
 
 output "nhais-responder_lb_port" {
   value = var.nhais-responder_service_application_port
-  description = "Port on which NHAIS LB is listening"
+  description = "Port on which NHAIS-Responder LB is listening"
 }
 
 output "nhais-responder_lb_arn" {
@@ -26,10 +26,5 @@ output "nhais-responder_lb_target_group_arn" {
 
 output "nhais-responder_external_access_sg_id" {
   value = aws_security_group.nhais-responder_external_access.id
-  description = "ID of the security group allowing NHAIS to call other services"
+  description = "ID of the security group allowing NHAIS-Responder to call other services"
 }
-
-# output "nhais_service_subnets" {
-#  value = aws_subnet.service_subnet.*.id
-#  description = "Subnets used by NHAIS service"
-# }
