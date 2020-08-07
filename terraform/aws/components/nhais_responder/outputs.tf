38e7d5a9186bdb0af7adce0036eb188fcb6de87e
Ali Jaffer <a.jaffer@kainos.com>
2020-08-07 14:47:20 +0100

    More clean up

--- terraform/aws/components/nhais_responder/outputs.tf
+++ terraform/aws/components/nhais_responder/outputs.tf
@@ -5,7 +5,7 @@ output "nhais_responder_url" {
 
 output "nhais_lb_sg_id" {
   value = module.nhais_responder_ecs_service.loadbalancer_sg_id
-  description = "Security Group ID for NHAIS-Respomder load balancer, use it create rules to allow other components to connect"
+  description = "Security Group ID for NHAIS_Responder load balancer, use it create rules to allow other components to connect"
 }
 # TODO - In case of chicken and egg situation - cross dependency between components - create SG and rules in base components and supply the SG IDs to components and ECS modules
 
