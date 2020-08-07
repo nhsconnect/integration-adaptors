c94ea5925c9ca82ab88019e447f78458c418f59c
Ali Jaffer <a.jaffer@kainos.com>
2020-08-07 12:19:53 +0100

    More cleanup

--- terraform/aws/components/nhais-responder/security_groups.tf
+++ terraform/aws/components/nhais-responder/security_groups.tf
@@ -4,5 +4,5 @@ resource "aws_security_group" "nhais-responder_external_access" {
   tags = merge(local.default_tags, {
     Name = "${local.resource_prefix}-nhais-responder_access_sg"
   })
-  description = "SG for additional nhais access in env: ${var.environment}"
+  description = "SG for additional nhais-responder access in env: ${var.environment}"
 }
--- terraform/aws/components/nhais-responder/task_role.tf
+++ terraform/aws/components/nhais-responder/task_role.tf
@@ -2,4 +2,4 @@ data "aws_iam_role" "ecs_service_task_role" {
   name = "MHSTaskRole"
 }
 
-# uses the same Task Role as MHS - TODO check what is needed and create separate role for NHAIS 
+# uses the same Task Role as MHS - TODO check what is needed and create separate role for NHAIS & Responder
