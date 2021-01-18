a9e32f2e9cd6205baf09c6d7f76ab71c1f5f8e2c
Łukasz Kwaśniewski <l.kwasniewski@kainos.com>
2021-01-18 11:34:33 +0100

    queues prefixed with environment

--- terraform/aws/components/mhs/locals_env_variables.tf
+++ terraform/aws/components/mhs/locals_env_variables.tf
@@ -7,11 +7,11 @@ locals {
     },
     {
       name = "MHS_STATE_TABLE_NAME"
-      value = "mhs_state"
+      value = "${var.evinronment}_mhs_state"
     },
     {
       name = "MHS_SYNC_ASYNC_STATE_TABLE_NAME"
-      value = "mhs_sync_async"
+      value = "${var.evinronment}_mhs_sync_async"
     },
     {
       name = "MHS_PERSISTENCE_ADAPTOR"
@@ -77,7 +77,7 @@ locals {
     },
     {
       name = "MHS_INBOUND_QUEUE_NAME"
-      value = "mhs-inbound"
+      value = "${var.evinronment}_mhs_inbound"
     }
   ])
 
@@ -104,116 +104,3 @@ locals {
     }
   ])
 }
-
-/*
-
-    {
-      name = "MHS_RESYNC_RETRIES"
-      value = var.mhs_resynchroniser_max_retries
-    },
-    {
-      name = "MHS_RESYNC_INTERVAL"
-      value = var.mhs_resynchroniser_interval
-    },
-    {
-      name = "MHS_SPINE_ORG_CODE"
-      value = var.mhs_spine_org_code
-    },
-    {
-      name = "MHS_SPINE_REQUEST_MAX_SIZE"
-      value = var.mhs_spine_request_max_size
-    },
-    {
-      name = "MHS_FORWARD_RELIABLE_ENDPOINT_URL"
-      value = var.mhs_forward_reliable_endpoint_url
-    },
-
-  ]
-  mhs_outbound_base_secrets = [
-    {
-      name = "MHS_SECRET_PARTY_KEY"
-      valueFrom = var.party_key_arn
-    },
-    {
-      name = "MHS_SECRET_CLIENT_CERT"
-      valueFrom = var.client_cert_arn
-    },
-    {
-      name = "MHS_SECRET_CLIENT_KEY"
-      valueFrom = var.client_key_arn
-    },
-    {
-      name = "MHS_SECRET_CA_CERTS"
-      valueFrom = var.ca_certs_arn
-    }
-
-
-
-*/
-/*
-     - env:
-        - name: MHS_INBOUND_QUEUE_BROKERS
-          valueFrom:
-            secretKeyRef:
-              name: mhs-queue
-              key: broker
-        - name: MHS_INBOUND_QUEUE_NAME
-          valueFrom:
-            secretKeyRef:
-              name: mhs-queue
-              key: queue
-        - name: MHS_SECRET_INBOUND_QUEUE_USERNAME
-          valueFrom:
-            secretKeyRef:
-              name: mhs-queue
-              key: username
-        - name: MHS_SECRET_INBOUND_QUEUE_PASSWORD
-          valueFrom:
-            secretKeyRef:
-              name: mhs-queue
-              key: password
-        - name: MHS_LOG_LEVEL
-          value: DEBUG
-        - name: MHS_STATE_TABLE_NAME
-          value: mhs_state
-        - name: MHS_SYNC_ASYNC_STATE_TABLE_NAME
-          value: sync_async_state
-        - name: MHS_SECRET_PARTY_KEY
-          valueFrom:
-            secretKeyRef:
-              name: mhs-partykey
-              key: partyKey
-        - name: MHS_SECRET_CLIENT_CERT
-          valueFrom:
-            secretKeyRef:
-              name: mhs-client-cert
-              key: tls.crt
-        - name: MHS_SECRET_CLIENT_KEY	
-          valueFrom:
-            secretKeyRef:
-              name: mhs-client-cert
-              key: tls.key
-        - name: MHS_SECRET_CA_CERTS
-          valueFrom:
-            secretKeyRef:
-              name: mhs-ca-certs
-              key: ca-certs
-        - name: MHS_DB_ENDPOINT_URL
-          valueFrom:
-            secretKeyRef:
-              name: mhs-database
-              key: connectionString
-        - name: MHS_PERSISTENCE_ADAPTOR
-          value: mongodb
-        - name: MHS_INBOUND_USE_SSL
-          value: "false"
-        - name: MHS_INBOUND_QUEUE_MESSAGE_TTL_IN_SECONDS
-          value: "0"
-        - name: SERVICE_PORTS
-          value: 443,80
-        - name: TCP_PORTS
-          value: "443"
-
-
-
-*/
