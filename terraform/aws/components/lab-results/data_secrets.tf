406e05634647184d97a15b74ba8623d423dbd8b2
RIashfaqahmed <25818860+RIashfaqahmed@users.noreply.github.com>
2021-02-03 16:31:09 +0000

    Update data_secrets.tf

--- terraform/aws/components/lab-results/data_secrets.tf
+++ terraform/aws/components/lab-results/data_secrets.tf
@@ -18,8 +18,8 @@ data "aws_secretsmanager_secret" "lab-results_mesh_mailbox_id" {
   name = "lab-results_${var.environment}_mesh_mailbox_id"
 }
 
-data "aws_secretsmanager_secret" "nhais_mesh_endpoint_private_key" {
-  name = "nhais_${var.environment}_mesh_endpoint_private_key"
+data "aws_secretsmanager_secret" "lab-results_mesh_endpoint_private_key" {
+  name = "lab-results_${var.environment}_mesh_endpoint_private_key"
 }
 
 data "aws_secretsmanager_secret" "lab-results_mesh_shared_key" {
@@ -30,16 +30,16 @@ data "aws_secretsmanager_secret" "lab-results_mesh_mailbox_password" {
   name = "lab-results_${var.environment}_mesh_mailbox_password"
 }
 
-data "aws_secretsmanager_secret" "nhais_mesh_endpoint_cert" {
-  name = "nhais_${var.environment}_mesh_endpoint_cert"
+data "aws_secretsmanager_secret" "lab-results_mesh_endpoint_cert" {
+  name = "lab-results_${var.environment}_mesh_endpoint_cert"
 }
 
-data "aws_secretsmanager_secret" "nhais_mesh_sub_ca" {
-  name = "nhais_${var.environment}_mesh_sub_ca"
+data "aws_secretsmanager_secret" "lab-results_mesh_sub_ca" {
+  name = "lab-results_${var.environment}_mesh_sub_ca"
 }
 
-data "aws_secretsmanager_secret" "nhais_mesh_recipient_codes" {
-  name = "nhais_${var.environment}_mesh_recipient_codes"
+data "aws_secretsmanager_secret" "lab-results_mesh_recipient_codes" {
+  name = "lab-results_${var.environment}_mesh_recipient_codes"
 }
 
 data "aws_secretsmanager_secret" "lab-results_ssl_trust_store_password" {
