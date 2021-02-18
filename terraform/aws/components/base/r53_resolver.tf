9410db295276fe093202bb37d0c1e4c24b72d752
Łukasz Kwaśniewski <l.kwasniewski@kainos.com>
2021-02-18 16:22:09 +0100

    Dynamic setting in r53 resolver

--- terraform/aws/components/base/r53_resolver.tf
+++ terraform/aws/components/base/r53_resolver.tf
@@ -7,18 +7,32 @@ resource "aws_route53_resolver_endpoint" "nhs_resolver" {
     aws_security_group.r53_resolver_sg[0].id,
   ]
 
+  dynamic "ip_address" {
+    for_each = aws_subnet.service_containers_subnet.*.id
+    content {
+      subnet_id = ip_address.value
+    }
+  }
+
+  dynamic "ip_address" {
+    for_each = aws_subnet.service_lb_subnet.*.id
+    content {
+      subnet_id = ip_address.value
+    }
+  }
+
   # TODO change to dynamic - although tricky
-  ip_address {
-    subnet_id = aws_subnet.service_containers_subnet[0].id
-  }
+  # ip_address {
+  #   subnet_id = aws_subnet.service_containers_subnet[0].id
+  # }
 
-  ip_address {
-    subnet_id = aws_subnet.service_containers_subnet[1].id
-  }
+  # ip_address {
+  #   subnet_id = aws_subnet.service_containers_subnet[1].id
+  # }
 
-  ip_address {
-    subnet_id = aws_subnet.service_containers_subnet[2].id
-  }
+  # ip_address {
+  #   subnet_id = aws_subnet.service_containers_subnet[2].id
+  # }
 
   # ip_address {
   #   subnet_id = aws_subnet.service_lb_subnet[0].id
@@ -40,13 +54,20 @@ resource "aws_route53_resolver_rule" "nhs_dns_resolver_rule" {
   name = "${local.resource_prefix}-r53_resolver_rule"
   resolver_endpoint_id = aws_route53_resolver_endpoint.nhs_resolver[0].id
 
-  target_ip {
-    ip = var.ptl_dns_servers[0]
+  dynamic "target_ip" {
+    for_each = var.ptl_dns_servers
+    content {
+      ip = target_ip.value
+    }
   }
 
-  target_ip {
-    ip = var.ptl_dns_servers[1]
-  }
+  # target_ip {
+  #   ip = var.ptl_dns_servers[0]
+  # }
+
+  # target_ip {
+  #   ip = var.ptl_dns_servers[1]
+  # }
 
   tags = merge(local.default_tags, {
     Name = "${local.resource_prefix}-r53_resolver_rule"
