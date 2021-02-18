49488170385f9089866cbd59e1a0c3205b725e7f
Łukasz Kwaśniewski <l.kwasniewski@kainos.com>
2021-02-18 15:29:28 +0100

    Add option to set static private IPs on Inbound loadbalancer #4

--- terraform/aws/modules/module_ecs_service/lb.tf
+++ terraform/aws/modules/module_ecs_service/lb.tf
@@ -5,23 +5,23 @@ resource "aws_lb" "service_load_balancer" {
   load_balancer_type = var.load_balancer_type
   security_groups = local.lb_sgs
 
-  # dynamic "subnet_mapping" {
-  #   for_each = local.lb_subnets_to_private_ips
-  #   content {
-  #     subnet_id = subnet_mapping.key
-  #     private_ipv4_address = subnet_mapping.value != "auto" ?  subnet_mapping.value : null
-  #   }
+  dynamic "subnet_mapping" {
+    for_each = local.lb_subnets_to_private_ips
+    content {
+      subnet_id = subnet_mapping.key
+      private_ipv4_address = subnet_mapping.value != "auto" ?  subnet_mapping.value : null
+    }
+  }
+
+  # subnet_mapping {
+  #   subnet_id = var.lb_subnet_ids[0]
+  #   private_ipv4_address = var.private_ips_for_lb[0]
   # }
 
-  subnet_mapping {
-    subnet_id = var.lb_subnet_ids[0]
-    private_ipv4_address = var.private_ips_for_lb[0]
-  }
-
-  subnet_mapping {
-    subnet_id = var.lb_subnet_ids[1]
-    //private_ipv4_address = var.private_ips_for_lb[1]
-  }
+  # subnet_mapping {
+  #   subnet_id = var.lb_subnet_ids[1]
+  #   //private_ipv4_address = var.private_ips_for_lb[1]
+  # }
 
 
   tags = merge(local.default_tags, {
