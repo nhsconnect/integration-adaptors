f1fbb43d2f0b71e14a93760b319b7264563237de
Łukasz Kwaśniewski <l.kwasniewski@kainos.com>
2021-02-19 15:43:31 +0100

    Add rule to allow outgoing LDAPS on PTL env

--- terraform/aws/components/base/sgrules_core.tf
+++ terraform/aws/components/base/sgrules_core.tf
@@ -7,3 +7,14 @@ resource "aws_security_group_rule" "core_to_internet_443" {
   cidr_blocks = ["0.0.0.0/0"]
   description = "Allow outgoing internet traffic on 443 in env: ${var.environment}"
 }
+
+resource "aws_security_group_rule" "core_to_ldap_636" {
+  count = var.ptl_connected ? 1 : 0
+  type = "egress"
+  from_port = 636
+  to_port = 636
+  protocol = "tcp"
+  security_group_id = aws_security_group.core_sg.id
+  cidr_blocks = ["0.0.0.0/0"]
+  description = "Allow outgoing LDAPS connections in env: ${var.environment}"
+}
