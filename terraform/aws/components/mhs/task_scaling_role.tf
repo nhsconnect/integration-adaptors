acb0dfe8027a84df35ee069c4b08aeb000a2732f
Łukasz Kwaśniewski <l.kwasniewski@kainos.com>
2021-02-19 11:45:59 +0100

    Output the hostname of testbox if created #3

--- terraform/aws/components/mhs/task_scaling_role.tf
+++ terraform/aws/components/mhs/task_scaling_role.tf
@@ -1,3 +1,3 @@
 data "aws_iam_role" "ecs_autoscale_role" {
-  name = "ecsAutoscaleRole"
+  name = "aws-service-role/ecs.application-autoscaling.amazonaws.com/AWSServiceRoleForApplicationAutoScaling_ECSService"
 }
