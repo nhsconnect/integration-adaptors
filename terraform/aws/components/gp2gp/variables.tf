b046f3768b2b748faa596e12b7064a52e7ac2432
RIashfaqahmed <25818860+RIashfaqahmed@users.noreply.github.com>
2021-02-22 10:56:50 +0000

    Add variables for S3 buckets

--- terraform/aws/components/gp2gp/variables.tf
+++ terraform/aws/components/gp2gp/variables.tf
@@ -153,6 +153,17 @@ variable "gp2gp_service_target_request_count" {
   default = 1200
 }
 
+variable "gp2gp_extract_cache_bucket" {
+  type = string
+  description = "Bucket to store extracts from app to use as cache"
+}
+
+variable "gp2gp_extract_cache_bucket_retention_period" {
+  type = number
+  description = "Number of days objects will be retained in gp2gp_extract_cache_bucket"
+  default = 7
+}
+
 variable mhs_inbound_queue_name {
   type = string
   description = "Name of queue used by MHS Inbound "
