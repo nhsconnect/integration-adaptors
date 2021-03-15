bfb798e12c2a9e2e958e9e24eb7b3f4e59e568fe
Łukasz Kwaśniewski <l.kwasniewski@kainos.com>
2021-03-15 15:09:29 +0100

    Cleanup #2

--- terraform/aws/components/lab-results/locals_env_variables.tf
+++ terraform/aws/components/lab-results/locals_env_variables.tf
@@ -50,7 +50,7 @@ locals {
     },
     {
       name = "LAB_RESULTS_MONGO_OPTIONS"
-      value = var.lab-results_mongo_options
+      value = join("&",[var.var.lab-results_mongo_options,"ssl=${var.mongo_ssl_enabled}"])
     },
     {
       name = "LAB_RESULTS_MONGO_AUTO_INDEX_CREATION"
