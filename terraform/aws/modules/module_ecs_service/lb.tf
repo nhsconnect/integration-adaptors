7534bce0fd774152bf10f937eab3037a8afbaa4f
Matt Dooner <mattd@kainos.com>
2021-03-24 16:41:11 +0000

    comment

--- terraform/aws/modules/module_ecs_service/lb.tf
+++ terraform/aws/modules/module_ecs_service/lb.tf
@@ -4,7 +4,9 @@ resource "aws_lb" "service_load_balancer" {
   internal = true
   load_balancer_type = var.load_balancer_type
   security_groups = local.lb_sgs
-  # Needed for LB in AZ A to route to service in AZ B
+
+  # Only used if type is network. Required for Spine which only routes to a
+  # single IP in a single zone but services run in multiple AZ
   enable_cross_zone_load_balancing = true
 
   dynamic "subnet_mapping" {
