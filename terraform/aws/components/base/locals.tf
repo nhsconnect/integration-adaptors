94712311937990f3e42032e7438a24c596aab473
LukaszKwasniewski <l.kwasniewski@kainos.com>
2021-03-29 14:06:27 +0200

    Use 2 AZ for LBs, and 2 AZ for containers in PTL (#360)
    * Use 2 AZ for LBs, and 2 AZ for containers in PTL
    
    * Comment out LB mentions
    
    * Comment out LB mentions #2
    
    * Comment out LB mentions #3
    
    * Correct number of AZ for route table assoc
    
    * Restore module outputs
    
    * Restore module outputs #2
    
    * Restore module outputs #3

--- terraform/aws/components/base/locals.tf
+++ terraform/aws/components/base/locals.tf
@@ -10,24 +10,24 @@ locals {
   ptl_lb_availability_zones = ["${var.region}a", "${var.region}b"]
 
 # The minimal size for aws subnet is /28 - 14 hosts
+# but with icreasing number of adapters, we need more ips for LBs
+# as when adding a new LB, at least 8 free adresses have to remain
 # We need at least two of these for inbound load balancer
 # We have to make sure that the assigned ip is always available to the Inbound Load Balancer
 # For Inbound LB
-# cidrsubnet("10.x.x.128/25",3,0) # 10.x.x.128/28 # zone a
-# cidrsubnet("10.x.x.128/25",3,1) # 10.x.x.144/28 # zone b
+# cidrsubnet("10.x.x.128/25",2,0) # 10.x.x.128/27 # zone a
+# cidrsubnet("10.x.x.128/25",2,1) # 10.x.x.160/27 # zone b
 # For everything else
-# cidrsubnet("10.x.x.128/25",2,1) # 10.x.x.160/27 # zone a
-# cidrsubnet("10.x.x.128/25",2,2) # 10.x.x.192/27 # zone b
-# cidrsubnet("10.x.x.128/25",2,3) # 10.x.x.224/27 # zone c
+# cidrsubnet("10.x.x.128/25",2,2) # 10.x.x.192/27 # zone a
+# cidrsubnet("10.x.x.128/25",2,3) # 10.x.x.224/27 # zone b
 
   ptl_lb_subnet_cidrs = var.ptl_connected ? [
-    cidrsubnet(var.ptl_assigned_cidr,3,0),
-    cidrsubnet(var.ptl_assigned_cidr,3,1)
+    cidrsubnet(var.ptl_assigned_cidr,2,0),
+    cidrsubnet(var.ptl_assigned_cidr,2,1)
   ] : []
 
   ptl_containers_subnet_cidrs = var.ptl_connected ? [
-    cidrsubnet(var.ptl_assigned_cidr,2,1),
     cidrsubnet(var.ptl_assigned_cidr,2,2),
-    cidrsubnet(var.ptl_assigned_cidr,2,3)
+    cidrsubnet(var.ptl_assigned_cidr,2,3),
   ] : []
 }
--- terraform/aws/components/base/subnet_ptl.tf
+++ terraform/aws/components/base/subnet_ptl.tf
@@ -1,10 +1,10 @@
 # PTL - separate subnets for LB and others
 
 resource "aws_subnet" "service_containers_subnet" {
-  count = var.ptl_connected ? length(local.availability_zones) : 0
+  count = var.ptl_connected ? length(local.ptl_lb_availability_zones) : 0
   vpc_id  = aws_vpc_ipv4_cidr_block_association.base_ptl_cidr[0].vpc_id
   cidr_block = local.ptl_containers_subnet_cidrs[count.index]
-  availability_zone = local.availability_zones[count.index]
+  availability_zone = local.ptl_lb_availability_zones[count.index]
 
   tags = merge(local.default_tags, {
     Name = "${local.resource_prefix}-ptl_containers_subnet-${count.index}"
