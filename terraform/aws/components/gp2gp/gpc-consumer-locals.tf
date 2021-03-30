3590d9664b8e0a75da5939665155af078ef3b577
Ashfaq Ahmed <25818860+RIashfaqahmed@users.noreply.github.com>
2021-03-30 14:39:58 +0100

    Removal of gpc-consumer-wiremock ecs service

--- terraform/aws/components/gp2gp/gpc-consumer-locals.tf
+++ terraform/aws/components/gp2gp/gpc-consumer-locals.tf
@@ -13,8 +13,7 @@ locals {
   # Use below when the ECR repo is created by terraform in account component.
   #image_name = "${data.terraform_remote_state.account.outputs.nhais_ecr_repo_url}:${var.nhais_build_id}"
   gpc-consumer_image_name = "${var.account_id}.dkr.ecr.${var.region}.amazonaws.com/gpc-consumer:${var.gpc-consumer_build_id}"
-  gpc-consumer_wiremock_image_name = "${var.account_id}.dkr.ecr.${var.region}.amazonaws.com/gpc-consumer-wiremock:${var.gpc-consumer_build_id}"
-
+  
   #subnet_cidrs = [
   #  cidrsubnet(data.terraform_remote_state.base.outputs.gpc-consumer_cidr,2,0),
   #  cidrsubnet(data.terraform_remote_state.base.outputs.gpc-consumer_cidr,2,1),
