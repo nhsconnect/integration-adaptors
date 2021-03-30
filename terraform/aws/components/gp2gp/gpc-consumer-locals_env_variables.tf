07f456bd8f077ec8b7a06799774c678b14ce5b8b
Ashfaq Ahmed <25818860+RIashfaqahmed@users.noreply.github.com>
2021-03-30 15:31:44 +0100

    Code clean up (removal of mesh references)

--- terraform/aws/components/gp2gp/gpc-consumer-locals_env_variables.tf
+++ terraform/aws/components/gp2gp/gpc-consumer-locals_env_variables.tf
@@ -49,34 +49,6 @@ locals {
       value = var.gpc-consumer_sds_url
     },
     {
-      name = "GPC_CONSUMER_MESH_API_HTTP_PROXY"
-      value = var.opentest_connected ? data.aws_instance.opentest_instance.private_ip : ""
-    },
-    {
-      name = "GPC_CONSUMER_AMQP_BROKERS"
-      value = replace(data.aws_mq_broker.nhais_mq_broker.instances[0].endpoints[1],"amqp+ssl","amqps") # https://www.terraform.io/docs/providers/aws/r/mq_broker.html#attributes-reference
-    },
-    {
-      name = "GPC_CONSUMER_MESH_OUTBOUND_QUEUE_NAME"
-      value = "${var.environment}_gpc-consumer_mesh_outbound"
-    },
-    {
-      name = "GPC_CONSUMER_MESH_INBOUND_QUEUE_NAME"
-      value = "${var.environment}_gpc-consumer_mesh_inbound"
-    },
-    {
-      name = "GPC_CONSUMER_GP_SYSTEM_INBOUND_QUEUE_NAME"
-      value = "${var.environment}_gpc-consumer_gp_system_inbound"
-    },
-    {
-      name = "GPC_CONSUMER_AMQP_MAX_RETRIES"
-      value = var.gpc-consumer_amqp_max_retries
-    },
-    {
-      name = "GPC_CONSUMER_AMQP_RETRY_DELAY"
-      value = var.gpc-consumer_amqp_retry_delay
-    },
-    {
       name = "GPC_CONSUMER_MONGO_DATABASE_NAME"
       value = "gpc-consumer"
     },
@@ -91,30 +63,6 @@ locals {
     {
       name = "GPC_CONSUMER_MONGO_OPTIONS"
       value = join("&",[var.gpc-consumer_mongo_options,"ssl=${data.terraform_remote_state.base.outputs.docdb_tls_enabled}"])
-    },
-    {
-      name = "GPC_CONSUMER_MESH_HOST"
-      value = var.gpc-consumer_mesh_host
-    },
-    {
-      name = "GPC_CONSUMER_MESH_CERT_VALIDATION"
-      value = var.gpc-consumer_mesh_cert_validation
-    },
-    {
-      name = "GPC_CONSUMER_MESH_POLLING_CYCLE_MINIMUM_INTERVAL_IN_SECONDS"
-      value = var.gpc-consumer_mesh_polling_cycle_minimum_interval_in_seconds
-    },
-    {
-      name = "GPC_CONSUMER_MESH_CLIENT_WAKEUP_INTERVAL_IN_MILLISECONDS"
-      value = var.gpc-consumer_mesh_client_wakeup_interval_in_milliseconds
-    },
-    {
-      name = "GPC_CONSUMER_MESH_POLLING_CYCLE_DURATION_IN_SECONDS",
-      value = var.gpc-consumer_mesh_polling_cycle_duration_in_seconds
-    },
-    {
-      name = "GPC_CONSUMER_SCHEDULER_ENABLED"
-      value = var.gpc-consumer_scheduler_enabled
     },
     {
       name = "GPC_CONSUMER_SSL_TRUST_STORE_URL"
