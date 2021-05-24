34d74ca5569e7316cd636e09a545e4fa842bcc92
ElliotB-96 <elliotb@kainos.com>
2021-05-24 16:59:37 +0100

    Updated to use both overrides

--- terraform/aws/components/gp2gp/locals_env_variables.tf
+++ terraform/aws/components/gp2gp/locals_env_variables.tf
@@ -71,12 +71,15 @@ locals {
     {
       name = "GP2GP_GPC_OVERRIDE_NHS_NUMBER"
       value = var.gp2gp_gpc_override_nhs_number
+    },
+    {
+      name = "GP2GP_GPC_OVERRIDE_TO_ASID"
+      value = var.gp2gp_gpc_override_to_asid
+    },
+    {
+      name = "GP2GP_GPC_OVERRIDE_FROM_ASID"
+      value = var.gp2gp_gpc_override_from_asid
     }
-    ,
-        {
-          name = "GP2GP_GPC_OVERRIDE_TO_ASID"
-          value = var.gp2gp_gpc_override_to_asid
-        }
   ])
 
   mock_mhs_environment_variables = [
--- terraform/aws/components/gp2gp/variables.tf
+++ terraform/aws/components/gp2gp/variables.tf
@@ -227,6 +227,12 @@ variable gp2gp_gpc_override_nhs_number {
 
 variable gp2gp_gpc_override_to_asid {
   type = string
+  description = "Override value to override to aside in GPC requests"
+  default = ""
+}
+
+variable gp2gp_gpc_override_from_asid {
+  type = string
   description = "Override value to override from aside in GPC requests"
   default = ""
 }
