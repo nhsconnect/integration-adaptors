2d695b7e9d8f6ea452795c97ca6a4a8bc7a59515
RIashfaqahmed <25818860+RIashfaqahmed@users.noreply.github.com>
2021-05-27 14:36:46 +0100

    Trim trailing dot from URL value of Env Var (#385)

--- terraform/aws/components/gp2gp/locals_env_variables.tf
+++ terraform/aws/components/gp2gp/locals_env_variables.tf
@@ -62,7 +62,7 @@ locals {
     },
     {
       name = "GP2GP_MHS_OUTBOUND_URL"
-      value = var.gp2gp_create_mhs_mock ? "http://${module.mock_mhs_ecs_service[0].loadbalancer_dns_name}:${var.gp2gp_mock_mhs_port}/mock-mhs-endpoint" : "http://mhs-outbound.${data.terraform_remote_state.base.outputs.r53_zone_name}/"
+      value = var.gp2gp_create_mhs_mock ? "http://${module.mock_mhs_ecs_service[0].loadbalancer_dns_name}:${var.gp2gp_mock_mhs_port}/mock-mhs-endpoint" : "http://mhs-outbound.${trimsuffix(data.terraform_remote_state.base.outputs.r53_zone_name,".")}/"
     },
     {
       name = "GP2GP_GPC_GET_URL"
