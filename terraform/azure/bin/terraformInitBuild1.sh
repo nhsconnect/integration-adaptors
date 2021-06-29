033d1f82519aa84b5e9932cb09b11d88405054fc
Ashfaq Ahmed <25818860+RIashfaqahmed@users.noreply.github.com>
2021-06-29 16:17:51 +0100

    Adding Azure Resource Group Reference

--- terraform/azure/bin/terraformInitBuild1.sh
+++ terraform/azure/bin/terraformInitBuild1.sh
@@ -3,5 +3,16 @@ export TF_ENVIRONMENT="build1"
 export TF_COMPONENT=$(basename $(pwd))
 export TF_STATE_CONTAINER=${TF_PROJECT}-state-container
 export TF_STATE_ACCOUNT=${TF_PROJECT}state
+export TF_STATE_RESOURCE_GROUP=${TF_PROJECT}-state-rg
 
-terraform init -backend-config=key=${TF_PROJECT}-${TF_ENVIRONMENT}-${TF_COMPONENT} -backend-config=container_name=${TF_STATE_CONTAINER} -backend-config=storage_account_name=${TF_STATE_ACCOUNT}
+echo $TF_PROJECT
+echo "TF_PROJECT = ${TF_PROJECT}"
+echo "TF_ENVIRONMENT = ${TF_ENVIRONMENT}"
+echo "TF_COMPONENT = ${TF_COMPONENT}"
+echo "TF_STATE_CONTAINER = ${TF_STATE_CONTAINER}"
+echo "TF_STATE_ACCOUNT = ${TF_STATE_ACCOUNT}"
+echo "TF_STATE_RESOURCE_GROUP = ${TF_STATE_RESOURCE_GROUP}"
+
+
+terraform init -backend-config=resource_group_name=${TF_STATE_RESOURCE_GROUP} -backend-config=key=${TF_PROJECT}-${TF_ENVIRONMENT}-${TF_COMPONENT} -backend-config=container_name=${TF_STATE_CONTAINER} -backend-config=storage_account_name=${TF_STATE_ACCOUNT}
+
