a5cffb0c38e3d379bd8c1e1cdbff48461fccf8e8
Tyler McDowell <Tylerm@kainos.com>
2021-10-01 17:06:35 +0100

    adding a depends on for private dns zone virtual link

--- terraform/azure/components/base/private_dns_zone.tf
+++ terraform/azure/components/base/private_dns_zone.tf
@@ -8,4 +8,6 @@ resource "azurerm_private_dns_zone_virtual_network_link" "virtual_network_link" 
   resource_group_name   = var.account_resource_group
   private_dns_zone_name = azurerm_private_dns_zone.private-dns.name
   virtual_network_id    = azurerm_virtual_network.base_vnet.id
+
+  depends_on = [azurerm_private_dns_zone.private-dns]
 }
