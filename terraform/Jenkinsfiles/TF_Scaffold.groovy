7ad17e7d32fb4257b94856d04718070f8966c923
Tyler-34 <77496655+Tyler-34@users.noreply.github.com>
2021-11-15 15:36:31 +0000

    Update TF_Scaffold.groovy

--- terraform/Jenkinsfiles/TF_Scaffold.groovy
+++ terraform/Jenkinsfiles/TF_Scaffold.groovy
@@ -6,8 +6,8 @@ Map <String, Map<String, String>> componentImageBranch = [
   nhais_responder: [ecrRepo: "nhais-fake-responder", branch: "origin-develop"],
   gp2gp:           [ecrRepo: "gp2gp",                branch: "main"],
   mhs:             [ecrRepo: "mhs/outbound",         branch: "develop"], // mhs/route and mhs/inboud will have the same tag, no need to search in them
-  "lab-results":     [ecrRepo: "lab-results",          branch: "main"]
-  "pss":           [ecrRepo: "pss",                  branch: "main"]
+  "lab-results":     [ecrRepo: "lab-results",          branch: "main"],
+  pss:           [ecrRepo: "pss",                    branch: "main"]
 ]
 
 pipeline {
