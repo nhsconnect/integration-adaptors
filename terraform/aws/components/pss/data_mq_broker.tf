ac02a0f953e3f436c1e70143677c8b1127995369
Tyler McDowell <Tylerm@kainos.com>
2021-11-15 10:38:37 +0000

    removing pss component from this branch

--- terraform/aws/components/pss/data_mq_broker.tf
+++
@@ -1,3 +0,0 @@
-data "aws_mq_broker" "pss_mq_broker" {
-  broker_name = var.mq_broker_name
-}
--- terraform/aws/components/pss/data_remote_state_base.tf
+++
@@ -1,9 +0,0 @@
-data "terraform_remote_state" "base" {
-  backend = "s3"
-  
-  config = {
-    bucket = var.tf_state_bucket
-    key = "${var.project}-${var.environment}-base.tfstate"
-    region = var.region
-  }
-}
