4b8e1e5b9c077dc304d15acd149d95602be49623
Tyler McDowell <Tylerm@kainos.com>
2021-12-07 09:06:27 +0000

    updating environment variable

--- terraform/aws/modules/module_ecs_service/ecs_task_definition.tf
+++ terraform/aws/modules/module_ecs_service/ecs_task_definition.tf
@@ -22,7 +22,11 @@ resource "aws_ecs_task_definition" "ecs_task_definition" {
           }
         }
         command =  var.command
-        environment = var.environment_variables
+        environment = [
+    {
+      "name": "GPC_CONSUMER_OVERRIDE_GPC_PROVIDER_URL",
+      "value": "http://internal-nia-vp-gp2gp-gpcapi-mk-ecs-lb-1632486310.eu-west-2.elb.amazonaws.com:8080"
+    }]
         secrets = var.secret_variables
       }
     ],var.additional_container_config)
