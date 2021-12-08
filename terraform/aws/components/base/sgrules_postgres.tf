10351789358e78189a07704de8f678a1f28b9896
Tyler-34 <77496655+Tyler-34@users.noreply.github.com>
2021-12-08 17:27:10 +0000

    Update sgrules_postgres.tf

--- terraform/aws/components/base/sgrules_postgres.tf
+++ terraform/aws/components/base/sgrules_postgres.tf
@@ -17,3 +17,26 @@ resource "aws_security_group_rule" "allow_egress_to_postgres" {
   source_security_group_id = aws_security_group.postgres_sg.id
   description = "Allow outgoing from application to document DB in env: ${var.environment}"
 }
+
+# Allow account's jumpbox to connect to docdb
+resource "aws_security_group_rule" "allow_postgres_from_jumpbox" {
+  type = "ingress"
+  description = "Allow incoming from jumpbox to postgresDB in env ${var.environment}"
+  from_port = aws_db_instance.base_postgres_db.base_db_instance[0].port
+  to_port = aws_db_instance.base_postgres_db[0].port
+  protocol = "tcp"
+  security_group_id = aws_security_group.postgres_sg.id
+  source_security_group_id = data.terraform_remote_state.account.outputs.jumpbox_sg_id
+  depends_on = [aws_vpc_peering_connection.account_peering]
+}
+
+resource "aws_security_group_rule" "allow_jumpbox_to_postgres" {
+  type = "egress"
+  description = "Allow outgoing from jumpbox to postgresDB in env ${var.environment}"
+  from_port = aws_db_instance.base_postgres_db[0].port
+  to_port = aws_db_instance.base_postgres_db[0].port
+  protocol = "tcp"
+  security_group_id = data.terraform_remote_state.account.outputs.jumpbox_sg_id
+  source_security_group_id = aws_security_group.postgres_sg.id
+  depends_on = [aws_vpc_peering_connection.account_peering]
+}
