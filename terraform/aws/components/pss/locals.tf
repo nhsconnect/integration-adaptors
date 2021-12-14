1f2f8450917832c0f16762e41350d73c53ab6516
Tyler McDowell <Tylerm@kainos.com>
2021-12-14 09:57:05 +0000

    resolving reviews

--- terraform/aws/components/pss/locals.tf
+++ terraform/aws/components/pss/locals.tf
@@ -9,9 +9,9 @@ locals {
 
   availability_zones = ["${var.region}a", "${var.region}b", "${var.region}c"]
 
-  gp2gp_translator_image_name = "${var.account_id}.dkr.ecr.${var.region}.amazonaws.com/pss:${var.gp2gp_translator_build_id}"
-  mhs_mock_image_name = "${var.account_id}.dkr.ecr.${var.region}.amazonaws.com/gp2gp-mock-mhs:${var.pss_mhs_mock_build_id}"
-  gpc_api_facade_image_name = "${var.account_id}.dkr.ecr.${var.region}.amazonaws.com/gp2gp-mock-mhs:${var.gpc_api_facade_build_id}"
+  gp2gp_translator_image_name = "${var.account_id}.dkr.ecr.${var.region}.amazonaws.com/pss_gp2gp-translator:${var.gp2gp_translator_build_id}"
+  mhs_mock_image_name = "${var.account_id}.dkr.ecr.${var.region}.amazonaws.com/pss-mock-mhs:${var.pss_mhs_mock_build_id}"
+  gpc_api_facade_image_name = "${var.account_id}.dkr.ecr.${var.region}.amazonaws.com/pss_gpc_facade:${var.gpc_api_facade_build_id}"
 
 
   subnet_cidrs = [
--- terraform/aws/components/pss/locals_secret_variables.tf
+++ terraform/aws/components/pss/locals_secret_variables.tf
@@ -1,11 +1,11 @@
 locals {
   secret_variables = [
     {
-      name      = "pss_postgres_USERNAME"
+      name      = "PSS_POSTGRES_USERNAME"
       valueFrom = data.aws_secretsmanager_secret.postgres_master_username.arn
     },
     {
-      name      = "pss_postgres_PASSWORD"
+      name      = "PSS_POSTGRES_PASSWORD"
       valueFrom = data.aws_secretsmanager_secret.postgres_master_password.arn
     },
     {
