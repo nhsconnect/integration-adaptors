4fc72191263240d9f8259a5ea25b341ce4f5cf4b
Tyler McDowell <Tylerm@kainos.com>
2021-12-15 17:03:01 +0000

    fixing outputs

--- terraform/aws/components/base/sgrules_postgres.tf
+++ terraform/aws/components/base/sgrules_postgres.tf
@@ -8,7 +8,7 @@ resource "aws_security_group_rule" "allow_ingress_in_postgres" {
   description = "Allow incoming from application to document DB in env: ${var.environment}"
 }
 
-resource "aws_security_group_rule" "allow_egress_to_postgres" {
+/*resource "aws_security_group_rule" "allow_egress_to_postgres" {
   type = "egress"
   from_port = aws_db_instance.base_postgres_db[0].port
   to_port = aws_db_instance.base_postgres_db[0].port
@@ -16,7 +16,7 @@ resource "aws_security_group_rule" "allow_egress_to_postgres" {
   security_group_id = aws_security_group.postgres_access_sg.id
   source_security_group_id = aws_security_group.postgres_sg.id
   description = "Allow outgoing from application to document DB in env: ${var.environment}"
-}
+}*/
 
 # Allow account's jumpbox to connect to docdb
 resource "aws_security_group_rule" "allow_postgres_from_jumpbox" {
