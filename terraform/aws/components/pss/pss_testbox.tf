bc987072e1cd33d91736d0ee212221068285ce86
Tyler McDowell <Tylerm@kainos.com>
2021-12-17 11:04:53 +0000

    adding testbox

--- terraform/aws/components/pss/pss_testbox.tf
+++ terraform/aws/components/pss/pss_testbox.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "pss_testbox" {
   ami           = data.aws_ami.base_linux.id
   instance_type = "t2.micro"
   key_name = "kainos-dev"
-  vpc_security_group_ids = [aws_security_group.pss_testbox_sg]
+  vpc_security_group_ids = [aws_security_group.pss_testbox_sg.id]
   subnet_id = aws_subnet.service_subnet.id
 
   tags = merge(local.default_tags, {
