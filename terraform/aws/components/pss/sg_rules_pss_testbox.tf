002571841a4a14449fffb0eb6f1f9906af840a50
Tyler McDowell <Tylerm@kainos.com>
2021-12-17 12:29:34 +0000

    adding testbox

--- terraform/aws/components/pss/sg_rules_pss_testbox.tf
+++ terraform/aws/components/pss/sg_rules_pss_testbox.tf
@@ -3,7 +3,7 @@ resource "aws_security_group_rule" "connect_to_testbox" {
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
-  cidr_blocks       = [aws_security_group.jumpbox_sg.id]
+  cidr_blocks       = [data.terraform_remote_state.account.outputs.jumpbox_sg_id]
   security_group_id = aws_security_group.pss_testbox_sg.id
 }
 
