afa4f4fe743068b23d54da40ea5284bf72bce369
bchotkowski <bartosz.chotkowski@kainos.com>
2021-12-27 13:09:16 +0100

    Removed unwanted argument, increased memory

--- terraform/aws/modules/module_ecs_service/variables.tf
+++ terraform/aws/modules/module_ecs_service/variables.tf
@@ -147,13 +147,13 @@ variable "image_name" {
 variable "cpu_units" {
   type = number
   description = "Number of CPU units to assign to containers"
-  default = 2048
+  default = 4096
 }
 
 variable "memory_units" {
   type = number
   description = "Number of Memory units to assign to containers"
-  default = 4096
+  default = 8192
 }
 
 variable "network_mode" {
