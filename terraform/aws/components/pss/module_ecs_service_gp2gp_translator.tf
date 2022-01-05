82998221f514b0bc60c219539f13b0a63eb3085d
Ashfaq <25818860+RIashfaqahmed@users.noreply.github.com>
2022-01-05 23:59:30 +0000

    Removed SG PSS External Ref from ECS

--- terraform/aws/components/pss/module_ecs_service_gp2gp_translator.tf
+++ terraform/aws/components/pss/module_ecs_service_gp2gp_translator.tf
@@ -37,8 +37,7 @@ module "ecs_service_gp2gp_translator" {
 
   additional_security_groups = [
     data.terraform_remote_state.base.outputs.core_sg_id,
-    data.terraform_remote_state.base.outputs.postgres_access_sg_id,
-    aws_security_group.pss_external_access.id
+    data.terraform_remote_state.base.outputs.postgres_access_sg_id
   ]
 
   lb_allowed_security_groups = [
--- terraform/aws/components/pss/module_ecs_service_gpc_api_facade.tf
+++ terraform/aws/components/pss/module_ecs_service_gpc_api_facade.tf
@@ -38,8 +38,7 @@ module "ecs_service_gpc_api_facade" {
 
   additional_security_groups = [
     data.terraform_remote_state.base.outputs.core_sg_id,
-    data.terraform_remote_state.base.outputs.postgres_access_sg_id,
-    aws_security_group.pss_external_access.id
+    data.terraform_remote_state.base.outputs.postgres_access_sg_id
   ]
 
   lb_allowed_security_groups = [
