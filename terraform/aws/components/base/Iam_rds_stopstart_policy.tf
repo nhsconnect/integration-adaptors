360998b772ecfe107a6322018247d62e89521cc1
Tyler McDowell <Tylerm@kainos.com>
2022-01-25 14:50:25 +0000

    adding iam policy and role for system state manager

---
+++ terraform/aws/components/base/Iam_rds_stopstart_policy.tf
@@ -0,0 +1,44 @@
+resource "aws_iam_role" "rds_stop_start_role" {
+  name = "rds_scheduler"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ssm.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+
+resource "aws_iam_policy" "rds_stop_start_Policy" {
+  name        = "RDS_AutoStopStart"
+  description = "My test policy"
+
+ 
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "rds:Describe*",
+                "rds:Start*",
+                "rds:Stop*",
+                "rds:Reboot*"
+            ],
+            "Resource": "*"
+        }
+    ]
+})
+}
+
+resource "aws_iam_role_policy_attachment" "test-attach" {
+  role       = aws_iam_role.rds_stop_start_role.name
+  policy_arn = aws_iam_policy.rds_stop_start_Policy.arn
+}
---
+++ terraform/aws/components/base/state_manager_association.tf
@@ -0,0 +1,63 @@
+/*resource "aws_ssm_association" "stop_rds_association" {
+  name = aws_ssm_document.stop_rds_document.name
+  association_name = "Stop_RDS"
+  schedule_expression = "cron(0 00 18 ? * * *)"
+
+  parameters { 
+    InstanceId = aws_db_instance.base_postgres_db.id
+    AutomationAssumeRole = aws_iam_role.rds_stop_start_role.name
+  }
+}
+
+resource "aws_ssm_document" "stop_rds_document" {
+  name          = "AWS-StopRdsInstance"
+  document_type = "Automation"
+
+  content = <<DOC
+  {
+schemaVersion: "0.3"
+assumeRole: "{{ AutomationAssumeRole }}"
+parameters:
+  InstanceId:
+    type: String
+    description: (Required) RDS Instance Id to stop
+  AutomationAssumeRole:
+    type: String
+    description: (Optional) The ARN of the role that allows Automation to perform the actions on your behalf.
+    default: ""
+mainSteps:
+  -
+    name: AssertNotStopped
+    action: aws:assertAwsResourceProperty
+    isCritical: false
+    onFailure: step:StopInstance
+    nextStep: CheckStop
+    inputs:
+      Service: rds
+      Api: DescribeDBInstances
+      DBInstanceIdentifier: "{{InstanceId}}"
+      PropertySelector: "$.DBInstances[0].DBInstanceStatus"
+      DesiredValues: ["stopped", "stopping"]
+  -
+    name: StopInstance
+    action: aws:executeAwsApi
+    inputs:
+      Service: rds
+      Api: StopDBInstance
+      DBInstanceIdentifier: "{{InstanceId}}"
+  -
+    name: CheckStop
+    action: aws:waitForAwsResourceProperty
+    onFailure: Abort
+    maxAttempts: 10
+    timeoutSeconds: 600
+    inputs:
+      Service: rds
+      Api: DescribeDBInstances
+      DBInstanceIdentifier: "{{InstanceId}}"
+      PropertySelector: "$.DBInstances[0].DBInstanceStatus"
+      DesiredValues: ["stopped"]
+  }
+DOC
+}
+*/
