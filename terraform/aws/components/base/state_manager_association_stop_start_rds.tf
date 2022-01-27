120e74bf974631f6940f52b3b6b0a2ad1ea94530
Tyler McDowell <Tylerm@kainos.com>
2022-01-27 16:01:18 +0000

    updating names for rds policy

--- terraform/aws/components/base/state_manager_association_stop_start_rds.tf
+++ terraform/aws/components/base/state_manager_association_stop_start_rds.tf
@@ -66,7 +66,7 @@ mainSteps:
 DOC
 }
 
-/*resource "aws_ssm_association" "start_rds_association" {
+resource "aws_ssm_association" "start_rds_association" {
   count = var.postgresdb_scheduler_enabled ? 1 : 0
   name = aws_ssm_document.start_rds_document[0].name
   association_name = "${replace(local.resource_prefix,"_","-")}-Start-RDS"
@@ -133,4 +133,4 @@ mainSteps:
         - available
     isEnd: true
 DOC
-}*/
+}
--- terraform/aws/components/pss/state_manager_association_stop_start_testbox.tf
+++ terraform/aws/components/pss/state_manager_association_stop_start_testbox.tf
@@ -1,6 +1,6 @@
 resource "aws_ssm_association" "stop_pss_testbox_association" {
   count = var.pss_testbox_scheduler_enabled ? 1 : 0
-  name = aws_ssm_document.stop_pss_testbox_document.name
+  name = aws_ssm_document.stop_pss_testbox_document[0].name
   association_name = "${replace(local.resource_prefix,"_","-")}-Stop_pss-testbox"
   schedule_expression = var.pss_testbox_scheduler_stop_pattern
   
@@ -59,7 +59,7 @@ DOC
 
 /*resource "aws_ssm_association" "start_pss_testbox_association" {
   count = var.pss_testbox_scheduler_enabled ? 1 : 0
-  name = aws_ssm_document.start_pss_testbox_document.name
+  name = aws_ssm_document.start_pss_testbox_document[0].name
   association_name = "${replace(local.resource_prefix,"_","-")}-Start_pss-testbox"
   schedule_expression = var.pss_testbox_scheduler_start_pattern
   
