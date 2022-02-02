03d94a3d021a4a51f0fa4002c7c1a0d54fc072ce
Tyler McDowell <Tylerm@kainos.com>
2022-02-02 19:04:52 +0000

    changing stop time to test

--- terraform/aws/components/base/ecs_schedule_start_stop.tf
+++ terraform/aws/components/base/ecs_schedule_start_stop.tf
@@ -4,11 +4,12 @@ resource "null_resource" "schedule_ecs_stop" {
     command = <<COMMAND
         aws application-autoscaling put-scheduled-action --service-namespace ecs \
         --scalable-dimension ecs:service:DesiredCount \
-        --resource-id service/nia-kdev-base-ecs_cluster/nia-kdev-ps_gpc_fcde_ecs-service \
+        --resource-id service/nia-kdev-base-ecs_cluster/nia-kdev-ps_mhs_mock_ecs-service \
         --scheduled-action-name cron-scaleout-action \
-        --schedule "cron(37 18 * * ? *)" \
+        --schedule "cron(10 19 * * ? *)" \
         --scalable-target-action MinCapacity=0,MaxCapacity=0 \
         --region eu-west-2
+
 COMMAND
   }
 }
@@ -19,11 +20,12 @@ resource "null_resource" "schedule_ecs_start" {
     command = <<COMMAND
         aws application-autoscaling put-scheduled-action --service-namespace ecs \
         --scalable-dimension ecs:service:DesiredCount \
-        --resource-id service/nia-kdev-base-ecs_cluster/nia-kdev-ps_gpc_fcde_ecs-service \
+        --resource-id service/nia-kdev-base-ecs_cluster/nia-kdev-ps_mhs_mock_ecs-service \
         --scheduled-action-name cron-scaleout-action \
-        --schedule "cron(50 18 * * ? *)" \
+        --schedule "cron(00 06 * * ? *)" \
         --scalable-target-action MinCapacity=1,MaxCapacity=1 \
         --region eu-west-2
+
 COMMAND
   }
 }
