7fa7638350f0cbf9ee10d8a49588f4597af72429
Tyler McDowell <Tylerm@kainos.com>
2022-02-02 18:48:11 +0000

    changing stop time to test

--- terraform/aws/components/base/ecs_schedule_start_stop.tf
+++ terraform/aws/components/base/ecs_schedule_start_stop.tf
@@ -6,7 +6,7 @@ resource "null_resource" "schedule_ecs_stop" {
         --scalable-dimension ecs:service:DesiredCount \
         --resource-id service/nia-kdev-base-ecs_cluster/nia-kdev-ps_gpc_fcde_ecs-service \
         --scheduled-action-name cron-scaleout-action \
-        --schedule "cron(33 18 * * ? *)" \
+        --schedule "cron(37 18 * * ? *)" \
         --scalable-target-action MinCapacity=0,MaxCapacity=0 \
         --region eu-west-2
 COMMAND
@@ -21,7 +21,7 @@ resource "null_resource" "schedule_ecs_start" {
         --scalable-dimension ecs:service:DesiredCount \
         --resource-id service/nia-kdev-base-ecs_cluster/nia-kdev-ps_gpc_fcde_ecs-service \
         --scheduled-action-name cron-scaleout-action \
-        --schedule "cron(40 18 * * ? *)" \
+        --schedule "cron(50 18 * * ? *)" \
         --scalable-target-action MinCapacity=1,MaxCapacity=1 \
         --region eu-west-2
 COMMAND
