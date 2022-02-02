e024a91be1127674e6c966786c5a3b5a3f3ecd6b
Tyler McDowell <Tylerm@kainos.com>
2022-02-02 16:43:52 +0000

    adding local exec to run aws command to add a schedule to one service in kdev cluster

---
+++ terraform/aws/components/base/ecs_schedule_start_stop.tf
@@ -0,0 +1,29 @@
+resource "null_resource" "schedule_ecs_stop" {
+
+  provisioner "local-exec" {
+    command = <<COMMAND
+        aws application-autoscaling put-scheduled-action --service-namespace ecs \
+        --scalable-dimension ecs:service:DesiredCount \
+        --resource-id service/nia-kdev-base-ecs_cluster/nia-build1-ps_gpc_fcde_ecs-service \
+        --scheduled-action-name cron-scaleout-action \
+        --schedule "cron(00 18 * * ? *)" \
+        --scalable-target-action MinCapacity=1,MaxCapacity=1 \
+        --region eu-west-2
+COMMAND
+  }
+}
+
+resource "null_resource" "schedule_ecs_start" {
+
+  provisioner "local-exec" {
+    command = <<COMMAND
+        aws application-autoscaling put-scheduled-action --service-namespace ecs \
+        --scalable-dimension ecs:service:DesiredCount \
+        --resource-id service/nia-kdev-base-ecs_cluster/nia-build1-ps_gpc_fcde_ecs-service \
+        --scheduled-action-name cron-scaleout-action \
+        --schedule "cron(30 14 * * ? *)" \
+        --scalable-target-action MinCapacity=1,MaxCapacity=1 \
+        --region eu-west-2
+COMMAND
+  }
+}
