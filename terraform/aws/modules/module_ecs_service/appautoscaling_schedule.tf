325be38cbc9480aa5e96c0e750cf7c7773702aad
Tyler McDowell <Tylerm@kainos.com>
2022-02-03 12:32:25 +0000

    adding auto schedule to test

--- terraform/aws/modules/module_ecs_service/appautoscaling_schedule.tf
+++ terraform/aws/modules/module_ecs_service/appautoscaling_schedule.tf
@@ -10,3 +10,16 @@ resource "aws_appautoscaling_scheduled_action" "ecs_schedule_stop" {
     max_capacity = 0
   }
 }
+
+resource "aws_appautoscaling_scheduled_action" "ecs_schedule_start" {
+  name               = "${local.resource_prefix}_schedule_start"
+  service_namespace  = "ecs"
+  resource_id        = "service/${aws_ecs_service.ecs_service.cluster}/${aws_ecs_service.ecs_service.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  schedule           = "at(2022-02-03T12:40:00)"
+
+  scalable_target_action {
+    min_capacity = 1
+    max_capacity = 1
+  }
+}
