3fd4c282e0fd0157d34335939ab3c8d3679d592b
Tyler McDowell <Tylerm@kainos.com>
2022-02-09 10:00:26 +0000

    adding target and automation parameter name to association

--- terraform/aws/components/pss/state_manager_association_stop_start_testbox.tf
+++ terraform/aws/components/pss/state_manager_association_stop_start_testbox.tf
@@ -3,7 +3,7 @@ resource "aws_ssm_association" "stop_pss_testbox_association" {
   name = aws_ssm_document.stop_pss_testbox_document[0].name
   association_name = "${replace(local.resource_prefix,"_","-")}-Stop_pss-testbox"
   schedule_expression = var.pss_testbox_scheduler_stop_pattern
-  automation_target_parameter_name = aws_instance.pss_testbox.name
+  automation_target_parameter_name = aws_instance.pss_testbox[0].id
 
   targets = {
     key = "tag:EnableScheduler"
@@ -12,6 +12,7 @@ resource "aws_ssm_association" "stop_pss_testbox_association" {
   
 
   parameters = { 
+    InstanceId = aws_instance.pss_testbox[0].id
     AutomationAssumeRole = data.terraform_remote_state.account.outputs.scheduler_role_arn
   }
 }
