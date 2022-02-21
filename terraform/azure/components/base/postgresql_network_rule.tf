43229bddcea26291612bb8574f8450e8a219d5a1
Tyler McDowell <Tylerm@kainos.com>
2022-02-21 10:32:05 +0000

    adding postgres server and postgres network rules

--- terraform/azure/components/base/postgresql_network_rule.tf
+++ terraform/azure/components/base/postgresql_network_rule.tf
@@ -1,4 +1,4 @@
-resource "azurerm_postgresql_virtual_network_rule" "example" {
+resource "azurerm_postgresql_virtual_network_rule" "testbox_network_rule" {
   name                                 = "${local.resource_prefix}-postgresql-vnet-rule"
   resource_group_name                  = var.account_resource_group
   server_name                          = azurerm_postgresql_server.postgres_server.name
@@ -6,7 +6,7 @@ resource "azurerm_postgresql_virtual_network_rule" "example" {
   ignore_missing_vnet_service_endpoint = true
 }
 
-resource "azurerm_postgresql_virtual_network_rule" "example" {
+resource "azurerm_postgresql_virtual_network_rule" "aks_network_rule" {
   name                                 = "${local.resource_prefix}-postgresql-vnet-rule"
   resource_group_name                  = var.account_resource_group
   server_name                          = azurerm_postgresql_server.postgres_server.name
