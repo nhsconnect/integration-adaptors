c3603a6fcae3afe09434f131886e29ab00ca843f
BenHession <bshession@gmail.com>
2023-09-22 11:06:56 +0100

    Increase default consumer adaptor build to current version

--- terraform/aws/components/gp2gp/gpc-consumer-variables.tf
+++ terraform/aws/components/gp2gp/gpc-consumer-variables.tf
@@ -32,7 +32,7 @@ variable "gpc-consumer_service_launch_type" {
 variable "gpc-consumer_build_id" {
   type = string
   description = "Number of the current build, used for tagging the logs"
-  default = "main-21-188a231"
+  default = "main-51-5768b7f"
 }
 
 variable "gpc-consumer_ssl_trust_store_url" {
